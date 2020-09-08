ce177436bbae342e8d4f502ac8fede896d2ba296
Todor Bachvarov <tobachvarov@gmail.com>
2020-09-08 18:30:03 +0300

    Improve large text support for BrowsePanel

--- lib/ui/BrowsePanel.dart
+++ lib/ui/BrowsePanel.dart
@@ -429,13 +429,15 @@ class _BrowsePanelState extends State<BrowsePanel> implements NotificationsListe
                 child:  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        Localization().getStringEx("panel.browse.button.privacy_center.title","Privacy Center"),
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: Styles().colors.white,
-                            fontSize: 20,
-                            fontFamily: Styles().fontFamilies.bold),
+                      Expanded(child:
+                        Text(
+                          Localization().getStringEx("panel.browse.button.privacy_center.title","Privacy Center"),
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Styles().colors.white,
+                              fontSize: 20,
+                              fontFamily: Styles().fontFamilies.bold),
+                        ),
                       ),
                       Image.asset("images/group-8.png", excludeFromSemantics: true,),
                   ],),
@@ -655,7 +657,7 @@ class _RibbonButton extends StatelessWidget {
         button: true,
         excludeSemantics: true,
         child: Container(
-          height: 48,
+//          height: 48,
           padding: padding,
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 14),
