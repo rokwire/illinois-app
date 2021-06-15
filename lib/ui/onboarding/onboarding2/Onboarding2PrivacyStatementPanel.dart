7e6a97381cd71f19275ab8f235f8823021ffcfcd
Mihail Varbanov <mihail.varbanov@inabyte.com>
2021-06-15 10:41:33 +0300

    Fixed unused import warnings.

--- lib/ui/onboarding/onboarding2/Onboarding2PrivacyStatementPanel.dart
+++ lib/ui/onboarding/onboarding2/Onboarding2PrivacyStatementPanel.dart
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
@@ -26,7 +25,6 @@ import 'package:illinois/ui/onboarding/onboarding2/Onboarding2ExploreCampusPanel
 import 'package:illinois/ui/widgets/ScalableWidgets.dart';
 import 'package:illinois/ui/widgets/SwipeDetector.dart';
 import 'package:illinois/service/Styles.dart';
-import 'package:illinois/utils/Utils.dart';
 
 import 'Onboarding2Widgets.dart';
 
