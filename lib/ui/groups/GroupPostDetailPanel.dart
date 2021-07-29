317e95941b5240e446289aa83d4d04937acd5a80
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2021-07-29 10:12:49 +0300

    Pop when editing post/reply as well [#623]

--- lib/ui/groups/GroupPostDetailPanel.dart
+++ lib/ui/groups/GroupPostDetailPanel.dart
@@ -876,9 +876,7 @@ class _GroupPostDetailPanelState extends State<GroupPostDetailPanel> implements 
   void _onUpdateFinished(bool succeeded) {
     _setLoading(false);
     if (succeeded) {
-      _editingPost = null;
-      _clearBodyControllerContent();
-      _reloadPost();
+      Navigator.of(context).pop(true);
     } else {
       AppAlert.showDialogResult(context, Localization().getStringEx('panel.group.detail.post.update.reply.failed.msg', 'Failed to edit reply.'));
     }
