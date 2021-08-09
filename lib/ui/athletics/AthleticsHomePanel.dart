8ccf6ae9a99894580c9e38e2757352be61849571
Todor Bachvarov <tobachvarov@gmail.com>
2021-08-09 17:30:48 +0300

    Remove "Unlabeled image" pronouncement

--- lib/ui/athletics/AthleticsHomePanel.dart
+++ lib/ui/athletics/AthleticsHomePanel.dart
@@ -618,6 +618,7 @@ class _AthleticsCardState extends State<_AthleticsCard> implements Notifications
                         ? Positioned(
                         child: Image.network(
                           widget.game.imageUrl,
+                          excludeFromSemantics: true,
                         ))
                         : Container(height: 0),
                     showImage
