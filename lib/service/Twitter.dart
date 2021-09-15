57690c33b65fa6bb2ad79eecff629fa246528dd4
Mihail Varbanov <mihail.varbanov@inabyte.com>
2021-09-15 17:39:44 +0300

    Use content BB for retrieving tweets (#749).

--- lib/service/Twitter.dart
+++ lib/service/Twitter.dart
@@ -19,12 +19,6 @@ class Twitter with Service implements NotificationsListener {
 
   static const String _cacheFileName = "twitter.json";
 
-  static const String _tweetFieldsUrlParam = "tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld";
-  static const String _userFieldsUrlParam = "user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld";
-  static const String _mediaFieldsUrlParam = "media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics,non_public_metrics,organic_metrics,promoted_metrics,alt_text";
-  static const String _expansionsUrlParam = "expansions=attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id";
-  static const String _excludeUrlParam = "exclude=retweets,replies";
-
   Tweets        _tweets;
   File          _cacheFile;
   DateTime      _pausedDateTime;
@@ -118,6 +112,22 @@ class Twitter with Service implements NotificationsListener {
     return Tweets.fromJson(AppJson.decodeMap(await _loadContentStringFromCache()));
   }
 
+  Future<String> _loadContentStringFromNet({bool force}) async {
+    if (Config().contentUrl != null) {
+      String url = "${Config().contentUrl}//twitter/posts?count=${Config().twitterTweetsCount * 2}${(force == true) ? '&force=true' : ''}";
+      Response response = await Network().get(url, auth: NetworkAuth.App);
+      return ((response != null) && (response.statusCode == 200)) ? response.body : null;
+    }
+    return null;
+  }
+
+  /*
+  static const String _tweetFieldsUrlParam = "tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld";
+  static const String _userFieldsUrlParam = "user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld";
+  static const String _mediaFieldsUrlParam = "media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics,non_public_metrics,organic_metrics,promoted_metrics,alt_text";
+  static const String _expansionsUrlParam = "expansions=attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id";
+  static const String _excludeUrlParam = "exclude=retweets,replies";
+
   Future<String> _loadContentStringFromNet() async {
     if ((Config().twitterUrl != null) && (Config().twitterUserId != null)) {
       String url = "${Config().twitterUrl}/users/${Config().twitterUserId}/tweets?$_tweetFieldsUrlParam&$_userFieldsUrlParam&$_mediaFieldsUrlParam&$_expansionsUrlParam&$_excludeUrlParam&max_results=${Config().twitterTweetsCount * 2}";
@@ -129,6 +139,7 @@ class Twitter with Service implements NotificationsListener {
     }
     return null;
   }
+  */
 
   Future<void> _updateContentFromNet() async {
     try {
