e82847e467a7419c98dd41b4be7d2672311c7d99
Mihail Varbanov <mihail.varbanov@inabyte.com>
2021-09-16 15:05:55 +0300

    Created HomeTwitter2WidgetState that attempts to implement paging (#759).

--- lib/ui/home/HomeTwitterWidget.dart
+++ lib/ui/home/HomeTwitterWidget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:illinois/model/Twitter.dart';
+import 'package:illinois/service/AppLivecycle.dart';
 import 'package:illinois/service/Config.dart';
 import 'package:illinois/service/NotificationService.dart';
 import 'package:illinois/service/Styles.dart';
@@ -20,10 +21,180 @@ class HomeTwitterWidget extends StatefulWidget {
   HomeTwitterWidget({this.refreshController});
 
   @override
-  _HomeTwitterWidgetState createState() => _HomeTwitterWidgetState();
+  HomeTwitter2WidgetState createState() => HomeTwitter2WidgetState();
 }
 
-class _HomeTwitterWidgetState extends State<HomeTwitterWidget> implements NotificationsListener {
+class HomeTwitter2WidgetState extends State<HomeTwitterWidget> implements NotificationsListener {
+
+  List<TweetsPage> _tweetsPages = <TweetsPage>[];
+  bool _loadingPage = false;
+  DateTime _pausedDateTime;
+
+  @override
+  void initState() {
+    super.initState();
+
+    NotificationService().subscribe(this, [
+      AppLivecycle.notifyStateChanged
+    ]);
+
+
+    if (widget.refreshController != null) {
+      widget.refreshController.stream.listen((_) {
+        _refresh();
+      });
+    }
+
+    _loadingPage = true;
+    Twitter().loadTweetsPage(count: Config().twitterTweetsCount).then((TweetsPage tweetsPage) {
+      if (mounted) {
+        setState(() {
+          _loadingPage = false;
+          if (tweetsPage != null) {
+            _tweetsPages.add(tweetsPage);
+          }
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    NotificationService().unsubscribe(this);
+  }
+
+  // NotificationsListener
+
+  @override
+  void onNotification(String name, dynamic param) {
+    if (name == AppLivecycle.notifyStateChanged) {
+      _onAppLivecycleStateChanged(param);
+    }
+  }
+
+  void _onAppLivecycleStateChanged(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      _pausedDateTime = DateTime.now();
+    }
+    else if (state == AppLifecycleState.resumed) {
+      if (_pausedDateTime != null) {
+        Duration pausedDuration = DateTime.now().difference(_pausedDateTime);
+        if (Config().refreshTimeout < pausedDuration.inSeconds) {
+          _refresh();
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int displayPagesCount = tweetsCount + ((_loadingPage == true) ? 1 : 0);
+    return Visibility(visible: (0 < displayPagesCount), child:
+      Semantics(container: true, child:
+        Column(children: <Widget>[
+          _buildHeader(),
+          Stack(children:<Widget>[
+            _buildSlant(),
+            _buildContent(),
+          ]),
+        ]),
+    ));
+  }
+
+  Widget _buildHeader() {
+    return Container(color: Styles().colors.fillColorPrimary, child:
+      Padding(padding: EdgeInsets.only(left: 20, top: 10, bottom: 10), child:
+        Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Padding(padding: EdgeInsets.only(right: 16), child: Image.asset('images/campus-tools.png')),
+          Expanded(child: 
+            Text("Twitter", style:
+              TextStyle(color: Styles().colors.white, fontFamily: Styles().fontFamilies.extraBold, fontSize: 20,),),),
+      ],),),);
+  }
+
+  Widget _buildSlant() {
+    return Column(children: <Widget>[
+      Container(color:  Styles().colors.fillColorPrimary, height: 45,),
+      Container(color: Styles().colors.fillColorPrimary, child:
+        CustomPaint(painter: TrianglePainter(painterColor: Styles().colors.background, left : true), child:
+          Container(height: 65,),
+        )),
+    ],);
+  }
+
+  Widget _buildContent() {
+    List<Widget> pages = <Widget>[];
+    for (TweetsPage tweetsPage in _tweetsPages) {
+      if (tweetsPage?.tweets != null) {
+        for (Tweet tweet in tweetsPage.tweets) {
+          pages.add(_TweetWidget(tweet: tweet,));
+        }
+      }
+    }
+
+    if (_loadingPage == true) {
+      pages.add(_TweetLoadingWidget());
+    }
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double pageHeight = screenWidth - 20 * 2 + 5;
+    double pageViewport = (screenWidth - 40) / screenWidth;
+    
+    return
+      Padding(padding: EdgeInsets.only(top: 10, bottom: 50), child:
+        Container(height: pageHeight, child:
+          PageView(controller: PageController(viewportFraction: pageViewport), onPageChanged: _onPageChanged, children: pages,)
+        )
+      );
+  }
+
+  int get tweetsCount {
+    int tweetsCount = 0;
+    for (TweetsPage tweetsPage in _tweetsPages) {
+      tweetsCount += (tweetsPage.tweets?.length ?? 0);
+    }
+    return tweetsCount;
+  }
+
+  void _onPageChanged(int index) {
+    if ((tweetsCount <= (index + 1)) && (_loadingPage != true)) {
+      setState(() {
+        _loadingPage = true;
+      });
+      TweetsPage lastTweetsPage = (0 < _tweetsPages.length) ? _tweetsPages.last : null;
+      Tweet lastTweet = ((lastTweetsPage?.tweets != null) && (0 < lastTweetsPage.tweets.length)) ? lastTweetsPage.tweets.last : null;
+      Twitter().loadTweetsPage(count: Config().twitterTweetsCount, endTimeUtc: lastTweet?.createdAtUtc).then((TweetsPage tweetsPage) {
+        if (mounted) {
+          setState(() {
+            _loadingPage = false;
+            if (tweetsPage != null) {
+              _tweetsPages.add(tweetsPage);
+            }
+          });
+        }
+      });
+    }
+  }
+
+  void _refresh() {
+    setState(() {
+      _loadingPage = true;
+    });
+    Twitter().loadTweetsPage(count: tweetsCount).then((TweetsPage tweetsPage) {
+      if (mounted) {
+        setState(() {
+          _loadingPage = false;
+          if (tweetsPage != null) {
+            _tweetsPages = [tweetsPage];
+          }
+        });
+      }
+    });
+  }
+}
+
+class HomeTwitterWidgetState extends State<HomeTwitterWidget> implements NotificationsListener {
 
   List<Tweet> _tweets;
 
@@ -143,7 +314,7 @@ class _TweetWidget extends StatelessWidget {
         ),
         clipBehavior: Clip.none,
         child:
-          Column(children: <Widget>[
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
             
             Expanded(child: 
               SingleChildScrollView(child:
@@ -191,5 +362,17 @@ class _TweetWidget extends StatelessWidget {
         launch(url);
       }
     }
+  }
+}
+
+class _TweetLoadingWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: 
+      SizedBox(height: 24, width: 24, child:
+        CircularProgressIndicator(strokeWidth: 2, valueColor: AlwaysStoppedAnimation<Color>(Styles().colors.fillColorPrimary), )
+      ),
+    );
   }
 }
