3e9ef7437641214ca8ad39d467b40d1fb1d4bf2a
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2021-11-16 15:39:33 +0200

    Handle FCM notifications for athletics news updates [#972]

--- lib/ui/RootPanel.dart
+++ lib/ui/RootPanel.dart
@@ -37,6 +37,7 @@ import 'package:illinois/service/Localization.dart';
 import 'package:illinois/service/NotificationService.dart';
 import 'package:illinois/ui/SavedPanel.dart';
 import 'package:illinois/ui/athletics/AthleticsGameDetailPanel.dart';
+import 'package:illinois/ui/athletics/AthleticsNewsArticlePanel.dart';
 import 'package:illinois/ui/explore/ExplorePanel.dart';
 import 'package:illinois/ui/groups/GroupDetailPanel.dart';
 import 'package:illinois/ui/home/HomePanel.dart';
@@ -103,6 +104,7 @@ class _RootPanelState extends State<RootPanel> with TickerProviderStateMixin imp
       FirebaseMessaging.notifyPopupMessage,
       FirebaseMessaging.notifyEventDetail,
       FirebaseMessaging.notifyAthleticsGameStarted,
+      FirebaseMessaging.notifyAthleticsNewsUpdated,
       FirebaseMessaging.notifyGroupsNotification,
       ExploreService.notifyEventDetail,
       Sports.notifyGameDetail,
@@ -207,6 +209,9 @@ class _RootPanelState extends State<RootPanel> with TickerProviderStateMixin imp
     }
     else if (name == FirebaseMessaging.notifyGroupsNotification) {
       _onFirebaseGroupsNotification(param);
+    }
+    else if (name == FirebaseMessaging.notifyAthleticsNewsUpdated) {
+      _onFirebaseAthleticsNewsNotification(param);
     }
   }
 
@@ -591,6 +596,17 @@ class _RootPanelState extends State<RootPanel> with TickerProviderStateMixin imp
           Navigator.push(context, CupertinoPageRoute(builder: (context) => GroupDetailPanel(group: value)));
         } else {
           AppAlert.showDialogResult(context, Localization().getStringEx("panel.group_detail.label.error_message", "Failed to load group data."));
+        }
+      });
+    }
+  }
+
+  void _onFirebaseAthleticsNewsNotification(param) {
+    if (param is Map<String, dynamic>) {
+      String newsId = param["news_id"];
+      Sports().loadNewsArticle(newsId).then((article) {
+        if (article != null) {
+          Navigator.push(context, CupertinoPageRoute(builder: (context) => AthleticsNewsArticlePanel(article: article)));
         }
       });
     }
