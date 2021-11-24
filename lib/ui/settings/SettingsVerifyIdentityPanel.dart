cf34e981d9e660077c33588988d54412a07f66ed
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2021-11-24 16:41:22 +0200

    Add loading indicator. [#1015]

--- lib/ui/settings/SettingsVerifyIdentityPanel.dart
+++ lib/ui/settings/SettingsVerifyIdentityPanel.dart
@@ -33,6 +33,8 @@ class SettingsVerifyIdentityPanel extends StatefulWidget{
 
 class _SettingsVerifyIdentityPanelState extends State<SettingsVerifyIdentityPanel> {
 
+  bool _loading;
+
   @override
   void initState() {
     super.initState();
@@ -55,65 +57,72 @@ class _SettingsVerifyIdentityPanelState extends State<SettingsVerifyIdentityPane
   }
 
   Widget _buildContent() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(height: 41),
-        Container(padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Text(
-            Localization().getStringEx("panel.settings.verify_identity.label.description", "Connect to Illinois"),
-            style: TextStyle(color: Styles().colors.fillColorPrimary, fontSize: 24, fontFamily: Styles().fontFamilies.extraBold),
-          ),
-        ),
-        Container(height: 8,),
-        Container(padding: EdgeInsets.symmetric(horizontal: 24),
-        child:RichText(
-            text: TextSpan(
-              style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.regular, fontSize: 16),
-              children: <TextSpan>[
-                TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription1", "Are you a ")),
-                TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription2", "Student "),
-                    style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.bold, fontSize: 16)),
-                TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription3", "or ")),
-                TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription4", "faculty member "),
-                    style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.bold, fontSize: 16)),
-                TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription5", "? Log in with your NetID to see Illinois information specific to you, like your Illini Cash and meal plan.")),
-              ],
+    return Stack(alignment: Alignment.center, children: [
+      // Content Widgets
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(height: 41),
+          Container(padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Text(
+              Localization().getStringEx("panel.settings.verify_identity.label.description", "Connect to Illinois"),
+              style: TextStyle(color: Styles().colors.fillColorPrimary, fontSize: 24, fontFamily: Styles().fontFamilies.extraBold),
             ),
-          )
-        ),
-        Container(height: 12,),
-        Container(padding: EdgeInsets.symmetric(horizontal: 16),
-        child:RibbonButton(
-            label: Localization().getStringEx("panel.settings.verify_identity.button.connect_net_id.title", "Connect Your NetID"),
-            borderRadius: BorderRadius.circular(4),
-            onTap: _onTapConnectNetId
-        )),
-        Container(height: 16,),
-        Container(padding: EdgeInsets.symmetric(horizontal: 24),
-            child:RichText(
-              text: TextSpan(
-                style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.regular, fontSize: 16),
-                children: <TextSpan>[
-                  TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.phone_or_email.desription1", "Don’t have a NetID"),
-                      style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.bold, fontSize: 16)),
-                  TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.phone_or_email.desription2", "? Verify your phone number or sign in by email to save your preferences and have the same experience on more than one device.")),
-                ],
-              ),
-            )
-        ),
-        Container(height: 12,),
-        Container(padding: EdgeInsets.symmetric(horizontal: 16),
-            child:RibbonButton(
-                label: Localization().getStringEx("panel.settings.verify_identity.button.phone_or_phone.title", "Proceed"),
-                borderRadius: BorderRadius.circular(4),
-                onTap: _onTapProceed
-            )),
-    ],);
+          ),
+          Container(height: 8,),
+          Container(padding: EdgeInsets.symmetric(horizontal: 24),
+              child:RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.regular, fontSize: 16),
+                  children: <TextSpan>[
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription1", "Are you a ")),
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription2", "Student "),
+                        style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.bold, fontSize: 16)),
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription3", "or ")),
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription4", "faculty member "),
+                        style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.bold, fontSize: 16)),
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.connect_id.desription5", "? Log in with your NetID to see Illinois information specific to you, like your Illini Cash and meal plan.")),
+                  ],
+                ),
+              )
+          ),
+          Container(height: 12,),
+          Container(padding: EdgeInsets.symmetric(horizontal: 16),
+              child:RibbonButton(
+                  label: Localization().getStringEx("panel.settings.verify_identity.button.connect_net_id.title", "Connect Your NetID"),
+                  borderRadius: BorderRadius.circular(4),
+                  onTap: _onTapConnectNetId
+              )),
+          Container(height: 16,),
+          Container(padding: EdgeInsets.symmetric(horizontal: 24),
+              child:RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.regular, fontSize: 16),
+                  children: <TextSpan>[
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.phone_or_email.desription1", "Don’t have a NetID"),
+                        style: TextStyle(color: Styles().colors.textSurface, fontFamily: Styles().fontFamilies.bold, fontSize: 16)),
+                    TextSpan(text: Localization().getStringEx("panel.settings.verify_identity.label.phone_or_email.desription2", "? Verify your phone number or sign in by email to save your preferences and have the same experience on more than one device.")),
+                  ],
+                ),
+              )
+          ),
+          Container(height: 12,),
+          Container(padding: EdgeInsets.symmetric(horizontal: 16),
+              child:RibbonButton(
+                  label: Localization().getStringEx("panel.settings.verify_identity.button.phone_or_phone.title", "Proceed"),
+                  borderRadius: BorderRadius.circular(4),
+                  onTap: _onTapProceed
+              )),
+        ],),
+      // Loading indicator widgets
+      Visibility(visible: (_loading == true), child: CircularProgressIndicator())
+    ]);
   }
 
   void _onTapConnectNetId() {
+    _setLoading(true);
     Auth2().authenticateWithOidc().then((success) {
+      _setLoading(false);
       if (success) {
         _didLogin(context);
       }
@@ -121,12 +130,13 @@ class _SettingsVerifyIdentityPanelState extends State<SettingsVerifyIdentityPane
   }
 
   void _onTapProceed() {
+    _setLoading(true);
     Navigator.push(
         context,
         CupertinoPageRoute(
-            settings: RouteSettings(),
             builder: (context) => Onboarding2LoginPhoneOrEmailPanel(onboardingContext: {
                   "onContinueAction": () {
+                    _setLoading(false);
                     _didLogin(context);
                   }
                 })));
@@ -142,6 +152,15 @@ class _SettingsVerifyIdentityPanelState extends State<SettingsVerifyIdentityPane
         return false;
       }
     });
+  }
+
+  void _setLoading(bool loading) {
+    if (_loading != loading) {
+      _loading = loading;
+      if (mounted) {
+        setState(() {});
+      }
+    }
   }
 
   //TBD consider availability for phone/netId depending on role ()
