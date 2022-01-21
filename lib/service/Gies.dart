2449c2bb1c1d1271cb32eefa085c6869a0f47342
Todor Bachvarov <tobachvarov@gmail.com>
2022-01-21 18:04:54 +0200

    Implement Gies Panel: capable to present page 2 for now

--- lib/service/Gies.dart
+++ lib/service/Gies.dart
@@ -8,7 +8,11 @@ import 'package:rokwire_plugin/utils/utils.dart';
 class Gies with Service implements NotificationsListener{
 
   List<dynamic>? _pages;
+  List<String>?  _navigationPages;
+
+  late Map<int, Set<String>> _progressPages;
   Set<String>? _completedPages;
+  List<int>? _progressSteps;
 
   // Singletone instance
   static final Gies _instance = Gies._internal();
@@ -35,10 +39,13 @@ class Gies with Service implements NotificationsListener{
   @override
   Future<void> initService() async{
     await super.initService();
+    _navigationPages = Storage().giesNavPages ?? [];
     _completedPages = Storage().giesCompletedPages ?? Set<String>();
 
     AppBundle.loadString('assets/gies2.json').then((String? assetsContentString) {
         _pages = JsonUtils.decodeList(assetsContentString);
+        _buildProgressSteps();
+        _ensureNavigationPages();
     });
   }
 
@@ -52,6 +59,59 @@ class Gies with Service implements NotificationsListener{
     //TBD Notifications
   }
 
+  void _buildProgressSteps() {
+    _progressPages = Map<int, Set<String>>();
+    if ((_pages != null) && _pages!.isNotEmpty) {
+      for (dynamic page in _pages!) {
+        if (page is Map) {
+          int? pageProgress = JsonUtils.intValue(page['progress']);
+          if (pageProgress != null) {
+            String? pageId = JsonUtils.stringValue(page['id']);
+            if ((pageId != null) && pageId.isNotEmpty && _pageCanComplete(page)) {
+              Set<String>? progressPages = _progressPages[pageProgress];
+              if (progressPages == null) {
+                _progressPages[pageProgress] = progressPages = Set<String>();
+              }
+              progressPages.add(pageId);
+            }
+          }
+        }
+      }
+      _progressSteps = List.from(_progressPages.keys);
+      _progressSteps!.sort();
+    }
+  }
+
+  void _ensureNavigationPages() {
+    if (_navigationPages!.isEmpty) {
+      String? rootPageId = _navigationRootPageId;
+      if ((rootPageId != null) && rootPageId.isNotEmpty) {
+        Storage().giesNavPages = _navigationPages = [rootPageId];
+      }
+    }
+  }
+
+  Map<String, dynamic>? getPage({String? id, int? progress}) {
+    if (_pages != null) {
+      for (dynamic page in _pages!) {
+        if (page is Map) {
+          if (((id == null) || (id == JsonUtils.stringValue(page['id']))) &&
+              ((progress == null) || (progress == (JsonUtils.intValue(page['progress']) ?? JsonUtils.intValue(page['progress-possition'])))))
+          {
+            try { return page.cast<String, dynamic>(); }
+            catch(e) { print(e.toString()); }
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  bool isProgressStepCompleted(int? progressStep) {
+    Set<String>? progressPages = _progressPages[progressStep];
+    return (progressPages == null) || _completedPages!.containsAll(progressPages);
+  }
+
   List<dynamic>? get pages{
     return _pages;
   }
@@ -60,4 +120,58 @@ class Gies with Service implements NotificationsListener{
     return _completedPages;
   }
 
+  List<dynamic>? get navigationPages{
+    return _navigationPages;
+  }
+
+  Map<int, Set<String>> get progressPages{
+    return _progressPages;
+  }
+
+  List<int>? get progressSteps{
+    return _progressSteps;
+  }
+
+  String? get currentPageId {
+    Map<String, dynamic> page = _pages?[1] ?? {};
+    return page["id"];
+    // return (_navigationPages?.isNotEmpty?? false) ? _navigationPages!.last : null;// TBD Implement properly
+  }
+
+  String? get _navigationRootPageId {
+    if ((_pages != null) && _pages!.isNotEmpty) {
+      for (dynamic page in _pages!) {
+        if (page is Map) {
+          String? pageId = JsonUtils.stringValue(page['id']);
+          if (pageId != null) {
+            return pageId;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  //Utils
+  static bool _pageCanComplete(Map? page) {
+    List<dynamic>? buttons = (page != null) ? JsonUtils.listValue(page['buttons']) : null;
+    if (buttons != null) {
+      for (dynamic button in buttons) {
+        if ((button is Map) && _pageButtonCompletes(button)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  static bool _pageButtonCompletes(Map button) {
+    return JsonUtils.boolValue(button['completes']) == true;
+  }
+
+  static int? getPageProgress(Map<String, dynamic>? page) {
+    return (page != null) ? (JsonUtils.intValue(page['progress']) ?? JsonUtils.intValue(page['progress-possition'])) : null;
+  }
+
+  //TBD Notify when data changed
 }
