46103f231f82fe2921892bf365d1558f7b86861c
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-02-07 13:57:47 +0200

    Removed rokwire plugin files from main repo (#1203)

--- plugin/ios/Classes/RokwirePlugin.h
+++
@@ -1,6 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface RokwirePlugin : NSObject<FlutterPlugin>
-+ (instancetype)sharedInstance;
-- (void)notifyGeoFenceEvent:(NSString*)event arguments:(id)arguments;
-@end
