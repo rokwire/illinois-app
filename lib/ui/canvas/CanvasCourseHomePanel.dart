998e48922575ae129f5b83d3e48031b9a6c54c07
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2022-02-08 15:41:35 +0200

    Feature/issue 1274 (#1317)
    * Started Canvas model (#998).
    
    * Resolve merge errors.
    
    * Load courses [#1274]
    
    * Add Canvas to the Service list [#1274]
    
    * Introduce "HomeCanvasCoursesWidget" - horizontal scroll with the courses of the users [#1274]
    
    * Show home widget "Canvas Courses" to "gies" role [#1274]
    
    * Format content only (no changes) [#1274]
    
    * UI adjustments in course card [#1274]
    
    * Introduce empty CanvasCourseHomePanel. [#1274]
    
    * Extract CanvasCourseCard as widget. Reuse it in CanvasCoursesHomePanel [#1274]
    
    * Initial version of CanvasCourseHomePanel [#1274]
    
    * Prevent crash - check for null values.
    
    * Adjust the UI of CanvasCourseCard [#1274]
    
    * Introduce "CanvasCourseSyllabusPanel" [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Fix typos in CHANGELOG.md
    
    * Load group by canvas course id. Show it in CanvasCourseHomePanel if exists. TBD: sync with backend team [#1274]
    
    * Add "Syllabus" button to "Syllabus" panel. [#1274]
    
    * Do not add non-needed widgets. [#1274]
    
    * Load course in Syllabus panel
    
    * Add possibility for including content when loading course. Currently only syllabus_body [#1274]
    
    * Localization.
    
    * Load Syllabus html body [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Rename method name. [#1274]
    
    * Model for Canvas files and folders [#1274]
    
    * Load files and folders per course [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Announcements data model and GET API call [#1274]
    
    * Add Copyright heading [#1274]
    
    * Draw Canvas Announcements panel [#1274]
    
    * Draw Announcement panel [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Implement Canvas Files [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Remove unused API calls [#1274]
    
    * Canvas Collaborations - data model, service call and UI [#1274]
    
    * CanvasCalendarEvent data model and API call [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Fix loading syllabus html body [#1274]
    
    * Cnavas Calendar - simple UI. Task is in progress [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Canvas Calendar: Differ selected and today date [#1274]
    
    * Canvas Calendar: Fix changing selected date [#1274]
    
    * Canvas Calendar: Handle swipe left and right actions to change visible week [#1274]
    
    * Canvas Calendar: properly load events for the current selected month [#1274]
    
    * Canvas Calendar: display events in the calendar [#1274]
    
    * Canvas Calendar: Display only events that are not hidden [#1274]
    
    * Fix overflowing [#1274]
    
    * Canvas Clendar: limit events to 50 per page [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Canvas calendar: fix display dates - use device local time zone [#1274]
    
    * Canvas: fix the other dates to display device local time [#1274]
    
    * Canvas: implement account notifications - data model and UI [#1274]
    
    * Canvas: Implement Calendar event detail panel [#1274]
    
    * Add "Detail" suffix to panels [#1274]
    
    * Fix CHANGELOG.md file again.
    
    * Update CHANGELOG.md [#1274]
    
    * Fix text overflowing [#1274]
    
    * Canvas: icons with different colors for implemented/working features [#1274]
    
    * Introduce Canvas Modules data model and service calls [#1274]
    
    * Introduce CanvasCourseModulesPanel [#1274]
    
    * Introduce CanvasModuleDetailPanel [#1274]
    
    * Use "implemented and working" image for modules. [#1274]
    
    * Canvas modules - handle "Assignment" types [#1274]
    
    * Sort Canvas modules and module items by position [#1274]
    
    * Canvas Assignments data model in progress [#1274]
    
    * Update CHANGELOG.md [#1274]
    
    * Canvas: change "Inbox" to "Notification history" [#1274]
    
    * Canvas: Move all items from Syllabus sub panel and place them on Course Home panel [#1274]
    
    * Canvas: add Group button and redirct to GroupsHomePanel [#1274]
    
    * Canvas: test commit - add missing delimiter. [#1274]
    
    * Canvas Calendar: fix overflowing for small displays [#1274]
    
    * Canvas Course: fix header cut off [#1274]
    
    * Canvas: Implement Feedback - report error [#1274]
    
    * Update CHANGELOG.md [#1274]

--- lib/ui/canvas/CanvasCourseHomePanel.dart
+++ lib/ui/canvas/CanvasCourseHomePanel.dart
@@ -18,6 +18,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:illinois/model/Canvas.dart';
 import 'package:illinois/ui/canvas/CanvasAccountNotificationsPanel.dart';
+import 'package:illinois/ui/canvas/CanvasCourseModulesPanel.dart';
+import 'package:illinois/ui/canvas/CanvasFeedbackPanel.dart';
+import 'package:illinois/ui/canvas/CanvasSyllabusHtmlPanel.dart';
+import 'package:illinois/ui/groups/GroupsHomePanel.dart';
 import 'package:rokwire_plugin/model/group.dart';
 import 'package:illinois/service/Analytics.dart';
 import 'package:illinois/service/Canvas.dart';
@@ -25,7 +29,6 @@ import 'package:rokwire_plugin/service/groups.dart';
 import 'package:illinois/ui/canvas/CanvasCourseAnnouncementsPanel.dart';
 import 'package:illinois/ui/canvas/CanvasCourseCalendarPanel.dart';
 import 'package:illinois/ui/canvas/CanvasCourseCollaborationsPanel.dart';
-import 'package:illinois/ui/canvas/CanvasCourseSyllabusPanel.dart';
 import 'package:illinois/ui/canvas/CanvasFileSystemEntitiesListPanel.dart';
 import 'package:illinois/ui/canvas/CanvasWidgets.dart';
 import 'package:illinois/ui/groups/GroupWidgets.dart';
@@ -116,7 +119,7 @@ class _CanvasCourseHomePanelState extends State<CanvasCourseHomePanel> {
       RibbonButton(
           label: Localization().getStringEx('panel.home_canvas_course.button.syllabus.title', 'Syllabus'),
           hint: Localization().getStringEx('panel.home_canvas_course.button.syllabus.hint', ''),
-          leftIcon: 'images/icon-canvas-partially-implemented.png',
+          leftIcon: 'images/icon-canvas-implemented-working.png',
           onTap: _onTapSyllabus),
       _buildDelimiter(),
       RibbonButton(
@@ -150,11 +153,44 @@ class _CanvasCourseHomePanelState extends State<CanvasCourseHomePanel> {
           onTap: _onTapNotifications),
       _buildDelimiter(),
       RibbonButton(
-          label: Localization().getStringEx('panel.home_canvas_course.button.inbox.title', 'Inbox'),
-          hint: Localization().getStringEx('panel.home_canvas_course.button.inbox.hint', ''),
+          label: Localization().getStringEx('panel.home_canvas_course.button.notitification.history.title', 'Notification history'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.notitification.history.hint', ''),
           leftIcon: 'images/icon-canvas-implemented-working.png',
-          onTap: _onTapInbox),
+          onTap: _onTapNotificationHistory),
       _buildDelimiter(),
+      RibbonButton(
+          label: Localization().getStringEx('panel.home_canvas_course.button.modules.title', 'Modules'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.modules.hint', ''),
+          leftIcon: 'images/icon-canvas-implemented-working.png',
+          onTap: _onTapModules),
+      _buildDelimiter(),
+      RibbonButton(
+          label: Localization().getStringEx('panel.home_canvas_course.button.assignments.title', 'Assignments'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.assignments.hint', ''),
+          leftIcon: 'images/icon-settings.png'),
+      _buildDelimiter(),
+      RibbonButton(
+          label: Localization().getStringEx('panel.home_canvas_course.button.zoom.title', 'Zoom Meeting'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.zoom.hint', ''),
+          leftIcon: 'images/icon-settings.png'),
+      _buildDelimiter(),
+      RibbonButton(
+          label: Localization().getStringEx('panel.home_canvas_course.button.grades.title', 'Grades'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.grades.hint', ''),
+          leftIcon: 'images/icon-settings.png'),
+      _buildDelimiter(),
+      RibbonButton(
+          label: Localization().getStringEx('panel.home_canvas_course.button.group.title', 'Group'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.group.hint', ''),
+          leftIcon: 'images/icon-canvas-implemented-working.png',
+          onTap: _onTapGroup),
+      _buildDelimiter(),
+      RibbonButton(
+          label: Localization().getStringEx('panel.home_canvas_course.button.feedback.title', 'Feedback'),
+          hint: Localization().getStringEx('panel.home_canvas_course.button.feedback.hint', ''),
+          leftIcon: 'images/icon-canvas-implemented-working.png',
+          onTap: _onTapFeedback),
+      _buildDelimiter()
     ]);
   }
 
@@ -163,38 +199,53 @@ class _CanvasCourseHomePanelState extends State<CanvasCourseHomePanel> {
   }
 
   void _onTapSyllabus() {
-    Analytics().logSelect(target: "Canvas Course -> Syllabus");
-    Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasCourseSyllabusPanel(courseId: widget.courseId!)));
+    Analytics().logSelect(target: 'Canvas Course -> Syllabus');
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasSyllabusHtmlPanel(courseId: widget.courseId!)));
   }
 
   void _onTapAnnouncements() {
-    Analytics().logSelect(target: "Canvas Course -> Announcements");
+    Analytics().logSelect(target: 'Canvas Course -> Announcements');
     Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasCourseAnnouncementsPanel(courseId: widget.courseId!)));
   }
 
   void _onTapFiles() {
-    Analytics().logSelect(target: "Canvas Course -> Files");
+    Analytics().logSelect(target: 'Canvas Course -> Files');
     Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasFileSystemEntitiesListPanel(courseId: widget.courseId)));
   }
 
   void _onTapCollaborations() {
-    Analytics().logSelect(target: "Canvas Course -> Collaborations");
+    Analytics().logSelect(target: 'Canvas Course -> Collaborations');
     Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasCourseCollaborationsPanel(courseId: widget.courseId!)));
   }
 
   void _onTapCalendar() {
-    Analytics().logSelect(target: "Canvas Course -> Calendar");
+    Analytics().logSelect(target: 'Canvas Course -> Calendar');
     Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasCourseCalendarPanel(courseId: widget.courseId!)));
   }
 
   void _onTapNotifications() {
-    Analytics().logSelect(target: "Canvas Course -> Notifications");
+    Analytics().logSelect(target: 'Canvas Course -> Notifications');
     Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasAccountNotificationsPanel()));
   }
 
-  void _onTapInbox() {
-    Analytics().logSelect(target: "Canvas Course -> Inbox");
+  void _onTapNotificationHistory() {
+    Analytics().logSelect(target: 'Canvas Course -> Notification history');
     Navigator.push(context, CupertinoPageRoute(builder: (context) => InboxHomePanel()));
+  }
+
+  void _onTapModules() {
+    Analytics().logSelect(target: 'Canvas Course -> Modules');
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasCourseModulesPanel(courseId: widget.courseId!)));
+  }
+
+  void _onTapGroup() {
+    Analytics().logSelect(target: 'Canvas Course -> Group');
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => GroupsHomePanel()));
+  }
+
+  void _onTapFeedback() {
+    Analytics().logSelect(target: 'Canvas Course -> Feedback');
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => CanvasFeedbackPanel()));
   }
 
   void _loadCourse() {
