707c856462ae1069c663f41d43e326e08e9c3342
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-02-16 16:04:35 +0200

    TabBar widget moved to Rokwire plugin (#1325).

--- lib/ui/widgets/TabBarWidget.dart
+++ lib/ui/widgets/TabBarWidget.dart
@@ -14,321 +14,95 @@
  * limitations under the License.
  */
 
-import 'dart:math';
-
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:illinois/service/Analytics.dart';
-import 'package:rokwire_plugin/service/config.dart';
-import 'package:illinois/service/FlexUI.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:illinois/ui/wallet/WalletSheet.dart';
-import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/ui/widgets/tabbar_widget.dart' as rokwire;
 
-class TabBarWidget extends StatefulWidget {
+class TabBarWidget extends rokwire.TabBarWidget {
 
   static const String notifySelectionChanged = "edu.illinois.rokwire.tabbar_widget.selection.changed";
 
-  final TabController? tabController;
   final bool? walletExpanded;
 
-  TabBarWidget({this.tabController, this.walletExpanded});
-
-  _TabBarWidgetState createState() => _TabBarWidgetState();
-}
-
-class _TabBarWidgetState extends State<TabBarWidget>  implements NotificationsListener {
-
-  List<dynamic>? _contentListCodes;
+  TabBarWidget({Key? key, TabController? tabController, this.walletExpanded}) : super(key: key, tabController: tabController);
 
   @override
-  void initState() {
-    super.initState();
-    NotificationService().subscribe(this, FlexUI.notifyChanged);
-    widget.tabController?.addListener(_onTabControllerChanged);
-    _contentListCodes = _getContentListCodes();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    NotificationService().unsubscribe(this);
-    widget.tabController?.removeListener(_onTabControllerChanged);
-  }
-
-  // NotificationsListener
-
-  @override
-  void onNotification(String name, dynamic param) {
-    if (name == FlexUI.notifyChanged) {
-      _updateContentListCodes();
+  Widget? buildTab(BuildContext context, String code, int index) {
+    if ((code == 'home') || (code == 'athletics')) {
+      return rokwire.TabWidget(
+        label: Localization().getStringEx('tabbar.home.title', 'Home'),
+        hint: Localization().getStringEx('tabbar.home.hint', ''),
+        iconAsset: 'images/tab-home.png',
+        selectedIconAsset: 'images/tab-home-selected.png',
+        selected: (tabController?.index == index),
+        onTap: (rokwire.TabWidget tabWidget) => _onSwitchTab(index, tabWidget),
+      );
+    }
+    else if (code == 'explore') {
+      return rokwire.TabWidget(
+        label: Localization().getStringEx('tabbar.explore.title', 'Explore'),
+        hint: Localization().getStringEx('tabbar.explore.hint', ''),
+        iconAsset: 'images/tab-explore.png',
+        selectedIconAsset: 'images/tab-explore-selected.png',
+        selected: (tabController?.index == index),
+        onTap: (rokwire.TabWidget tabWidget) => _onSwitchTab(index, tabWidget),
+      );
+    }
+    else if (code == 'wallet') {
+      return (walletExpanded != true) ?
+        rokwire.TabWidget(
+          label: Localization().getStringEx('tabbar.wallet.title', 'Wallet'),
+          hint: Localization().getStringEx('tabbar.wallet.hint', ''),
+          iconAsset: 'images/tab-wallet.png',
+          selected: false,
+          onTap: (rokwire.TabWidget tabWidget) => _onShowWalletSheet(context, tabWidget),
+        ) :
+        rokwire.TabCloseWidget(
+          label: Localization().getStringEx('panel.wallet.button.close.title', 'close'),
+          hint: Localization().getStringEx('panel.wallet.button.close.hint', ''),
+          iconAsset: 'images/icon-close-big.png',
+          onTap: (rokwire.TabCloseWidget tabCloseWidget) => _onCloseWalletSheet(context, tabCloseWidget),
+        );
+    }
+    else if (code == 'browse') {
+      return rokwire.TabWidget(
+        label: Localization().getStringEx('tabbar.browse.title', 'Browse'),
+        hint: Localization().getStringEx('tabbar.browse.hint', ''),
+        iconAsset: 'images/tab-browse.png',
+        selectedIconAsset: 'images/tab-browse-selected.png',
+        selected: (tabController?.index == index),
+        onTap: (rokwire.TabWidget tabWidget) => _onSwitchTab(index, tabWidget),
+      );
+    }
+    else {
+      return null;
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(mainAxisSize: MainAxisSize.min, children: [
-      Container(decoration: BoxDecoration(color: backgroundColor, border: border), child:
-        SafeArea(child:
-            Row(children: 
-              _buildTabs(),
-            ),
-        ),
-      ),
-    ],);
-  }
-
-  @protected
-  Color? get backgroundColor {
-    switch(Config().configEnvironment) {
-      case ConfigEnvironment.dev:        return Colors.yellowAccent;
-      case ConfigEnvironment.test:       return Colors.lightGreenAccent;
-      case ConfigEnvironment.production: return Colors.white;
-      default:                           return Colors.white;
-    }
-  }
-
-  @protected
-  BoxBorder? get border => Border(top: BorderSide(color: Styles().colors!.surfaceAccent!, width: 1, style: BorderStyle.solid));
-
-  List<Widget> _buildTabs() {
-    List<Widget> tabs = [];
-    int tabsCount = (_contentListCodes != null) ? _contentListCodes!.length : 0;
-    for (int tabIndex = 0; tabIndex < tabsCount; tabIndex++) {
-      String code = _contentListCodes![tabIndex];
-      if ((code == 'home') || (code == 'athletics')) {
-        tabs.add(Expanded(
-          child: TabWidget(
-            label: Localization().getStringEx('tabbar.home.title', 'Home'),
-            hint: Localization().getStringEx('tabbar.home.hint', ''),
-            iconAsset: 'images/tab-home.png',
-            selectedIconAsset: 'images/tab-home-selected.png',
-            selected: (widget.tabController?.index == tabIndex),
-            onTap: (TabWidget tabWidget) => _onSwitchTab(tabIndex, tabWidget),
-          ),
-        ));
-      }
-      else if (code == 'explore') {
-        tabs.add(Expanded(
-          child: TabWidget(
-            label: Localization().getStringEx('tabbar.explore.title', 'Explore'),
-            hint: Localization().getStringEx('tabbar.explore.hint', ''),
-            iconAsset: 'images/tab-explore.png',
-            selectedIconAsset: 'images/tab-explore-selected.png',
-            selected: (widget.tabController?.index == tabIndex),
-            onTap: (TabWidget tabWidget) => _onSwitchTab(tabIndex, tabWidget),
-          )
-        ));
-      }
-      else if (code == 'wallet') {
-        if (widget.walletExpanded != true) {
-          tabs.add(Expanded(
-            child: TabWidget(
-              label: Localization().getStringEx('tabbar.wallet.title', 'Wallet'),
-              hint: Localization().getStringEx('tabbar.wallet.hint', ''),
-              iconAsset: 'images/tab-wallet.png',
-              selected: false,
-              onTap: (TabWidget tabWidget) => _onShowWalletSheet(tabWidget),
-            )
-          ));
-        }
-        else {
-          tabs.add(Expanded(
-            child: TabCloseWidget(
-              label: Localization().getStringEx('panel.wallet.button.close.title', 'close'),
-              hint: Localization().getStringEx('panel.wallet.button.close.hint', ''),
-              iconAsset: 'images/icon-close-big.png',
-              onTap: _onCloseWalletSheet,
-            )
-          ));
-        }
-      }
-      else if (code == 'browse') {
-        tabs.add(Expanded(
-          child: TabWidget(
-            label: Localization().getStringEx('tabbar.browse.title', 'Browse'),
-            hint: Localization().getStringEx('tabbar.browse.hint', ''),
-            iconAsset: 'images/tab-browse.png',
-            selectedIconAsset: 'images/tab-browse-selected.png',
-            selected: (widget.tabController?.index == tabIndex),
-            onTap: (TabWidget tabWidget) => _onSwitchTab(tabIndex, tabWidget),
-          ),
-        ));
-      }
-    }
-    return tabs;
-  }
-
-  void _onTabControllerChanged(){
-    setState(() {});
-  }
-
-  void _onSwitchTab(int tabIndex, TabWidget tabWidget) {
+  void _onSwitchTab(int tabIndex, rokwire.TabWidget tabWidget) {
     Analytics().logSelect(target: tabWidget.label);
     NotificationService().notify(TabBarWidget.notifySelectionChanged, tabIndex);
   }
 
-  void _onShowWalletSheet(TabWidget tabWidget) {
+  void _onShowWalletSheet(BuildContext context, rokwire.TabWidget tabWidget) {
     Analytics().logSelect(target: tabWidget.label);
     showModalBottomSheet(context: context,
-        isScrollControlled: true,
-        isDismissible: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24.0),
-        ),
-        builder: (context){
-          return WalletSheet();
-        }
+      isScrollControlled: true,
+      isDismissible: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24.0),
+      ),
+      builder: (context){
+        return WalletSheet();
+      }
     );
   }
 
-  void _onCloseWalletSheet(TabCloseWidget tabCloseWidget) {
+  void _onCloseWalletSheet(BuildContext context, rokwire.TabCloseWidget tabCloseWidget) {
     Analytics().logSelect(target: tabCloseWidget.label);
     Navigator.pop(context);
   }
-
-  List<String>? _getContentListCodes() {
-    try {
-      dynamic tabsList = FlexUI()['tabbar'];
-      return (tabsList is List) ? tabsList.cast<String>() : null;
-    }
-    catch(e) {
-      print(e.toString());
-    }
-    return null;
-  }
-
-  void _updateContentListCodes() {
-    List<String>? contentListCodes = _getContentListCodes();
-    if ((contentListCodes != null) && !DeepCollectionEquality().equals(_contentListCodes, contentListCodes)) {
-      if (mounted) {
-        setState(() {
-          _contentListCodes = contentListCodes;
-        });
-      }
-    }
-  }
-}
-
-class TabWidget extends StatelessWidget {
-  final String? label;
-  final String? hint;
-  final String? iconAsset;
-  final String? selectedIconAsset;
-  final bool selected;
-  final void Function(TabWidget tabWidget) onTap;
-
-  TabWidget(
-      {this.label,
-      this.iconAsset,
-      this.selectedIconAsset,
-      this.hint,
-      this.selected = false,
-      required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(onTap: () => onTap(this), behavior: HitTestBehavior.translucent, child:
-      Stack(children: <Widget>[
-        buildTab(context),
-        selected ? buildSelectedIndicator(context) : Container(),
-        ],
-      ),
-    );
-  }
-
-  // Tab
-
-  @protected
-  Widget buildTab(BuildContext context) => Center(child:
-    Semantics(label: label, hint: hint, excludeSemantics: true, child:
-      Padding(padding: tabPadding, child:
-        Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Padding(padding: tabIconPadding, child:
-            getTabIcon(context)
-          ),
-          Padding(padding: tabTextPadding, child:
-            getTabText(context)
-          ),
-        ],),
-      ),
-    ),
-  );
-
-  @protected
-  EdgeInsetsGeometry get tabPadding => EdgeInsets.only(top: 10);
-
-  @protected
-  EdgeInsetsGeometry get tabIconPadding => EdgeInsets.only(bottom: 4);
-
-  @protected
-  EdgeInsetsGeometry get tabTextPadding => EdgeInsets.all(0);
-
-  @protected
-  TextAlign get tabTextAlign => TextAlign.center;
-
-  @protected
-  TextStyle get tabTextStyle => TextStyle(fontFamily: Styles().fontFamilies!.bold, color: selected ? Styles().colors!.fillColorSecondary : Styles().colors!.mediumGray, fontSize: 12);
-
-  @protected
-  double getTextScaleFactor(BuildContext context) => min(MediaQuery.of(context).textScaleFactor, 2);
-
-  @protected
-  Widget getTabText(BuildContext context) => Row(children: [
-    Expanded(child:
-      Text(label ?? '', textScaleFactor: getTextScaleFactor(context), textAlign: tabTextAlign, style: tabTextStyle,),
-    )
-  ]);
-
-  @protected
-  Widget getTabIcon(BuildContext context)  {
-    String? asset = selected ? (selectedIconAsset ?? iconAsset) : iconAsset;
-    return (asset != null) ? Image.asset(asset, width: tabIconSize.width, height: tabIconSize.height) : Container(width: tabIconSize.width, height: tabIconSize.height);
-  }
-
-  @protected
-  Size get tabIconSize => Size(20, 20);
-
-  // Selected Indicator
-
-  @protected
-  Widget buildSelectedIndicator(BuildContext context) => Positioned.fill(child:
-    Column(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-      Container(height: selectedIndicatorHeight, color: selectedIndicatorColor)
-    ],),
-  );
-
-  @protected
-  double get selectedIndicatorHeight => 4;
-
-  @protected
-  Color? get selectedIndicatorColor => Styles().colors?.fillColorSecondary;
-
-}
-
-class TabCloseWidget extends StatelessWidget {
-  final String? label;
-  final String? hint;
-  final String iconAsset;
-  final void Function(TabCloseWidget tabWidget) onTap;
-
-  TabCloseWidget({
-    this.label,
-    this.hint,
-    required this.iconAsset,
-    required this.onTap
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Semantics(label: label, hint: hint, button: true, child:
-      GestureDetector(onTap: () => onTap(this), behavior: HitTestBehavior.translucent, child:
-        Center(child:
-          Image.asset(iconAsset, excludeFromSemantics: true,),
-        ),
-      )
-    );
-  }
-
 }
