2f090ce11ae5920707e93e300550bcbf7d164220
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2022-02-24 11:17:25 +0200

    Draw the new widgets in WellnessAnswerCenter [#1364]

--- lib/ui/home/HomeSaferWellnessAnswerCenterPanel.dart
+++ lib/ui/home/HomeSaferWellnessAnswerCenterPanel.dart
@@ -1,123 +1,69 @@
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:illinois/service/Config.dart';
+import 'package:illinois/service/DeepLink.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/styles.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:rokwire_plugin/utils/utils.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class HomeSaferWellnessAnswerCenterPanel extends StatelessWidget{
-
+class HomeSaferWellnessAnswerCenterPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Styles().colors!.background,
-      appBar: HeaderBar(
-        title: Localization().getStringEx("panel.home.safer.wellness_answer_center.header.title", "Answer Center"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(14),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                Localization().getStringEx("panel.home.safer.wellness_answer_center.label.description", "If you are having issues with the app or getting a test result, contact the Wellness Answer Center for assistance."),
-                style: TextStyle(
-                  fontFamily: Styles().fontFamilies!.regular,
-                  fontSize: 16,
-                  color: Styles().colors!.textSurface
-                ),
-              ),
-              Container(height: 20,),
-              RichText(
-                textScaleFactor: MediaQuery.textScaleFactorOf(context),
-                textAlign: TextAlign.start,
-                text: TextSpan(
-                  text: Localization().getStringEx("panel.home.safer.wellness_answer_center.label.email", "Email the Wellness Answer Center at "),
-                  style: TextStyle(
-                      fontFamily: Styles().fontFamilies!.regular,
-                      fontSize: 16,
-                      color: Styles().colors!.textSurface
-                  ),
-                  children: [
-                    TextSpan(
-                      text: this.displayEmail,
-                      style: TextStyle(
-                          fontFamily: Styles().fontFamilies!.regular,
-                          fontSize: 16,
-                          color: Styles().colors!.fillColorSecondary,
-                          decoration: TextDecoration.underline,
-                      ),
-                      recognizer: TapGestureRecognizer()..onTap = ()=>onEmailTapped(),
-                    ),
-                  ]
-                ),
-              ),
-              Container(height: 20,),
-              RichText(
-                textScaleFactor: MediaQuery.textScaleFactorOf(context),
-                textAlign: TextAlign.start,
-                text: TextSpan(
-                    text: Localization().getStringEx("panel.home.safer.wellness_answer_center.label.phone", "Phone the Answer Center at "),
-                    style: TextStyle(
-                        fontFamily: Styles().fontFamilies!.regular,
-                        fontSize: 16,
-                        color: Styles().colors!.textSurface
-                    ),
-                    children: [
-                      TextSpan(
-                        text: this.displayPhone,
-                        style: TextStyle(
-                          fontFamily: Styles().fontFamilies!.regular,
-                          fontSize: 16,
-                          color: Styles().colors!.fillColorSecondary,
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()..onTap = ()=>onPhoneTapped(),
-                      ),
-                    ]
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+        backgroundColor: Styles().colors!.background,
+        appBar: HeaderBar(title: Localization().getStringEx("panel.home.safer.wellness_answer_center.header.title", "Answer Center")),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.all(14),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                  _buildEntryCard(
+                      title: Localization()
+                          .getStringEx('panel.home.safer.wellness_answer_center.contacts.title', 'Contacts for Covid-19 Questions'),
+                      description: Localization().getStringEx('panel.home.safer.wellness_answer_center.contacts.description',
+                          'Find the right department for your particular question regarding Covid-19 rules, testing, software, etc.'),
+                      onTapEntry: _onTapContacts),
+                  Container(height: 14),
+                  _buildEntryCard(
+                      title: Localization().getStringEx('panel.home.safer.wellness_answer_center.faqs.title', 'Building Access FAQs'),
+                      description: Localization().getStringEx('panel.home.safer.wellness_answer_center.faqs.description',
+                          'See answers to common questions about building access.'),
+                      onTapEntry: _onTapFaqs)
+                ]))));
   }
 
-  String get displayEmail {
-    return Config().saferWellness['email']  ?? '';
+  Widget _buildEntryCard({required String title, required String description, GestureTapCallback? onTapEntry}) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Styles().colors!.white,
+            boxShadow: [BoxShadow(color: Styles().colors!.blackTransparent018!, spreadRadius: 1.0, blurRadius: 3.0, offset: Offset(1, 1))],
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(4))),
+        clipBehavior: Clip.none,
+        child: Stack(children: [
+          GestureDetector(
+              onTap: onTapEntry,
+              child: Semantics(
+                  button: true,
+                  child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                        Padding(
+                            padding: EdgeInsets.only(right: 17),
+                            child: Text(StringUtils.ensureNotEmpty(title),
+                                style: TextStyle(
+                                    color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.extraBold, fontSize: 20))),
+                        Container(height: 8),
+                        Text(StringUtils.ensureNotEmpty(description),
+                            style:
+                                TextStyle(color: Styles().colors!.textBackground, fontFamily: Styles().fontFamilies!.regular, fontSize: 16))
+                      ])))),
+          Container(color: Styles().colors!.accentColor3, height: 4)
+        ]));
   }
 
-  void onEmailTapped(){
-    String? email = Config().saferWellness['email'];
-    if (StringUtils.isNotEmpty(email)) {
-      launch('mailto:$email');
-    }
+  void _onTapContacts() {
+    //TBD implement
   }
 
-  String get displayPhone {
-    String? displayPhone;
-    String? phone = Config().saferWellness['phone'];
-    if (StringUtils.isNotEmpty(phone)) {
-      displayPhone = phone;
-      if (displayPhone!.startsWith('+1')) {
-        displayPhone = displayPhone.substring(2);
-      }
-      if (displayPhone.length == 10) {
-        displayPhone = '${displayPhone.substring(0, 3)} ${displayPhone.substring(3, 6)}-${displayPhone.substring(6)}';
-      }
-    }
-    return displayPhone ?? '';
-  }
-
-  void onPhoneTapped(){
-    String? phone = Config().saferWellness['phone'];
-    if (StringUtils.isNotEmpty(phone)) {
-      launch('tel:$phone');
-    }
+  void _onTapFaqs() {
+    //TBD implement
   }
 }
