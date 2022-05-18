d9dae50b8a4ab3d2c7b71e2bb8c0b59198f09899
Todor Bachvarov <tobachvarov@gmail.com>
2022-05-18 17:08:33 +0300

    PollBubblePromptPanel close prompt after voting for single vote polls

--- lib/ui/polls/PollBubblePromptPanel.dart
+++ lib/ui/polls/PollBubblePromptPanel.dart
@@ -483,6 +483,10 @@ class _PollContentState extends State<PollContentWidget> implements Notification
             _votingOptions.remove(optionIndex);
           }
         }
+        if(!_allowMultipleOptions && !_allowRepeatOptions){
+          //We only want to see the 2nd panel for multi voting. If a Poll is a single vote poll then when a user votes close the panel.
+          _onClose();
+        }
       });
     });
   }
