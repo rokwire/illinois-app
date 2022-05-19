f551e8f5430ba791c0a2f35634f7455bdf2c9326
Todor Bachvarov <tobachvarov@gmail.com>
2022-05-19 16:45:56 +0300

    Introduce HomeRadioWidget UI only

--- lib/ui/home/HomePanel.dart
+++ lib/ui/home/HomePanel.dart
@@ -31,6 +31,7 @@ import 'package:illinois/ui/home/HomeIlliniCashWidget.dart';
 import 'package:illinois/ui/home/HomeIlliniIdWidget.dart';
 import 'package:illinois/ui/home/HomeLibraryCardWidget.dart';
 import 'package:illinois/ui/home/HomeMealPlanWidget.dart';
+import 'package:illinois/ui/home/HomeRadioWidget.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:rokwire_plugin/model/event.dart';
 import 'package:rokwire_plugin/model/inbox.dart';
@@ -173,7 +174,10 @@ class _HomePanelState extends State<HomePanel> with AutomaticKeepAliveClientMixi
       else if (code == 'safer') {
         widget = saferWidget = _saferWidget ??= HomeSaferWidget(key: _saferKey);
       }
-      
+      else if (code == 'radio') {
+        widget = HomeRadioWidget();
+      }
+
       // Wallet Cards
 
       else if (code == 'illini_cash_card') {
