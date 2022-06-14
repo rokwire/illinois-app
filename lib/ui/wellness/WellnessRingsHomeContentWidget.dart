21d2c26a5f07a7d3d689c86893f99cb04d6f1fec
Todor Bachvarov <tobachvarov@gmail.com>
2022-06-14 17:39:12 +0300

    Store wellness data to file cache

--- lib/ui/wellness/WellnessRingsHomeContentWidget.dart
+++ lib/ui/wellness/WellnessRingsHomeContentWidget.dart
@@ -14,11 +14,14 @@
  * limitations under the License.
  */
 
+import 'dart:io';
+
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
-import 'package:illinois/service/Storage.dart';
 import 'package:illinois/utils/AppUtils.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:rokwire_plugin/service/log.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/service.dart';
@@ -318,7 +321,7 @@ class _WellnessRingState extends State<WellnessRing> with TickerProviderStateMix
           WellnessRingData? data = WellnessRingService().wellnessRings
               ?.firstWhere((element) => element.id == param);
           if (data != null) {
-            AppAlert.showCustomDialog(context: context, contentPadding: EdgeInsets.all(0),
+            AppAlert.showCustomDialog(context: this.context, contentPadding: EdgeInsets.all(0),
               contentWidget:
                 Container(
                     child: Column(
@@ -342,7 +345,7 @@ class _WellnessRingState extends State<WellnessRing> with TickerProviderStateMix
                                 Container(
                                   padding: EdgeInsets.only(left: 50, bottom: 10),
                                   child: GestureDetector(
-                                  onTap: () => Navigator.of(context).pop(),
+                                  onTap: () => Navigator.of(this.context).pop(),
                                   child: Text("x", style :TextStyle(color: Styles().colors!.textSurface!, fontFamily: Styles().fontFamilies!.regular, fontSize: 22),),
                                 )),
                               ],
@@ -448,7 +451,7 @@ class _WellnessRingButtonState extends State<WellnessRingButton>{
 
   Widget get _rightIcon{
     return GestureDetector(
-      onTap: (){ if (widget.onTapRightWidget!=null) widget.onTapRightWidget!(context);},
+      onTap: (){ if (widget.onTapRightWidget!=null) widget.onTapRightWidget!(this.context);},
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: Image.asset('images/icon-gear.png', excludeFromSemantics: true, color:  Styles().colors!.white!),
@@ -625,6 +628,8 @@ class WellnessRingRecord {
 class WellnessRingService with Service{
   static const String notifyUserRingsUpdated = "edu.illinois.rokwire.wellness.user.ring.updated";
   static const String notifyUserRingsAccomplished = "edu.illinois.rokwire.wellness.user.ring.accomplished";
+
+  static const String _cacheFileName = "wellness.json";
   static const int MAX_RINGS = 4;
   static const List<dynamic> predefinedRings = [
     {'name': "Hobby", 'goal': 10, 'color': 'FFF57C00' , 'id': "id_0", 'unit':'session'},
@@ -641,6 +646,8 @@ class WellnessRingService with Service{
     WellnessRingRecord(value: 1, timestamp: DateTime.now().millisecondsSinceEpoch, wellnessRingId: "id_1"),
     WellnessRingRecord(value: 1, timestamp: DateTime.now().millisecondsSinceEpoch, wellnessRingId: "id_2"),
   ];
+
+  File? _cacheFile;
 
   List<WellnessRingData>? _wellnessRings;
   List<WellnessRingRecord>? _wellnessRecords; //TBD implement its mocced for now
@@ -660,20 +667,62 @@ class WellnessRingService with Service{
   WellnessRingService.internal();
 
   @override
-  Future<void> initService() {
-    _loadFromStorage();
+  Future<void> initService() async {
+    _cacheFile = await _getCacheFile();
+    await _initFromCache();
     _loadFromNet();
     return super.initService();
   }
 
-  void _loadFromStorage(){
+  Future<void> _initFromCache() async{
+    _loadContentJsonFromCache().then((Map<String, dynamic>? storedValues) {
+        // _wellnessRings = storedValues?["wellness_rings_data"] ?? [];
+        _wellnessRecords = WellnessRingRecord.listFromJson(storedValues?["wellness_ring_records"] ?? []);
+      }
+    );
     _wellnessRings =  WellnessRingData.listFromJson(predefinedRings);//Storage().userWellnessRings; //TBD implement from file //TBD Moc for now
-    _wellnessRecords = []; //_mocWellnessRecords; //TBD Implement from file //TBD Moc for now
+    // _wellnessRecords = []; //_mocWellnessRecords; //TBD Implement from file //TBD Moc for now
   }
   
   void _loadFromNet(){
     //TBD network API
-    _storeWellnessRings();
+    // _storeWellnessRingData();
+  }
+
+  //Cashe
+  Future<File> _getCacheFile() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String cacheFilePath = join(appDocDir.path, _cacheFileName);
+    return File(cacheFilePath);
+  }
+
+  Future<String?> _loadContentStringFromCache() async {
+    return (await _cacheFile?.exists() == true) ? await _cacheFile?.readAsString() : null;
+  }
+
+  Future<void> _saveRingsDataToCache() async{
+    String? data = JsonUtils.encode({
+      "wellness_rings_data": _wellnessRings,
+      "wellness_ring_records": _wellnessRecords,
+    });
+
+    return _saveContentStringToCache(data);
+  }
+
+  Future<void> _saveContentStringToCache(String? value) async {
+    try {
+      if (value != null) {
+        await _cacheFile?.writeAsString(value, flush: true);
+      }
+      else {
+        await _cacheFile?.delete();
+      }
+    }
+    catch(e) { print(e.toString()); }
+  }
+
+  Future<Map<String, dynamic>?> _loadContentJsonFromCache() async {
+    return JsonUtils.decodeMap(await _loadContentStringFromCache());
   }
   
   void addRing(WellnessRingData data) async {
@@ -685,7 +734,7 @@ class WellnessRingService with Service{
       _wellnessRings!.add(data);
     }
     NotificationService().notify(notifyUserRingsUpdated);
-    _storeWellnessRings();
+    _storeWellnessRingData();
   }
   
   void updateRing(WellnessRingData data) async {
@@ -695,7 +744,7 @@ class WellnessRingService with Service{
       ringData.updateFromOther(data);
     }
     NotificationService().notify(notifyUserRingsUpdated);
-    _storeWellnessRings();
+    _storeWellnessRingData();
   }
   
   void removeRing(WellnessRingData data) async {
@@ -705,7 +754,7 @@ class WellnessRingService with Service{
        _wellnessRings?.remove(ringData);
     }
     NotificationService().notify(notifyUserRingsUpdated);
-    _storeWellnessRings();
+    _storeWellnessRingData();
   }
 
   void addRecord(WellnessRingRecord record){
@@ -717,10 +766,13 @@ class WellnessRingService with Service{
     if(alreadyAccomplished == false) {
       _checkForAccomplishment(record.wellnessRingId);
     }
+    _storeWellnessRecords();
   }
 
   double getRingDailyValue(String wellnessRingId){
-    Iterable<WellnessRingRecord>? selection = _wellnessRecords?.where((record) => record.wellnessRingId == wellnessRingId);
+    Iterable<WellnessRingRecord>? selection = _wellnessRecords?.where((record) =>
+      ((record.wellnessRingId == wellnessRingId)
+        && ((DateTimeUtils.midnight(DateTime.now())?.millisecondsSinceEpoch ?? 0) < record.timestamp)));
         // ?.where((record) => ((DateTimeUtils.midnight(DateTime.now())?.millisecondsSinceEpoch ?? 0) < record.timestamp));// Today records
 
     double value = 0.0;
@@ -744,13 +796,17 @@ class WellnessRingService with Service{
     return (_wellnessRings?.length ?? 0) < MAX_RINGS;
   }
   
-  void _storeWellnessRings(){
-    Storage().userWellnessRings = _wellnessRings;
+  void _storeWellnessRingData(){
+    _saveRingsDataToCache();
+  }
+
+  void _storeWellnessRecords(){
+    _saveRingsDataToCache();
   }
 
   Future<List<WellnessRingData>?> getWellnessRings() async {
     if(_wellnessRings == null){ //TBD REMOVE workaround while we are not added to the Services
-      _loadFromStorage();
+      _initFromCache();
     }
     return _wellnessRings; //TBD load from net
   }
@@ -794,7 +850,7 @@ class WellnessRingService with Service{
     return count;
   }
 
-  void _splitRecordsByDay(){
+  void splitRecordsByDay(){
 
   }
   //Other way to split use for dayly completions
