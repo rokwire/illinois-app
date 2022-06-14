f3158dcdf6d99f03966d8c2ba1e5b2a5a34a0f87
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-06-14 09:38:09 +0300

    HomeFavoritesWidget: removed Show More/Less, added See All instead (#1727).

--- lib/service/Config.dart
+++ lib/service/Config.dart
@@ -157,8 +157,9 @@ class Config extends rokwire.Config {
   String? get canvasAssignmentDeepLinkFormat => JsonUtils.stringValue(canvasDeepLink['assignment_format']);
 
   // Getters: settings
-  int  get recentItemsCount    => JsonUtils.intValue(settings['recentItemsCount']) ?? 32;
-  String get appPrivacyVersion => JsonUtils.stringValue(settings['privacyVersion']) ?? (JsonUtils.stringValue(content['mobileAppVersion']) ?? '0.0.0');
+  int  get homeFavoriteItemsCount => JsonUtils.intValue(settings['homeFavoriteItemsCount']) ?? 3;
+  int  get recentItemsCount       => JsonUtils.intValue(settings['recentItemsCount']) ?? 32;
+  String get appPrivacyVersion    => JsonUtils.stringValue(settings['privacyVersion']) ?? (JsonUtils.stringValue(content['mobileAppVersion']) ?? '0.0.0');
 
   @override
   int get refreshTimeout=> kReleaseMode ? super.refreshTimeout : 0;
