ee46c881b2a32fa26dbf76554fa9b962efde0071
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-06-14 10:10:32 +0300

    HomeRecentItemsWidget: added See All (#1727).

--- lib/service/Config.dart
+++ lib/service/Config.dart
@@ -160,6 +160,7 @@ class Config extends rokwire.Config {
   int  get homeCampusHighlightsCount => JsonUtils.intValue(settings['homeCampusHighlightsCount']) ?? 3;
   int  get homeCampusRemindersCount  => JsonUtils.intValue(settings['homeCampusRemindersCount']) ?? 3;
   int  get homeFavoriteItemsCount    => JsonUtils.intValue(settings['homeFavoriteItemsCount']) ?? 3;
+  int  get homeRecentItemsCount      => JsonUtils.intValue(settings['homeRecentItemsCount']) ?? 3;
   int  get recentItemsCount          => JsonUtils.intValue(settings['recentItemsCount']) ?? 32;
   String get appPrivacyVersion       => JsonUtils.stringValue(settings['privacyVersion']) ?? (JsonUtils.stringValue(content['mobileAppVersion']) ?? '0.0.0');
 
