952de6d6a697d8a6f2d5fdb824218476c774683d
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-06-16 10:55:01 +0300

    Created HomeAthleticsNewsWidget (#1752).

--- lib/service/Config.dart
+++ lib/service/Config.dart
@@ -160,6 +160,7 @@ class Config extends rokwire.Config {
   int  get homeCampusHighlightsCount => JsonUtils.intValue(settings['homeCampusHighlightsCount']) ?? 3;
   int  get homeCampusRemindersCount  => JsonUtils.intValue(settings['homeCampusRemindersCount']) ?? 3;
   int  get homeFavoriteItemsCount    => JsonUtils.intValue(settings['homeFavoriteItemsCount']) ?? 3;
+  int  get homeAthleticsNewsCount    => JsonUtils.intValue(settings['homeAthleticsNewsCount']) ?? 3;
   int  get homeRecentItemsCount      => JsonUtils.intValue(settings['homeRecentItemsCount']) ?? 3;
   int  get homeUpcomingEventsCount   => JsonUtils.intValue(settings['homeUpcomingEventsCount']) ?? 5;
   int  get recentItemsCount          => JsonUtils.intValue(settings['recentItemsCount']) ?? 32;
