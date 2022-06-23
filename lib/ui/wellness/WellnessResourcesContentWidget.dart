da22f49f6bf8d899867dc4455826f9d39c8782b0
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-06-23 14:57:48 +0300

    Load wellness resource content from assets (#1816).

--- lib/ui/wellness/WellnessResourcesContentWidget.dart
+++ lib/ui/wellness/WellnessResourcesContentWidget.dart
@@ -15,9 +15,16 @@
  */
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:illinois/service/Analytics.dart';
+import 'package:illinois/service/DeepLink.dart';
+import 'package:illinois/ui/WebPanel.dart';
 import 'package:illinois/ui/home/HomeWidgets.dart';
+import 'package:rokwire_plugin/service/assets.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WellnessResourcesContentWidget extends StatefulWidget {
   WellnessResourcesContentWidget();
@@ -27,6 +34,23 @@ class WellnessResourcesContentWidget extends StatefulWidget {
 }
 
 class _WellnessResourcesContentWidgetState extends State<WellnessResourcesContentWidget> {
+
+  List<dynamic>? _commands;
+  Map<String, dynamic>? _strings;
+
+  @override
+  void initState() {
+    Map<String, dynamic>? content = JsonUtils.mapValue(Assets()['wellness.resources']) ;
+    _commands = (content != null) ? JsonUtils.listValue(content['commands']) : null;
+    _strings = (content != null) ? JsonUtils.mapValue(content['strings']) : null;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return _buildContent();
@@ -35,8 +59,8 @@ class _WellnessResourcesContentWidgetState extends State<WellnessResourcesConten
   Widget _buildContent() {
     return Padding(padding: EdgeInsets.symmetric(horizontal: 16), child: Column(children: [
       _buildHeader(),
-      _buildActionButtonsContainer(),
-      _buildResourceButtonsContainer(),
+      _buildLargeButtonsContainer(),
+      _buildRegularButtonsContainer(),
     ]));
   }
 
@@ -50,98 +74,123 @@ class _WellnessResourcesContentWidgetState extends State<WellnessResourcesConten
         ]));
   }
 
-  Widget _buildActionButtonsContainer() {
+  Widget _buildLargeButtonsContainer() {
     List<Widget> widgetList = <Widget>[];
-    widgetList.add(_buildActionButton(
-        label: Localization().getStringEx('panel.wellness.resources.appointment.button', 'I want to make an appointment')));
-    widgetList.add(_buildActionButton(
-        label:
-            Localization().getStringEx('panel.wellness.resources.help_person.button', 'I want to help a friend, student, or a co-worker')));
-    widgetList.add(
-        _buildActionButton(label: Localization().getStringEx('panel.wellness.resources.not_sure.button', "I'm not sure where to start")));
+    if (_commands != null) {
+      for (dynamic entry in _commands!) {
+        Map<String, dynamic>? command = JsonUtils.mapValue(entry);
+        if (command != null) {
+          String? type = JsonUtils.stringValue(command['type']);
+          if (type == 'large') {
+            widgetList.add(_buildLargeButton(
+              label: _getString(JsonUtils.stringValue(command['id'])),
+              hasExternalLink: UrlUtils.isWebScheme(JsonUtils.stringValue(command['url'])),
+              onTap: () => _onCommand(command),
+            ));
+          }
+        }
+      }
+    }
+    
     return Column(children: widgetList);
   }
 
-  Widget _buildActionButton({required String label}) {
-    return Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Container(
-            decoration: BoxDecoration(
-                color: Styles().colors!.white,
-                border: Border.all(color: Styles().colors!.surfaceAccent!, width: 1),
-                borderRadius: BorderRadius.circular(5)),
-            child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Expanded(
-                      child: Padding(
-                          padding: EdgeInsets.only(left: 15),
-                          child: Text(label,
-                              style: TextStyle(
-                                  color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.bold, fontSize: 18)))),
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: Image.asset('images/external-link.png', color: Styles().colors!.mediumGray)),
-                    HomeFavoriteStar(selected: false, style: HomeFavoriteStyle.Button, padding: EdgeInsets.only(left: 7, right: 15))
-                  ])
-                ]))));
+  Widget _buildLargeButton({String? label, bool hasExternalLink = false, void Function()? onTap}) {
+    return Padding(padding: EdgeInsets.only(bottom: 10), child:
+      Container(decoration: BoxDecoration(color: Styles().colors!.white, border: Border.all(color: Styles().colors!.surfaceAccent!, width: 1), borderRadius: BorderRadius.circular(5)), child:
+        InkWell(onTap: onTap, child:
+          Padding(padding: EdgeInsets.only(left: 16), child:
+            Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Expanded(child:
+                Padding(padding: EdgeInsets.symmetric(vertical: 16), child:
+                  Text(label ?? '', style: TextStyle(color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.bold, fontSize: 18)),
+                ),
+              ),
+              hasExternalLink ? Padding(padding: EdgeInsets.only(left: 6, top: 18, bottom: 18), child:
+                Image.asset('images/external-link.png', color: Styles().colors!.mediumGray)
+              ) : Container(),
+              HomeFavoriteButton(style: HomeFavoriteStyle.Button, padding: EdgeInsets.only(left: 8, right: 16, top: 16, bottom: 16),)
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 
-  Widget _buildResourceButtonsContainer() {
+  Widget _buildRegularButtonsContainer() {
     List<Widget> widgetList = <Widget>[];
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.24_hour.button', '24-Hour Resources'), isExternalLink: false));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.accessibility.button', 'Accessibility and Accommodations')));
-    widgetList
-        .add(_buildResourceButton(label: Localization().getStringEx('panel.wellness.resources.after_hours.button', 'After Hours Support')));
-    widgetList.add(
-        _buildResourceButton(label: Localization().getStringEx('panel.wellness.resources.campus_service.button', 'Campus Service Units')));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.core_campus_resources.button', 'Core Campus Resources'),
-        isExternalLink: false));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.financial_assistance.button', 'Financial Assistance')));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.fitness_recreations.button', 'Fitness and Recreations')));
-    widgetList.add(
-        _buildResourceButton(label: Localization().getStringEx('panel.wellness.resources.food_nutrition.button', 'Food and Nutrition')));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.health_counselling.button', 'Mental Health and Counselling')));
-    widgetList.add(
-        _buildResourceButton(label: Localization().getStringEx('panel.wellness.resources.nature_culture.button', 'Nature and Culture')));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.other_resources.button', 'Other Wellness Resources')));
-    widgetList.add(
-        _buildResourceButton(label: Localization().getStringEx('panel.wellness.resources.sexual_misconduct.button', 'Sexual Misconduct')));
-    widgetList.add(_buildResourceButton(
-        label: Localization().getStringEx('panel.wellness.resources.events_workshops.button', 'Wellness Events & Workshops')));
-    return Column(children: widgetList);
+    if (_commands != null) {
+      for (dynamic entry in _commands!) {
+        Map<String, dynamic>? command = JsonUtils.mapValue(entry);
+        if (command != null) {
+          String? type = JsonUtils.stringValue(command['type']);
+          if (type == 'regular') {
+            if (widgetList.isNotEmpty) {
+              widgetList.add(Divider(color: Styles().colors!.surfaceAccent, height: 1,));
+            }
+            widgetList.add(_buildRegularButton(
+              label: _getString(JsonUtils.stringValue(command['id'])),
+              hasExternalLink: UrlUtils.isWebScheme(JsonUtils.stringValue(command['url'])),
+              onTap: () => _onCommand(command),
+            ));
+          }
+        }
+      }
+    }
+
+    return Container(decoration: BoxDecoration(color: Styles().colors!.white, border: Border.all(color: Styles().colors!.surfaceAccent!, width: 1), borderRadius: BorderRadius.circular(5)), child:
+    Column(children: widgetList));
+
   }
 
-  Widget _buildResourceButton({required String label, bool isExternalLink = true, void Function()? onTap}) {
-    return GestureDetector(
-        onTap: onTap,
-        child: Container(
-            color: Styles().colors!.white,
-            child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  HomeFavoriteStar(selected: false, style: HomeFavoriteStyle.Button, padding: EdgeInsets.symmetric(horizontal: 10)),
-                  Expanded(
-                      child: Text(label,
-                          style:
-                              TextStyle(color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.bold, fontSize: 16))),
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Visibility(
-                        visible: isExternalLink,
-                        child: Padding(
-                            padding: EdgeInsets.only(left: 7),
-                            child: Image.asset('images/external-link.png', color: Styles().colors!.mediumGray))),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 10), child: Image.asset('images/chevron-right.png'))
-                  ])
-                ]))));
+  Widget _buildRegularButton({String? label, bool hasExternalLink = true, void Function()? onTap}) {
+    return InkWell(onTap: onTap, child:
+      Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        HomeFavoriteButton(style: HomeFavoriteStyle.Button, padding: EdgeInsets.only(left: 16, right: 8, top: 16, bottom: 16)),
+        Expanded(child:
+          Padding(padding: EdgeInsets.symmetric(vertical: 17), child:
+            Text(label ?? '', style: TextStyle(color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.bold, fontSize: 16))
+          ),
+        ),
+        hasExternalLink ? Padding(padding: EdgeInsets.only(left: 8, top: 18, bottom: 18), child:
+          Image.asset('images/external-link.png', color: Styles().colors!.mediumGray)
+        ) : Container(),
+        Padding(padding: EdgeInsets.only(left: 8, right: 16, top: 18, bottom: 18), child:
+          Image.asset('images/chevron-right.png')
+        ),
+      ]),
+    );
   }
 
+  String? _getString(String? key, {String? languageCode}) {
+    if ((_strings != null) && (key != null)) {
+      Map<String, dynamic>? mapping =
+        JsonUtils.mapValue(_strings![languageCode]) ??
+        JsonUtils.mapValue(_strings![Localization().currentLocale?.languageCode]) ??
+        JsonUtils.mapValue(_strings![Localization().defaultLocale?.languageCode]);
+      if (mapping != null) {
+        return JsonUtils.stringValue(mapping[key]);
+      }
+    }
+    return null;
+  }
+
+  void _onCommand(Map<String, dynamic> command) {
+    Analytics().logSelect(target: _getString(JsonUtils.stringValue(command['id']), languageCode: Localization().defaultLocale?.languageCode),);
+    _launchUrl(JsonUtils.stringValue(command['url']));
+  }
+
+  void _launchUrl(String? url) {
+    if (StringUtils.isNotEmpty(url)) {
+      if (DeepLink().isAppUrl(url)) {
+        DeepLink().launchUrl(url);
+      }
+      else if (UrlUtils.launchInternal(url)){
+        Navigator.push(context, CupertinoPageRoute(builder: (context) => WebPanel(url: url)));
+      }
+      else{
+        launch(url!);
+      }
+    }
+  }
 }
