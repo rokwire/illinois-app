2d8378be65bc73541d820948e6994185ff49899d
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2022-07-05 14:06:57 +0300

    Allow possibility for loading predefined group post templates [#1877]

--- lib/ui/groups/GroupPostCreatePanel.dart
+++ lib/ui/groups/GroupPostCreatePanel.dart
@@ -24,6 +24,8 @@ class _GroupPostCreatePanelState extends State<GroupPostCreatePanel>{
   static final double _outerPadding = 16;
 
   PostDataModel _postData = PostDataModel();
+  List<GroupPostTemplate>? _postTemplates;
+  GroupPostTemplate? _selectedTemplate;
   bool _loading = false;
   //Refresh
   GlobalKey _postImageHolderKey = GlobalKey();
@@ -34,6 +36,7 @@ class _GroupPostCreatePanelState extends State<GroupPostCreatePanel>{
   void initState() {
     super.initState();
     _initAllMembersAllowedToPost();
+    _loadPostTemplates();
   }
 
   @override
@@ -73,6 +76,7 @@ class _GroupPostCreatePanelState extends State<GroupPostCreatePanel>{
                 children: [
                   Container(height: 12,),
                   GroupMembersSelectionWidget(allMembers: _allMembersAllowedToPost, selectedMembers: _selectedMembers, groupId: widget.group?.id, onSelectionChanged: _onMembersSelectionChanged),
+                  _buildTemplatesWidget(),
                   Container(height: 12,),
                   Text(Localization().getStringEx('panel.group.detail.post.create.subject.label', 'Subject'),
                     style: TextStyle(
@@ -82,6 +86,7 @@ class _GroupPostCreatePanelState extends State<GroupPostCreatePanel>{
                   Padding(
                     padding: EdgeInsets.only(top: 8, bottom: _outerPadding),
                     child: TextField(
+                      controller: TextEditingController(text: _postData.subject),
                       onChanged: (msg)=> _postData.subject = msg,
                       maxLines: 1,
                       decoration: InputDecoration(
@@ -129,11 +134,72 @@ class _GroupPostCreatePanelState extends State<GroupPostCreatePanel>{
     );
   }
 
+  Widget _buildTemplatesWidget() {
+    // Do not show the templates for regular members
+    if (!(widget.group?.currentUserIsAdmin ?? false)) {
+      return Container();
+    }
+    if (CollectionUtils.isEmpty(_postTemplates)) {
+      return Container();
+    }
+    return Padding(
+        padding: EdgeInsets.only(bottom: 12),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(Localization().getStringEx('panel.group.detail.post.create.templates.label', 'Templates'),
+              style: TextStyle(fontSize: 18, fontFamily: Styles().fontFamilies!.bold, color: Styles().colors!.fillColorPrimary)),
+          Padding(
+              padding: EdgeInsets.only(top: 5),
+              child: Container(
+                  height: 48,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Styles().colors!.mediumGray!, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: DropdownButtonHideUnderline(
+                          child: DropdownButton<GroupPostTemplate?>(
+                              icon: Icon(Icons.arrow_drop_down, color: Styles().colors!.fillColorSecondary),
+                              isExpanded: true,
+                              style: TextStyle(
+                                  fontFamily: Styles().fontFamilies!.regular, fontSize: 16, color: Styles().colors!.textBackground),
+                              items: _templatesDropDownItems,
+                              value: _selectedTemplate,
+                              onChanged: _onTemplateChanged)))))
+        ]));
+  }
+
   void _onMembersSelectionChanged(List<Member>? selectedMembers){
     if(mounted) {
       setState(() {
         _selectedMembers = selectedMembers;
       });
+    }
+  }
+
+  List<DropdownMenuItem<GroupPostTemplate?>> get _templatesDropDownItems {
+    List<DropdownMenuItem<GroupPostTemplate?>> items = [];
+    if (CollectionUtils.isNotEmpty(_postTemplates)) {
+      for (GroupPostTemplate template in _postTemplates!) {
+        items.add(DropdownMenuItem(value: template, child: Text(StringUtils.ensureNotEmpty(template.subject))));
+      }
+    }
+    items.add(DropdownMenuItem(
+        value: null, child: Text(Localization().getStringEx('panel.group.detail.post.create.templates.custom.label', 'Custom'))));
+    return items;
+  }
+
+  void _onTemplateChanged(GroupPostTemplate? template) {
+    _selectedTemplate = template;
+    String? subject;
+    String? body;
+    if (_selectedTemplate != null) {
+      subject = _selectedTemplate?.subject;
+      body = _selectedTemplate?.body;
+    }
+    _postData.subject = subject;
+    _postData.body = body;
+    if (mounted) {
+      setState(() {});
     }
   }
 
@@ -189,6 +255,17 @@ class _GroupPostCreatePanelState extends State<GroupPostCreatePanel>{
   void _initAllMembersAllowedToPost(){
     if((widget.group?.members?.length ?? 0) >0) {
       _allMembersAllowedToPost = widget.group!.members!.where((member) => _isMemberAllowedToReceivePost(member)).toList();
+    }
+  }
+
+  void _loadPostTemplates() {
+    // Load post templates only for admins
+    if (widget.group?.currentUserIsAdmin ?? false) {
+      _setLoading(true);
+      Groups().loadPostTemplates(groupName: StringUtils.ensureNotEmpty(widget.group?.title)).then((templates) {
+        _postTemplates = templates;
+        _setLoading(false);
+      });
     }
   }
 
