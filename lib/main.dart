a17599de3c180db4ac87ba586ba2f42e243fcd76
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-09-09 15:16:05 +0300

    Fixed warning [#2380].

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@
  */
 
 import 'dart:async';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/foundation.dart';
