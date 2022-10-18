75abed68513bd9a03a5ae02d68709a12038bd2a4
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-10-18 18:04:43 +0300

    Prepare demographics questionary content loading [#2465].

--- lib/ui/onboarding2/Onboarding2DemographicsQuestionnairePanel.dart
+++ lib/ui/onboarding2/Onboarding2DemographicsQuestionnairePanel.dart
@@ -2,9 +2,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:illinois/service/Analytics.dart';
+import 'package:illinois/ui/onboarding/OnboardingBackButton.dart';
 import 'package:illinois/utils/AppUtils.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
 
 class Onboarding2DemographicsQuestionnairePanel extends StatefulWidget {
 
@@ -36,13 +40,106 @@ class Onboarding2DemographicsQuestionnairePanel extends StatefulWidget {
 
 class _Onboarding2DemographicsQuestionnairePanelState extends State<Onboarding2DemographicsQuestionnairePanel> {
 
+  bool _loading = false;
+  Map<String, dynamic>? _questionnaire;
+
   @override
   void initState() {
+    _loading = true;
+    _loadQuestionnaire().then((Map<String, dynamic>? questionnaire) {
+      if (mounted) {
+        setState(() {
+          _loading = false;
+          _questionnaire = questionnaire;
+        });
+      }
+    });
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  void dispose() {
+    super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles().colors?.background,
+      body: SafeArea(child:
+        Stack(children: [
+          OnboardingBackButton(padding: const EdgeInsets.only(left: 10, top: 30, right: 20, bottom: 20), onTap: () {
+            Analytics().logSelect(target: "Back");
+            Navigator.pop(context);
+          }),
+          _buildContent(),
+        ],)
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    if (_loading) {
+      return _buildLoading();
+    }
+    else if (_questionnaire == null) {
+      return _buildError();
+    }
+    else {
+      return _buildQuestionnaire();
+    }
+  }
+    
+  Widget _buildQuestionnaire() {
+    return Column(children: [
+      Padding(padding: EdgeInsets.only(left: 45, right: 45, top: 20), child:
+        Semantics(label: 'Demographics Questionnaire', hint: '', excludeSemantics: true, child:
+          Row(children: [
+            Expanded(child:
+              Text('Demographics Questionnaire', style: TextStyle(fontFamily: Styles().fontFamilies?.extraBold, fontSize: 24, color: Styles().colors?.fillColorPrimary), textAlign: TextAlign.center,),
+            )
+          ],)
+        ),
+      )
+    ],);
+  }
+
+  Widget _buildLoading() {
+    return Row(children: [
+      Expanded(child:
+        Column(children: [
+          Expanded(child:
+            Align(alignment: Alignment.center, child:
+              CircularProgressIndicator(color: Styles().colors?.fillColorSecondary, strokeWidth: 3, )
+            ),
+          ),
+        ],))
+    ]);
+  }
+
+  Widget _buildError() {
+    return Row(children: [
+      Expanded(child:
+        Column(children: [
+          Expanded(child:
+            Align(alignment: Alignment.center, child:
+              Padding(padding: EdgeInsets.only(left: 32, right: 32, top: 24, bottom: 24), child:
+                Row(children: [
+                  Expanded(child:
+                    Text('Failed to load demographics questionnaire.', style: TextStyle(fontFamily: Styles().fontFamilies?.medium, fontSize: 20, color: Styles().colors?.fillColorPrimary), textAlign: TextAlign.center,),
+                  ),
+                ],)
+              )
+            ),
+          ),
+        ],))
+    ]);
+  }
+
+  Future<Map<String, dynamic>?> _loadQuestionnaire() async {
+    try { return JsonUtils.decodeMap(await rootBundle.loadString('assets/questionnaire.demographics.json')); }
+    catch(e) { debugPrint(e.toString()); }
+    return null;
+  }
+  
 }
