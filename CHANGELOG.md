adaba3096147a8bb01ac2e93752c94f2992f99f5
Mihail Varbanov <mihail.varbanov@inabyte.com>
2022-11-16 15:42:56 +0200

    Make the MTD route lines thicker [#2536].

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## Unreleased
 ### Changed
 - Research Project updates [#2533](https://github.com/rokwire/illinois-app/issues/2533).
+- Make the MTD route lines thicker [#2536](https://github.com/rokwire/illinois-app/issues/2536).
 
 ## [4.2.19] - 2022-11-15
 ### Added
--- lib/ui/mtd/MTDStopDeparturesPanel.dart
+++ lib/ui/mtd/MTDStopDeparturesPanel.dart
@@ -214,7 +214,7 @@ class _MTDStopDeparturesPanelState extends State<MTDStopDeparturesPanel> {
   Widget _buildRoute(MTDRoute route) {
     return Container(decoration: BoxDecoration(color: route.color, border: Border.all(color: route.textColor ?? Colors.transparent, width: 1), borderRadius: BorderRadius.circular(5)), child:
       Padding(padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2), child:
-        Text(route.shortName ?? '', overflow: TextOverflow.ellipsis, style: TextStyle(fontFamily: Styles().fontFamilies!.medium, fontSize: 12, color: route.textColor,)),
+        Text(route.shortName ?? '', overflow: TextOverflow.ellipsis, style: TextStyle(fontFamily: Styles().fontFamilies!.extraBold, fontSize: 12, color: route.textColor,)),
       )
     );
   }
