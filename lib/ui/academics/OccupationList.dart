7215cdd2c49f68af3eb161d7c0a2ecd65e9d7cab
Apoorva Aditya <apoorva9@illinois.edu>
2023-02-06 21:36:27 -0600

    Created the OccupationList screen

---
+++ lib/ui/academics/OccupationList.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OccupationList extends StatelessWidget {
+  const OccupationList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
