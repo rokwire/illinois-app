c088ad214507e7d01eaeaef1827bc218a0be4bc0
Apoorva Aditya <apoorva9@illinois.edu>
2023-02-07 21:20:29 -0600

    Built search bar and list view for matched occupations screen

--- lib/ui/academics/OccupationList.dart
+++ lib/ui/academics/OccupationList.dart
@@ -1,10 +1,115 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:illinois/model/occupation/Occupation.dart';
+import 'package:illinois/ui/widgets/HeaderBar.dart';
 
 class OccupationList extends StatelessWidget {
-  const OccupationList({Key? key}) : super(key: key);
+  OccupationList({Key? key}) : super(key: key);
+
+  final List<Occupation> occupations = [
+    Occupation(
+      name: 'Software Developer',
+      description: 'Die from segmentation fault',
+      matchPercentage: 75.0,
+      onetLink: '',
+      skills: [],
+    ),
+    Occupation(
+      name: 'Architect',
+      description: 'Build Minecraft Structures IRL',
+      matchPercentage: 20.0,
+      onetLink: '',
+      skills: [],
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: HeaderBar(
+        title: 'Matched Occupations',
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.list)),
+        ],
+      ),
+      body: Column(
+        children: [
+          _buildSearchBar(),
+          Expanded(
+            child: _buildOccupationListView(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Row(
+      children: [
+        Icon(Icons.search),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              label: Text('Search for specific job'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOccupationListView() {
+    return ListView.builder(
+      itemBuilder: (context, index) => OccupationListTile(
+        occupation: occupations[index],
+      ),
+      itemCount: occupations.length,
+    );
+  }
+}
+
+class OccupationListTile extends StatelessWidget {
+  const OccupationListTile({Key? key, required this.occupation}) : super(key: key);
+
+  final Occupation occupation;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: OccupationMatchCircle(
+        matchPercentage: occupation.matchPercentage ?? 100.0,
+      ),
+      title: Text(occupation.name.toString()),
+      subtitle: Text(occupation.description.toString()),
+      trailing: Icon(Icons.chevron_right),
+      onTap: () {},
+    );
+  }
+}
+
+class OccupationMatchCircle extends StatelessWidget {
+  const OccupationMatchCircle({Key? key, required this.matchPercentage}) : super(key: key);
+
+  final double matchPercentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(math.pi),
+          child: CircularProgressIndicator(
+            strokeWidth: 8.0,
+            color: Color.lerp(Colors.red, Colors.green, matchPercentage / 100.0),
+            value: matchPercentage / 100,
+          ),
+        ),
+        Positioned.fill(
+          child: Center(child: Text(matchPercentage.toInt().toString())),
+        ),
+      ],
+    );
   }
 }
