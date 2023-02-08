5ede3923a6ec33cb3badbc65d263a84fa70967fb
Mihail Varbanov <mihail.varbanov@inabyte.com>
2023-02-08 09:32:41 +0200

    Push category attributes panel as Cupertino route, not popup like [#2954].

--- lib/ui/attributes/ContentAttributesCategoryPanel.dart
+++ lib/ui/attributes/ContentAttributesCategoryPanel.dart
@@ -4,6 +4,7 @@ import 'dart:collection';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:illinois/service/Analytics.dart';
+import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:illinois/utils/AppUtils.dart';
 import 'package:rokwire_plugin/model/content_attributes.dart';
 import 'package:rokwire_plugin/service/localization.dart';
@@ -19,47 +20,10 @@ class ContentAttributesCategoryPanel extends StatefulWidget {
   final bool multipleSelection;
   final bool filtersMode;
 
-  ContentAttributesCategoryPanel._({this.contentAttributes, this.category, this.attributes, this.selection, this.multipleSelection = false, this.filtersMode = false});
+  ContentAttributesCategoryPanel({this.contentAttributes, this.category, this.attributes, this.selection, this.multipleSelection = false, this.filtersMode = false});
 
   @override
   State<StatefulWidget> createState() => _ContentAttributesCategoryPanelState();
-
-  static Future<LinkedHashSet<String>?> present(BuildContext context, {
-    ContentAttributes? contentAttributes,
-    ContentAttributesCategory? category,
-    List<ContentAttribute>? attributes,
-    LinkedHashSet<String>? selection,
-    bool multipleSelection = false,
-    bool filtersMode = false,
-  }) {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(WidgetsBinding.instance.window);
-    double height = mediaQuery.size.height - mediaQuery.viewPadding.top - mediaQuery.viewInsets.top - 16;
-    return showModalBottomSheet<LinkedHashSet<String>?>(
-      context: context,
-      isScrollControlled: true,
-      isDismissible: true,
-      useRootNavigator: true,
-      clipBehavior: Clip.antiAlias,
-      backgroundColor: Styles().colors?.background,
-      constraints: BoxConstraints(maxHeight: height, minHeight: height),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(16))),
-      builder: (context) {
-        return ContentAttributesCategoryPanel._(
-          contentAttributes: contentAttributes,
-          category: category,
-          attributes: attributes,
-          selection: selection,
-          multipleSelection: multipleSelection,
-          filtersMode: filtersMode,
-        );
-      }
-    );
-    /*Navigator.push(context, PageRouteBuilder(
-      pageBuilder: (context, animation1, animation2) => ContentAttributesCategoryPanel._(contentAttributes: contentAttributes),
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero
-    ));*/
-  }
 }
 
 class _ContentAttributesCategoryPanelState extends State<ContentAttributesCategoryPanel> {
@@ -92,60 +56,62 @@ class _ContentAttributesCategoryPanelState extends State<ContentAttributesCatego
 
   @override
   Widget build(BuildContext context) {
-    bool canClear = (0 < (widget.attributes?.length ?? 0)) && !widget.filtersMode && (widget.category?.isSingleSelection ?? false) && _selection.isNotEmpty;
-    String clearTitle = Localization().getStringEx('dialog.clear.title', 'Clear');
-    
-    String applyTitle = Localization().getStringEx('dialog.apply.title', 'Apply');
-    bool canApply = widget.multipleSelection && !DeepCollectionEquality().equals(widget.selection ?? LinkedHashSet<String>(), _selection);
+    String? title = widget.contentAttributes?.stringValue(widget.category?.title);
 
-    return Column(children: [
-      Container(color: Styles().colors?.white, child:
-        Row(children: [
-          Expanded(child:
-              Padding(padding: EdgeInsets.only(left: 16), child:
-                Text(widget.contentAttributes?.stringValue(widget.category?.title) ?? '', style: TextStyle(fontFamily: Styles().fontFamilies?.bold, fontSize: 18, color: Styles().colors?.fillColorSecondary),)
+    List<Widget> actions = <Widget>[];
+    
+    if ((0 < (widget.attributes?.length ?? 0)) && !widget.filtersMode && (widget.category?.isSingleSelection ?? false) && _selection.isNotEmpty) {
+      actions.add(_buildHeaderBarButton(
+        title:  Localization().getStringEx('dialog.clear.title', 'Clear'),
+        onTap: _onTapClear,
+      ));
+    }
+
+    if (widget.multipleSelection && !DeepCollectionEquality().equals(widget.selection ?? LinkedHashSet<String>(), _selection)) {
+      actions.add(_buildHeaderBarButton(
+        title:  Localization().getStringEx('dialog.apply.title', 'Apply'),
+        onTap: _onTapApply,
+      ));
+    }
+
+    return Scaffold(
+      appBar: HeaderBar(title: title, actions: actions,),
+      backgroundColor: Styles().colors?.background,
+      body: Column(children: [
+        Expanded(child:
+          SingleChildScrollView(child:
+            Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24,), child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children:
+                _buildContent(),
               )
+            ),
           ),
-          Visibility(visible: canApply, child:
-            Semantics(label: applyTitle, hint: Localization().getStringEx('dialog.clear.hint', ''), inMutuallyExclusiveGroup: true, button: true, child:
-              InkWell(onTap : _onTapApply, child:
-                Container(padding: EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12), child:
-                  Text(applyTitle, style: TextStyle(fontFamily: Styles().fontFamilies?.bold, fontSize: 18, color: Styles().colors?.fillColorSecondary, decoration: TextDecoration.underline,)
-                  ),
+        )
+      ]),
+    );
+
+  }
+
+  Widget _buildHeaderBarButton({String? title, void Function()? onTap, double horizontalPadding = 16}) {
+    return Semantics(label: title, button: true, excludeSemantics: true, child: 
+      InkWell(onTap: onTap, child:
+        Align(alignment: Alignment.center, child:
+          Padding(padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: 12), child:
+            Column(mainAxisSize: MainAxisSize.min, children: [
+              Container(
+                decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Styles().colors!.white!, width: 1.5, ))),
+                child: Text(title ?? '',
+                  style: TextStyle(fontFamily: Styles().fontFamilies?.bold, fontSize: 16, color: Styles().colors?.white,)
                 ),
               ),
-            ),
-          ),
-          Visibility(visible: canClear, child:
-            Semantics(label: clearTitle, hint: Localization().getStringEx('dialog.clear.hint', ''), inMutuallyExclusiveGroup: true, button: true, child:
-              InkWell(onTap : _onTapClear, child:
-                Container(padding: EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12), child:
-                  Text(clearTitle, style: TextStyle(fontFamily: Styles().fontFamilies?.bold, fontSize: 18, color: Styles().colors?.fillColorSecondary, decoration: TextDecoration.underline,)
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Semantics(label: Localization().getStringEx('dialog.close.title', 'Close'), hint: Localization().getStringEx('dialog.close.hint', ''), inMutuallyExclusiveGroup: true, button: true, child:
-            InkWell(onTap : _onTapClose, child:
-              Container(padding: EdgeInsets.only(left: 8, right: 16, top: 16, bottom: 16), child:
-                Styles().images?.getImage('close', excludeFromSemantics: true),
-              ),
-            ),
-          ),
-        ],),
-      ),
-      Container(color: Styles().colors?.surfaceAccent, height: 1,),
-      Expanded(child:
-        SingleChildScrollView(child:
-          Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24,), child:
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children:
-              _buildContent(),
-            )
+            ],)
           ),
         ),
-      )
-    ],);
+        //Padding(padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: 12), child:
+        //  Text(title ?? '', style: Styles().textStyles?.getTextStyle('panel.athletics.home.button.underline'))
+        //),
+      ),
+    );
   }
 
   List<Widget> _buildContent() {
@@ -267,10 +233,5 @@ class _ContentAttributesCategoryPanelState extends State<ContentAttributesCatego
     else {
       Navigator.of(context).pop(_selection);
     }
-  }
-
-  void _onTapClose() {
-    Analytics().logSelect(target: 'Close');
-    Navigator.of(context).pop(null);
   }
 }
