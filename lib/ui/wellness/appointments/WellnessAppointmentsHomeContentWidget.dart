b54f6f776f031a970b80d7f9a2745ac623c35f63
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2023-02-13 14:58:19 +0200

    Acknowledge sorted appointments from the backend. [#2971]

--- lib/ui/wellness/appointments/WellnessAppointmentsHomeContentWidget.dart
+++ lib/ui/wellness/appointments/WellnessAppointmentsHomeContentWidget.dart
@@ -44,11 +44,11 @@ class _WellnessAppointmentsHomeContentWidgetState extends State<WellnessAppointm
   List<Appointment>? _upcomingAppointments;
   List<Appointment>? _pastAppointments;
   late bool _appointmentsCanDisplay;
-  bool _loading = false;
+  int _loadingProgress = 0;
 
   @override
   void initState() {
-    NotificationService().subscribe(this, [Storage.notifySettingChanged, FlexUI.notifyChanged, Appointments.notifyAppointmentsChanged]);
+    NotificationService().subscribe(this, [Storage.notifySettingChanged, FlexUI.notifyChanged, Appointments.notifyUpcomingAppointmentsChanged, Appointments.notifyPastAppointmentsChanged]);
     _initAppointments();
     super.initState();
   }
@@ -75,7 +75,7 @@ class _WellnessAppointmentsHomeContentWidgetState extends State<WellnessAppointm
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [_buildRescheduleDescription(), _buildNothingToDisplayMsg(), _buildDisplayAppointmentsSettings()]));
-    } else if (_loading) {
+    } else if (_isLoading) {
       return _buildLoadingContent();
     } else {
       return RefreshIndicator(
@@ -327,29 +327,39 @@ class _WellnessAppointmentsHomeContentWidgetState extends State<WellnessAppointm
   }
 
   void _loadAppointments() {
+    _loadPastAppointments();
+    _loadUpcomingAppointments();
+  }
+
+  void _loadUpcomingAppointments() {
     if (_appointmentsCanDisplay) {
-      _setLoading(true);
-      List<Appointment>? appointments = Appointments().getAppointments();
-      if (CollectionUtils.isNotEmpty(appointments)) {
-        _upcomingAppointments = <Appointment>[];
-        _pastAppointments = <Appointment>[];
-        for (Appointment appointment in appointments!) {
-          if (appointment.isUpcoming) {
-            _upcomingAppointments!.add(appointment);
-          } else {
-            _pastAppointments!.add(appointment);
-          }
-        }
-      } else {
-        _upcomingAppointments = _pastAppointments = null;
-      }
-      _setLoading(false);
+      _increaseProgress();
+      _upcomingAppointments = Appointments().getAppointments(timeSource: AppointmentsTimeSource.upcoming);
+      _decreaseProgress();
     }
   }
 
-  void _setLoading(bool loading) {
+  void _loadPastAppointments() {
+    if (_appointmentsCanDisplay) {
+      _increaseProgress();
+      _pastAppointments = Appointments().getAppointments(timeSource: AppointmentsTimeSource.past);
+      _decreaseProgress();
+    }
+  }
+
+  bool get _isLoading {
+    return (_loadingProgress > 0);
+  }
+
+  void _increaseProgress() {
     setStateIfMounted(() {
-      _loading = loading;
+      _loadingProgress++;
+    });
+  }
+
+  void _decreaseProgress() {
+    setStateIfMounted(() {
+      _loadingProgress--;
     });
   }
 
@@ -363,8 +373,10 @@ class _WellnessAppointmentsHomeContentWidgetState extends State<WellnessAppointm
       if (mounted) {
         setState(() {});
       }
-    } else if (name == Appointments.notifyAppointmentsChanged) {
-      _loadAppointments();
+    } else if (name == Appointments.notifyUpcomingAppointmentsChanged) {
+      _loadUpcomingAppointments();
+    } else if (name == Appointments.notifyPastAppointmentsChanged) {
+      _loadPastAppointments();
     }
   }
 }
