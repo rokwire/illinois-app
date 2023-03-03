c800656cdd34ec8cebbc62cb1956d894ce74035f
Apoorva Aditya <apoorva9@illinois.edu>
2023-03-02 18:47:29 -0600

    Switched over to the OnetOccupationData to get the occupations data

--- lib/service/skills/OccupationsService.dart
+++ lib/service/skills/OccupationsService.dart
@@ -1,6 +1,7 @@
 import 'package:illinois/model/occupation/Occupation.dart';
 import 'package:illinois/service/skills/IOccupationsData.dart';
-import 'package:illinois/service/skills/MockOccupationsData.dart';
+// import 'package:illinois/service/skills/MockOccupationsData.dart';
+import 'package:illinois/service/skills/OnetOccupationsData.dart';
 
 class OccupationsService /* with Service */ {
   static final OccupationsService _instance = OccupationsService._internal();
@@ -10,9 +11,14 @@ class OccupationsService /* with Service */ {
   OccupationsService._internal();
 
   // TODO: Replace mock implmentation with real backend implementation
-  IOccupationsData _occupationsData = MockOccupationsData();
+  // IOccupationsData _occupationsData = MockOccupationsData();
+  IOccupationsData _occupationsData = OnetOccupationsData();
 
   Future<List<Occupation>> getAllOccupations() {
     return _occupationsData.getAllOccupations();
+  }
+
+  Future<Occupation> getOccupation({required String occupationCode}) {
+    return _occupationsData.getOccupation(occupationCode: occupationCode);
   }
 }
