89b25acf90e2c04fdd6c8049cf9d66c044c3a07b
Apoorva Aditya <apoorva9@illinois.edu>
2023-03-02 18:51:24 -0600

    Added FutureBuilder to build the correct widgets based on state of the API calls

--- lib/ui/academics/OccupationDetails.dart
+++ lib/ui/academics/OccupationDetails.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:illinois/model/occupation/Occupation.dart';
-import 'package:illinois/model/occupation/skill.dart';
+import 'package:illinois/model/occupation/Skill.dart';
+import 'package:illinois/service/skills/OccupationsService.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:illinois/utils/Utils.dart';
 
 class OccupationDetails extends StatelessWidget {
-  OccupationDetails({Key? key, required this.occupation}) : super(key: key);
+  OccupationDetails({Key? key, required this.occupationCode}) : super(key: key);
 
-  final Occupation occupation;
+  final String occupationCode;
 
   @override
   Widget build(BuildContext context) {
@@ -15,90 +16,98 @@ class OccupationDetails extends StatelessWidget {
       appBar: HeaderBar(
         title: 'Occupation Details',
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 12,
-            ),
-            Text(
-              occupation.name!,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 25),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Text("Match Percentage: ${occupation.matchPercentage!.toInt()}%"),
-            SizedBox(
-              height: 12,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                child: LinearProgressIndicator(
-                  backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
-                    occupation.matchPercentage! / 100.0,
-                  ),
-                  color: LinearProgressColorUtils.linearProgressIndicatorColor(
-                    occupation.matchPercentage! / 100.0,
-                  ),
-                  minHeight: 20,
-                  value: occupation.matchPercentage! / 100,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Text(occupation.description!,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontFamily: "regular", fontSize: 16, color: Colors.black)),
-            SizedBox(
-              height: 12,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      body: FutureBuilder(
+          future: OccupationsService().getOccupation(occupationCode: occupationCode),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final Occupation occupation = snapshot.data as Occupation;
+            return SingleChildScrollView(
               child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  ExpansionTile(
-                      title: Text(
-                        "Soft Skills",
-                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                      ),
-                      children: occupation.skills!.map((e) => SkillListTile(skill: e)).toList()),
-                  ExpansionTile(
-                    title: Text(
-                      "Technical Skills",
-                      style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                    children: occupation.technicalSkills!.map((e) => SkillListTile(skill: e)).toList(),
+                children: [
+                  SizedBox(
+                    height: 12,
                   ),
-                  ExpansionTile(
-                    title: Text(
-                      "Other Sections",
-                      style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  Text(
+                    occupation.title!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text("Match Percentage: ${occupation.matchPercentage!.toInt()}%"),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      child: LinearProgressIndicator(
+                        backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
+                          occupation.matchPercentage! / 100.0,
+                        ),
+                        color: LinearProgressColorUtils.linearProgressIndicatorColor(
+                          occupation.matchPercentage! / 100.0,
+                        ),
+                        minHeight: 20,
+                        value: occupation.matchPercentage! / 100,
+                      ),
                     ),
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('Learn More'),
-                      ),
-                      ListTile(
-                        title: Text('?'),
-                      ),
-                      ListTile(
-                        title: Text('?'),
-                      )
-                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(occupation.description!,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontFamily: "regular", fontSize: 16, color: Colors.black)),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        ExpansionTile(
+                            title: Text(
+                              "Soft Skills",
+                              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                            ),
+                            children: occupation.skills!.map((e) => SkillListTile(skill: e)).toList()),
+                        ExpansionTile(
+                          title: Text(
+                            "Technical Skills",
+                            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          children: occupation.technicalSkills!.map((e) => SkillListTile(skill: e)).toList(),
+                        ),
+                        ExpansionTile(
+                          title: Text(
+                            "Other Sections",
+                            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          children: <Widget>[
+                            ListTile(
+                              title: Text('Learn More'),
+                            ),
+                            ListTile(
+                              title: Text('?'),
+                            ),
+                            ListTile(
+                              title: Text('?'),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
