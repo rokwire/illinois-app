877836a92c94b248a29a7f2059772cbfbaf6bb67
Todor Bachvarov <tobachvarov@gmail.com>
2023-03-15 17:22:32 +0200

    export more text styles

--- lib/ui/SavedPanel.dart
+++ lib/ui/SavedPanel.dart
@@ -198,11 +198,11 @@ class _SavedPanelState extends State<SavedPanel> implements NotificationsListene
             HtmlWidget(
                 "<div style=text-align:center> ${HomeFavoritesWidget.emptyMessageHtml(favoriteCategory)} </div>",
                 onTapUrl : (url) {HomeFavoritesWidget.handleLocalUrl(url, context: context, analyticsTarget: 'View Home', analyticsSource: 'SavedPanel($favoriteCategory)'); return true;},
-                textStyle:  TextStyle(color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.medium, fontSize: 18),
+                textStyle: Styles().textStyles?.getTextStyle("widget.message.medium.semi_thin"),
                 customStylesBuilder: (element) => (element.localName == "a") ? {"color": ColorUtils.toHex(HomeFavoritesWidget.linkColor(favoriteCategory) ?? Colors.red)} : null,
                 // renderMode: RenderMode.sliverList,
             )
-            : Text(Localization().getStringEx("panel.saved.message.no_items.description", "Tap the \u2606 on events, dining locations, and reminders that interest you to quickly find them here."), style: TextStyle(fontFamily: Styles().fontFamilies?.regular, fontSize: 16, color: Styles().colors?.textBackground),),
+            : Text(Localization().getStringEx("panel.saved.message.no_items.description", "Tap the \u2606 on events, dining locations, and reminders that interest you to quickly find them here."), style: Styles().textStyles?.getTextStyle("widget.item.regular.thin"),),
         Expanded(child: Container(), flex: 3),
     ],),);
   }
