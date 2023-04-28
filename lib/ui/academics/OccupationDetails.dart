bf5012a4db38dd9cfc0d240492d32e322e6d80d7
Apoorva Aditya <apoorva9@illinois.edu>
2023-04-28 09:37:53 -0500

    Updated UI for OccupationDetails and OccupationList

--- lib/ui/academics/OccupationDetails.dart
+++ lib/ui/academics/OccupationDetails.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:illinois/model/occupation/Occupation.dart';
 import 'package:illinois/model/occupation/OccupationMatch.dart';
-import 'package:illinois/model/occupation/Skill.dart';
+import 'package:illinois/model/occupation/TechnologySkill.dart';
+import 'package:illinois/model/occupation/WorkStyles.dart';
 import 'package:illinois/service/OccupationMatching.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:illinois/utils/Utils.dart';
+import 'package:rokwire_plugin/service/connectivity.dart';
+import 'package:rokwire_plugin/service/localization.dart';
+import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/ui/widgets/section_header.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class OccupationDetails extends StatelessWidget {
   OccupationDetails({Key? key, required this.occupationMatch}) : super(key: key);
@@ -14,6 +20,7 @@ class OccupationDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Styles().colors?.background,
       appBar: HeaderBar(
         title: 'Occupation Details',
       ),
@@ -33,124 +40,205 @@ class OccupationDetails extends StatelessWidget {
             }
             final Occupation occupation = snapshot.data as Occupation;
             return SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(
-                    occupation.title!,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text("Match Percentage: ${occupationMatch.matchPercent!.toInt()}%"),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      child: LinearProgressIndicator(
-                        backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
-                          occupationMatch.matchPercent! / 100.0,
-                        ),
-                        color: LinearProgressColorUtils.linearProgressIndicatorColor(
-                          occupationMatch.matchPercent! / 100.0,
-                        ),
-                        minHeight: 20,
-                        value: occupationMatch.matchPercent! / 100,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(occupation.description!,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontFamily: "regular", fontSize: 16, color: Colors.black)),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 20.0),
-                        ExpansionTile(
-                            title: Text(
-                              "Soft Skills",
-                              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                            ),
-                            children: occupation.skills!.map((e) => SkillListTile(skill: e)).toList()),
-                        ExpansionTile(
-                          title: Text(
-                            "Technical Skills",
-                            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                          ),
-                          children: occupation.technicalSkills!.map((e) => SkillListTile(skill: e)).toList(),
-                        ),
-                        ExpansionTile(
-                          title: Text(
-                            "Other Sections",
-                            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                          ),
-                          children: <Widget>[
-                            ListTile(
-                              title: Text('Learn More'),
-                            ),
-                            ListTile(
-                              title: Text('?'),
-                            ),
-                            ListTile(
-                              title: Text('?'),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+              child: SectionSlantHeader(
+                headerWidget: _buildHeader(occupation),
+                slantColor: Styles().colors?.gradientColorPrimary,
+                slantPainterHeadingHeight: 0,
+                backgroundColor: Styles().colors?.background,
+                children: Connectivity().isOffline ? _buildOfflineMessage() : _buildContent(occupation),
+                childrenPadding: EdgeInsets.zero,
+                allowOverlap: !Connectivity().isOffline,
               ),
             );
           }),
     );
   }
+
+  Widget _buildHeader(Occupation occupation) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [
+            Styles().colors?.fillColorPrimaryVariant ?? Colors.transparent,
+            Styles().colors?.gradientColorPrimary ?? Colors.transparent,
+          ])),
+      child: Column(
+        children: [
+          SizedBox(height: 12),
+          Text(
+            occupation.title!,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+          SizedBox(height: 12),
+          Text(
+            "Match Percentage: ${occupationMatch.matchPercent!.toInt()}%",
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              child: LinearProgressIndicator(
+                backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
+                  occupationMatch.matchPercent! / 100.0,
+                ),
+                color: LinearProgressColorUtils.linearProgressIndicatorColor(
+                  occupationMatch.matchPercent! / 100.0,
+                ),
+                minHeight: 20,
+                value: occupationMatch.matchPercent! / 100,
+              ),
+            ),
+          ),
+          SizedBox(height: 12),
+          Text(occupation.description!,
+              textAlign: TextAlign.center, style: TextStyle(fontFamily: "regular", fontSize: 16, color: Colors.white)),
+          SizedBox(height: 12),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildContent(Occupation occupation) {
+    return [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 40, bottom: 10),
+              child: Text('Your Results'),
+            ),
+            ExpansionTile(
+                title: Text(
+                  "Work Styles",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+                children: occupation.workStyles!
+                    .map((workstyle) => WorkStyleListTile(workstyle: workstyle))
+                    .cast<Widget>()
+                    .toList()),
+            ExpansionTile(
+              title: Text(
+                "Technology Skills",
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              children: occupation.technologySkills!.map((e) => TechnologySkillListTile(technologySkill: e)).toList(),
+            ),
+            ExpansionTile(
+              title: Text(
+                "Other Sections",
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text('Learn More'),
+                  onTap: () {
+                    // TODO: Make sure launching URL works
+                    launchUrlString(occupation.onetLink ?? 'https://onetonline.org');
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> _buildOfflineMessage() {
+    return [
+      Padding(
+        padding: EdgeInsets.all(28),
+        child: Center(
+            child: Text(
+                Localization().getStringEx(
+                    'panel.skills_self_evaluation.results.offline.error.msg', 'Results not available while offline.'),
+                textAlign: TextAlign.center,
+                style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.content.title'))),
+      ),
+    ];
+  }
 }
 
-class SkillListTile extends StatelessWidget {
-  const SkillListTile({Key? key, required this.skill}) : super(key: key);
+class WorkStyleListTile extends StatelessWidget {
+  const WorkStyleListTile({Key? key, required this.workstyle, this.mostRecentScore, this.comparisonScore})
+      : super(key: key);
 
-  final Skill skill;
+  final WorkStyle workstyle;
+  final int? mostRecentScore;
+  final int? comparisonScore;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ListTile(
-          title: Text(skill.name.toString()),
-          subtitle: Text(skill.description.toString()),
-          trailing: Text(skill.matchPercentage!.toInt().toString() + "%"),
-          onTap: () {},
-        ),
-        ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          child: LinearProgressIndicator(
-            backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
-              skill.matchPercentage! / 100.0,
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: Card(
+            child: InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12, bottom: 12, left: 16),
+                child: Row(
+                  children: [
+                    Flexible(
+                        flex: 5,
+                        fit: FlexFit.tight,
+                        child: Text(workstyle.name.toString(),
+                            style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.content.title'))),
+                    Flexible(
+                        flex: 3,
+                        fit: FlexFit.tight,
+                        child: Text(
+                          // TODO: Add the actual scores here
+                          "S" ?? "--",
+                          style:
+                              Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.results.score.current'),
+                          textAlign: TextAlign.center,
+                        )),
+                    Flexible(
+                        flex: 3,
+                        fit: FlexFit.tight,
+                        // TODO: Add the comparison scores/ranks here
+                        child: Text("R" ?? "--",
+                            style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.results.score.past'),
+                            textAlign: TextAlign.center)),
+                  ],
+                ),
+              ),
             ),
-            color: LinearProgressColorUtils.linearProgressIndicatorColor(
-              skill.matchPercentage! / 100.0,
-            ),
-            // valueColor: AlwaysStoppedAnimation(,),
-            minHeight: 10,
-            value: skill.matchPercentage! / 100,
           ),
         ),
       ],
+    );
+  }
+}
+
+class TechnologySkillListTile extends StatelessWidget {
+  const TechnologySkillListTile({Key? key, required this.technologySkill}) : super(key: key);
+
+  final TechnologySkill technologySkill;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              ' • ' + technologySkill.title.toString(),
+              textAlign: TextAlign.start,
+              style: TextStyle(fontWeight: FontWeight.w100),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/ui/academics/OccupationList.dart
+++ lib/ui/academics/OccupationList.dart
@@ -7,7 +7,10 @@ import 'package:illinois/service/OccupationMatching.dart';
 import 'package:illinois/ui/academics/OccupationDetails.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:illinois/utils/Utils.dart';
+import 'package:rokwire_plugin/service/connectivity.dart';
+import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/ui/widgets/section_header.dart';
 
 class OccupationList extends StatelessWidget {
   OccupationList({Key? key}) : super(key: key);
@@ -15,68 +18,140 @@ class OccupationList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: HeaderBar(
-        title: 'Matched Occupations',
-        actions: [
-          IconButton(
-            onPressed: () {},
-            // TODO: Change the icon to a more appropriate one
-            icon: Styles().images?.getImage('edit') ?? Icon(Icons.list),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          _buildSearchBar(),
-          Expanded(
-            child: _buildOccupationListView(),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSearchBar() {
-    return Row(
-      children: [
-        Icon(Icons.search),
-        Expanded(
-          child: TextField(
-            decoration: InputDecoration(
-              label: Text('Search for specific job'),
-            ),
-          ),
+      appBar: HeaderBar(title: 'Results'),
+      body: SingleChildScrollView(
+        child: SectionSlantHeader(
+          headerWidget: _buildHeader(),
+          slantColor: Styles().colors?.gradientColorPrimary,
+          slantPainterHeadingHeight: 0,
+          backgroundColor: Styles().colors?.background,
+          children: Connectivity().isOffline ? _buildOfflineMessage() : _buildOccupationListView(),
+          childrenPadding: EdgeInsets.zero,
+          allowOverlap: !Connectivity().isOffline,
         ),
-      ],
+      ),
     );
   }
 
-  Widget _buildOccupationListView() {
-    return FutureBuilder(
-        future: OccupationMatching().getAllOccupationMatches(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.data == null) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'You do not have any matched occupations currently. Please take the survey first and wait for results to be processed.',
-                  textAlign: TextAlign.center,
+  Widget _buildHeader() {
+    return Container(
+      padding: EdgeInsets.only(top: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            Localization().getStringEx('panel.skills_self_evaluation.results.career_explorer.title', 'Career Explorer'),
+            style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.results.header'),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            Localization().getStringEx('panel.skills_self_evaluation.results.score.description', 'Skills Domain Score'),
+            style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.header.description'),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            Localization().getStringEx('panel.skills_self_evaluation.results.score.scale', '(0-100)'),
+            style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.header.description'),
+            textAlign: TextAlign.center,
+          ),
+          _buildOccupationsHeader(),
+        ],
+      ),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Styles().colors?.fillColorPrimaryVariant ?? Colors.transparent,
+            Styles().colors?.gradientColorPrimary ?? Colors.transparent,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOccupationsHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 28, right: 28),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Divider(color: Styles().colors?.surface, thickness: 2),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                    flex: 5,
+                    fit: FlexFit.tight,
+                    child: Text(
+                      Localization().getStringEx('panel.skills_self_evaluation.results.occupation.title', 'OCCUPATION'),
+                      style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.results.table.header'),
+                    )),
+                Flexible(
+                  flex: 3,
+                  fit: FlexFit.tight,
+                  child: Text(
+                    Localization()
+                        .getStringEx('panel.skills_self_evaluation.results.match_percentage.title', 'MATCH PERCENTAGE'),
+                    style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.results.table.header'),
+                  ),
                 ),
-              ),
-            );
-          }
-          List<OccupationMatch> occupationMatches = (snapshot.data as List).cast<OccupationMatch>();
-          return ListView.builder(
-            itemBuilder: (context, index) => OccupationListTile(
-              occupationMatch: occupationMatches[index],
+              ],
             ),
-            itemCount: occupationMatches.length,
-          );
-        });
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildOfflineMessage() {
+    return [
+      Padding(
+        padding: EdgeInsets.all(28),
+        child: Center(
+            child: Text(
+                Localization().getStringEx(
+                    'panel.skills_self_evaluation.results.offline.error.msg', 'Results not available while offline.'),
+                textAlign: TextAlign.center,
+                style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.content.title'))),
+      ),
+    ];
+  }
+
+  List<Widget> _buildOccupationListView() {
+    return [
+      FutureBuilder(
+          future: OccupationMatching().getAllOccupationMatches(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.data == null) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'You do not have any matched occupations currently. Please take the survey first and wait for results to be processed.',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            }
+            List<OccupationMatch> occupationMatches = (snapshot.data as List).cast<OccupationMatch>();
+            return Column(
+              children: occupationMatches
+                  .map(
+                    (occupationMatch) => OccupationListTile(
+                      occupationMatch: occupationMatch,
+                    ),
+                  )
+                  .toList(),
+            );
+          })
+    ];
   }
 }
 
@@ -87,29 +162,43 @@ class OccupationListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: OccupationMatchCircle(
-        matchPercentage: occupationMatch.matchPercent ?? 100.0,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+      child: Card(
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context, CupertinoPageRoute(builder: (context) => OccupationDetails(occupationMatch: occupationMatch)));
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 12, left: 16),
+            child: Row(
+              children: [
+                Flexible(
+                  flex: 5,
+                  fit: FlexFit.tight,
+                  child: Text(
+                    occupationMatch.occupation?.title.toString() ?? "",
+                    style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.content.title'),
+                  ),
+                ),
+                Spacer(),
+                Flexible(
+                  child: OccupationMatchCircle(
+                    matchPercentage: occupationMatch.matchPercent ?? 100.0,
+                  ),
+                ),
+                Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: SizedBox(
+                        height: 16.0,
+                        child: Styles().images?.getImage('chevron-right-bold', excludeFromSemantics: true))),
+              ],
+            ),
+          ),
+        ),
       ),
-      title: Text(occupationMatch.occupation?.title.toString() ?? ""),
-      subtitle: Text(
-        occupationMatch.occupation?.description.toString() ?? "",
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
-      ),
-      trailing: Styles().images?.getImage('chevron-right'),
-      onTap: () {
-        Navigator.push(
-            context,
-            CupertinoPageRoute(
-                builder: (context) => OccupationDetails(
-                      occupationMatch: occupationMatch,
-                      // survey: Config().bessiSurveyID,
-                      // onComplete: _gotoResults,
-                      // offlineWidget: _buildOfflineWidget(),
-                      // tabBar: uiuc.TabBar(),
-                    )));
-      },
     );
   }
 }
@@ -121,26 +210,28 @@ class OccupationMatchCircle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.rotationY(math.pi),
-          child: CircularProgressIndicator(
-            strokeWidth: 8.0,
-            backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
-              matchPercentage / 100.0,
+    return Container(
+      child: Stack(
+        children: [
+          Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationY(math.pi),
+            child: CircularProgressIndicator(
+              strokeWidth: 8.0,
+              backgroundColor: LinearProgressColorUtils.linearProgressIndicatorBackgroundColor(
+                matchPercentage / 100.0,
+              ),
+              color: LinearProgressColorUtils.linearProgressIndicatorColor(
+                matchPercentage / 100.0,
+              ),
+              value: matchPercentage / 100,
             ),
-            color: LinearProgressColorUtils.linearProgressIndicatorColor(
-              matchPercentage / 100.0,
-            ),
-            value: matchPercentage / 100,
           ),
-        ),
-        Positioned.fill(
-          child: Center(child: Text(matchPercentage.toInt().toString())),
-        ),
-      ],
+          Positioned.fill(
+            child: Center(child: Text(matchPercentage.toInt().toString())),
+          ),
+        ],
+      ),
     );
   }
 }
