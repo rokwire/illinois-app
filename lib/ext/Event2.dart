2efaa15e84be7241a6e117e6250d60b4e51247ad
Mihail Varbanov <mihail.varbanov@inabyte.com>
2023-05-30 17:55:03 +0300

    Created Event2Ext [#3300].

---
+++ lib/ext/Event2.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/material.dart';
+import 'package:illinois/service/Content.dart';
+import 'package:rokwire_plugin/model/event2.dart';
+import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
+
+extension Event2Ext on Event2 {
+
+  Color? get uiColor => Styles().colors?.eventColor;
+
+  String? get displayImageUrl => StringUtils.isNotEmpty(imageUrl) ? imageUrl : randomImageUrl;
+
+  String? get randomImageUrl {
+    if (assignedImageUrl == null) {
+      dynamic category = (attributes != null) ? attributes!['category'] : null;
+      assignedImageUrl = _randomImageUrlForAttribute('events', category);
+    }
+    if (assignedImageUrl == null) {
+      dynamic sport = (attributes != null) ? attributes!['sport'] : null;
+      assignedImageUrl = _randomImageUrlForAttribute('sports', sport, mapping: _sportCodes);
+    }
+    if (assignedImageUrl == null) {
+      assignedImageUrl = Content().randomImageUrl('events.Other');
+    }
+    return assignedImageUrl;
+  }
+
+  String? _randomImageUrlForAttribute(String prefix, dynamic value, { Map<String, String>? mapping }) {
+    if (value is String) {
+      return (mapping != null) ?
+        (Content().randomImageUrl('$prefix.${mapping[value]}') ?? Content().randomImageUrl('$prefix.$value')) :
+        Content().randomImageUrl('$prefix.$value');
+    }
+    else if (value is List) {
+      for (dynamic entry in value) {
+        String? result = _randomImageUrlForAttribute(prefix, entry, mapping: mapping);
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    return null;
+  }
+
+  static const Map<String, String> _sportCodes = {
+      "Baseball" : "baseball",
+      "Men's Basketball" : "mbball",
+      "Men's Cross Country" : "mcross",
+      "Football" : "football",
+      "Men's Golf" : "mgolf",
+      "Men's Gymnastics" : "mgym",
+      "Men's Tennis" : "mten",
+      "Men's Track Field" : "mtrack",
+      "Wrestling" : "wrestling",
+      "Women's Basketball" : "wbball",
+      "Women's Cross Country" : "wcross",
+      "Women's Golf" : "wgolf",
+      "Women's Gymnastics" : "wgym",
+      "Women's Soccer" : "wsoc",
+      "Softball" : "softball",
+      "Swim Dive" : "wswim",
+      "Women's Tennis" : "wten",
+      "Women's Track Field" : "wtrack",
+      "Volleyball" : "wvball"
+  };
+}
