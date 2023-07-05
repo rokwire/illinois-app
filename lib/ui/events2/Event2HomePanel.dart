7cdb31ffb5bdf64ea628906478d3ef341d5538f4
Mihail Varbanov <mihail.varbanov@inabyte.com>
2023-07-05 17:26:41 +0300

    Refresh Events2Home content when a new event is created [#3300].

--- lib/ui/events2/Event2HomePanel.dart
+++ lib/ui/events2/Event2HomePanel.dart
@@ -277,6 +277,7 @@ class _Event2HomePanelState extends State<Event2HomePanel> implements Notificati
       AppLivecycle.notifyStateChanged,
       FlexUI.notifyChanged,
       Event2FilterParam.notifyChanged,
+      Events2.notifyChanged,
     ]);
 
     _scrollController.addListener(_scrollListener);
@@ -330,6 +331,9 @@ class _Event2HomePanelState extends State<Event2HomePanel> implements Notificati
     }
     else if (name == Event2FilterParam.notifyChanged) {
       _updateFilers();
+    }
+    else if (name == Events2.notifyChanged) {
+      _reload();
     }
   }
 
