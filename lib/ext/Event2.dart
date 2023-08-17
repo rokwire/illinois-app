7951b09524965c1d12628c69ce5d9132d29ba50d
Mihail Varbanov <mihail.varbanov@inabyte.com>
2023-08-17 11:12:48 +0300

    "Multi-person" calendar event filter type renamed to "Multi-event" [#3570].

--- lib/ext/Event2.dart
+++ lib/ext/Event2.dart
@@ -366,7 +366,7 @@ String? event2TypeFilterToDisplayString(Event2TypeFilter? value) {
     case Event2TypeFilter.public: return Localization().getStringEx('model.event2.event_type.public', 'Public');
     case Event2TypeFilter.private: return Localization().getStringEx('model.event2.event_type.private', 'Private');
     case Event2TypeFilter.nearby: return Localization().getStringEx('model.event2.event_type.nearby', 'Nearby');
-    case Event2TypeFilter.superEvent: return Localization().getStringEx('model.event2.event_type.super_event', 'Multi-person');
+    case Event2TypeFilter.superEvent: return Localization().getStringEx('model.event2.event_type.super_event', 'Multi-event');
     default: return null;
   }
 }
