795e349941c47be5f34deb5bfbe468cad31f8111
Mihail Varbanov <misho@inabyte.com>
2024-01-18 11:18:57 +0200

    Created DeviceCalendar model [#3868].

--- lib/service/DeviceCalendar.dart
+++ lib/service/DeviceCalendar.dart
@@ -1,30 +1,17 @@
 
 import 'package:flutter/material.dart';
-import 'package:illinois/ext/Appointment.dart';
-import 'package:illinois/model/Appointment.dart';
-import 'package:illinois/service/Appointments.dart';
+import 'package:illinois/model/DeviceCalendar.dart';
 import 'package:illinois/service/Storage.dart';
 import 'package:illinois/ui/widgets/RibbonButton.dart';
 import 'package:illinois/utils/AppUtils.dart';
 import 'package:rokwire_plugin/model/auth2.dart';
-import 'package:rokwire_plugin/model/event2.dart';
-import 'package:rokwire_plugin/model/device_calendar.dart';
-import 'package:rokwire_plugin/service/app_datetime.dart';
 import 'package:rokwire_plugin/service/auth2.dart';
-import 'package:rokwire_plugin/service/events2.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/device_calendar.dart' as rokwire;
 import 'package:rokwire_plugin/service/styles.dart';
 import 'package:rokwire_plugin/ui/widgets/rounded_button.dart';
 import 'package:rokwire_plugin/utils/utils.dart';
-import 'package:illinois/model/sport/Game.dart';
-import 'package:rokwire_plugin/model/event.dart' as ExploreEvent;
-import 'package:illinois/service/Sports.dart';
-import 'package:illinois/service/Guide.dart';
-import 'package:illinois/model/Canvas.dart';
-import 'package:illinois/service/Canvas.dart';
-import 'package:rokwire_plugin/service/events.dart';
 import 'package:device_calendar/device_calendar.dart';
 
 class DeviceCalendar extends rokwire.DeviceCalendar implements NotificationsListener {
@@ -62,12 +49,12 @@ class DeviceCalendar extends rokwire.DeviceCalendar implements NotificationsList
   }
 
   Future<bool> addToCalendar(dynamic event) async {
-    _DeviceCalendarEvent? deviceCalendarEvent = _DeviceCalendarEvent.from(event);
+    DeviceCalendarEvent? deviceCalendarEvent = DeviceCalendarEvent.from(event);
     return deviceCalendarEvent != null ?  await super.addEvent(deviceCalendarEvent) : false;
   }
 
   void _processFavorite(dynamic event) {
-    _DeviceCalendarEvent? deviceCalendarEvent = Storage().calendarEnabledToAutoSave == true ? _DeviceCalendarEvent.from(event) : null;
+    DeviceCalendarEvent? deviceCalendarEvent = Storage().calendarEnabledToAutoSave == true ? DeviceCalendarEvent.from(event) : null;
     if(deviceCalendarEvent==null)
       return;
 
@@ -92,107 +79,6 @@ class DeviceCalendar extends rokwire.DeviceCalendar implements NotificationsList
   @override
   void onRequestPermissionFailed() {
     AppToast.show(Localization().getStringEx('logic.calendar.permission_denied', 'Unable to save event to calendar. Permissions not granted.'));
-  }
-}
-
-class _DeviceCalendarEvent extends DeviceCalendarEvent {
-  //String? internalEventId;
-  //String? title;
-  //String? deepLinkUrl;
-  //DateTime? startDate;
-  //DateTime? endDate;
-
-  _DeviceCalendarEvent({String? internalEventId, String? title, String? deepLinkUrl, DateTime? startDate, DateTime? endDate}) :
-    super(internalEventId: internalEventId, title: title, deepLinkUrl: deepLinkUrl, startDate: startDate, endDate: endDate);
-
-  static _DeviceCalendarEvent? from(dynamic data){
-    if (data is ExploreEvent.Event) {
-      return _DeviceCalendarEvent.fromEvent(data);
-    }
-    if (data is Event2) {
-      return _DeviceCalendarEvent.fromEvent2(data);
-    }
-    else if (data is Game){
-      return _DeviceCalendarEvent.fromGame(data);
-    }
-    else if (data is GuideFavorite){
-      return _DeviceCalendarEvent.fromGuide(data);
-    }
-    else if (data is Appointment) {
-      return _DeviceCalendarEvent.fromAppointment(data);
-    }
-    else if (data is CanvasCalendarEvent) {
-      return _DeviceCalendarEvent.fromCanvasCalendarEvent(data);
-    }
-
-    return null;
-  }
-
-  static _DeviceCalendarEvent? fromEvent(ExploreEvent.Event? event){
-    return (event != null) ? _DeviceCalendarEvent(
-      title: event.title,
-      internalEventId: event.id,
-      startDate: event.startDateLocal,
-      endDate: event.endDateLocal,
-      deepLinkUrl: "${Events().eventDetailUrl}?event_id=${event.id}"
-    ) : null;
-  }
-
-  static _DeviceCalendarEvent? fromEvent2(Event2? event){
-    return (event != null) ? _DeviceCalendarEvent(
-        title: event.exploreTitle,
-        internalEventId: event.id,
-        startDate: AppDateTime().getUniLocalTimeFromUtcTime(event.startTimeUtc),
-        endDate: AppDateTime().getUniLocalTimeFromUtcTime(event.endTimeUtc),
-        deepLinkUrl: "${Events2().eventDetailUrl}?event_id=${event.id}"
-    ) : null;
-  }
-
-  static _DeviceCalendarEvent? fromGame(Game? game){
-    return (game != null) ? _DeviceCalendarEvent(
-      title: game.title,
-      internalEventId: game.id,
-      startDate: game.dateTimeUniLocal,
-      endDate:  AppDateTime().getUniLocalTimeFromUtcTime(game.endDateTimeUtc),
-      deepLinkUrl: "${Sports().gameDetailUrl}?game_id=${game.id}%26sport=${game.sport?.shortName}"
-    ) : null;
-  }
-
-  static _DeviceCalendarEvent? fromGuide(GuideFavorite? guide){
-    Map<String, dynamic>? guideEntryData = (guide != null) ? Guide().entryById(guide.id) : null;
-    //Only reminders are allowed to save
-    return (Guide().isEntryReminder(guideEntryData)) ? _DeviceCalendarEvent(
-        title: Guide().entryListTitle(guideEntryData, stripHtmlTags: true),
-        internalEventId: guide?.id,
-        startDate: Guide().reminderDate(guideEntryData),
-        deepLinkUrl: "${Guide().guideDetailUrl}?guide_id=${guide?.id}"
-      ) : null;
-  }
-
-  static _DeviceCalendarEvent? fromAppointment(Appointment? appointment) {
-    if (appointment == null) {
-      return null;
-    }
-    DateTime? calendarEventStartDateTime = AppDateTime().getUniLocalTimeFromUtcTime(appointment.startTimeUtc);
-    DateTime? calendarEventEndDateTime = calendarEventStartDateTime?.add(Duration(hours: 1));
-    return _DeviceCalendarEvent(
-      title: appointment.title,
-      internalEventId: appointment.id,
-      startDate: calendarEventStartDateTime,
-      endDate: calendarEventEndDateTime,
-      deepLinkUrl: "${Appointments().appointmentDetailUrl}?appointment_id=${appointment.id}"
-    );
-  }
-
-  static _DeviceCalendarEvent? fromCanvasCalendarEvent(CanvasCalendarEvent? event) {
-    return (event != null)
-        ? _DeviceCalendarEvent(
-            title: event.title,
-            internalEventId: event.id?.toString(),
-            startDate: event.startAtLocal,
-            endDate: event.endAtLocal,
-            deepLinkUrl: "${Canvas().canvasEventDetailUrl}?event_id=${event.id}")
-        : null;
   }
 }
 
