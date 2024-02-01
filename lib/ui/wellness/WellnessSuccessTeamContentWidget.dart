3f21e7e6e2e32baeac8aeda25f621a003c249fc2
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2024-02-01 09:37:31 +0200

    Remove some warnings [#3925]

--- lib/ui/wellness/WellnessSuccessTeamContentWidget.dart
+++ lib/ui/wellness/WellnessSuccessTeamContentWidget.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Board of Trustees of the University of Illinois.
+ * Copyright 2024 Board of Trustees of the University of Illinois.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,49 +68,49 @@ class _WellnessSuccessTeamContentWidgetState extends State<WellnessSuccessTeamCo
   Widget _buildContentUi() {
     List<Widget> widgetList = <Widget>[];
 
-    List<Widget> primaryCareProviderWidgets = this.primaryCareProviders.map((pcp) => _buildSuccessTeamItem("MCKINLEY HEALTH CENTER", "Primary Care Provider", "${pcp?.firstName} ${pcp?.lastName}", pcp!.email, pcp.image!)).toList();
-    List<Widget> academicAdvisorWidgets = this.academicAdvisors.map((advisor) => _buildSuccessTeamItem("THE GRAINGER COLLEGE OF ENGINEERING", "Academic Advisor", "${advisor?.firstName} ${advisor?.lastName}", advisor!.email, advisor!.image)).toList();
+    List<Widget> primaryCareProviderWidgets = this.primaryCareProviders.map((pcp) => _buildSuccessTeamItem("MCKINLEY HEALTH CENTER", "Primary Care Provider", "${pcp?.firstName} ${pcp?.lastName}", pcp!.email, pcp.image)).toList();
+    List<Widget> academicAdvisorWidgets = this.academicAdvisors.map((advisor) => _buildSuccessTeamItem("THE GRAINGER COLLEGE OF ENGINEERING", "Academic Advisor", "${advisor?.firstName} ${advisor?.lastName}", advisor!.email, advisor.image)).toList();
     if (primaryCareProviderWidgets.length > 0) widgetList.add(primaryCareProviderWidgets[0]);
     if (academicAdvisorWidgets.length > 0) widgetList.add(academicAdvisorWidgets[advisorIndex]);
 
-    if (this.academicAdvisors!.length > 1) {
+    if (this.academicAdvisors.length > 1) {
       widgetList.add(Container(child: (
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Padding(padding: EdgeInsets.only(left: 6), child:
             advisorIndex != 0
-              ? GestureDetector(onTap: () => _changeActiveAdvisor(-1), child: Styles().images?.getImage('chevron-left-bold') ?? Container())
+              ? GestureDetector(onTap: () => _changeActiveAdvisor(-1), child: Styles().images.getImage('chevron-left-bold') ?? Container())
               : Container(
                 foregroundDecoration: BoxDecoration(
                   color: Colors.grey,
                   backgroundBlendMode: BlendMode.saturation,
                 ),
-                child: Styles().images?.getImage('chevron-left-bold') ?? Container())),
+                child: Styles().images.getImage('chevron-left-bold') ?? Container())),
           Spacer(),
-          Text("${advisorIndex+1} of ${academicAdvisors!.length}", style: Styles().textStyles?.getTextStyle('widget.description.regular')),
+          Text("${advisorIndex+1} of ${academicAdvisors.length}", style: Styles().textStyles.getTextStyle('widget.description.regular')),
           Spacer(),
           Padding(padding: EdgeInsets.only(right: 6), child:
-            advisorIndex+1 == academicAdvisors!.length
+            advisorIndex+1 == academicAdvisors.length
               ? Container(
                 foregroundDecoration: BoxDecoration(
                   color: Colors.grey,
                   backgroundBlendMode: BlendMode.saturation,
                 ),
-                child: Styles().images?.getImage('chevron-right-bold') ?? Container())
-              : GestureDetector(onTap: () => _changeActiveAdvisor(1), child: Styles().images?.getImage('chevron-right-bold') ?? Container()))
+                child: Styles().images.getImage('chevron-right-bold') ?? Container())
+              : GestureDetector(onTap: () => _changeActiveAdvisor(1), child: Styles().images.getImage('chevron-right-bold') ?? Container()))
         ])
       )));
     }
     Widget noMembers = Padding(padding: EdgeInsets.only(bottom: 16), child: Container(
       decoration: BoxDecoration(
-      color: Styles().colors?.white,
-      boxShadow: [BoxShadow(color: Styles().colors!.blackTransparent018!, spreadRadius: 1.0, blurRadius: 3.0, offset: Offset(1, 1))],
+      color: Styles().colors.white,
+      boxShadow: [BoxShadow(color: Styles().colors.blackTransparent018, spreadRadius: 1.0, blurRadius: 3.0, offset: Offset(1, 1))],
       borderRadius: BorderRadius.circular(4)),
       child: Container(
         padding: EdgeInsets.all(16),
         child: Row(
           children: [
             Spacer(),
-            Text("Your Success Team is empty.", style: Styles().textStyles?.getTextStyle('widget.description.regular')),
+            Text("Your Success Team is empty.", style: Styles().textStyles.getTextStyle('widget.description.regular')),
             Spacer(),
           ]
         )
@@ -125,36 +125,36 @@ class _WellnessSuccessTeamContentWidgetState extends State<WellnessSuccessTeamCo
   Widget _buildSuccessTeamItem(String department, String type, String name, String contactInfo, [String image = ""]) {
     return Padding(padding: EdgeInsets.only(bottom: 16), child: Container(
         decoration: BoxDecoration(
-            color: Styles().colors?.white,
-            boxShadow: [BoxShadow(color: Styles().colors!.blackTransparent018!, spreadRadius: 1.0, blurRadius: 3.0, offset: Offset(1, 1))],
+            color: Styles().colors.white,
+            boxShadow: [BoxShadow(color: Styles().colors.blackTransparent018, spreadRadius: 1.0, blurRadius: 3.0, offset: Offset(1, 1))],
             borderRadius: BorderRadius.circular(4)),
         child: Container(padding: EdgeInsets.all(16), child: Column(children: [
           Row(children: [
             Padding(padding: EdgeInsets.only(top: 4, bottom: 4), child:
-            Text(department, style: Styles().textStyles?.getTextStyle('widget.title.small.fat'))
+            Text(department, style: Styles().textStyles.getTextStyle('widget.title.small.fat'))
             ),
           ]),
           Row(children: [
             Column(crossAxisAlignment: CrossAxisAlignment.start, children: [Padding(padding: EdgeInsets.only(top: 4), child:
             Row(children: [
-              Text(type, style: Styles().textStyles?.getTextStyle('widget.title.medium.extra_fat')),
-              GestureDetector(onTap: () => _onTapInfo(type), child: Padding(padding: EdgeInsets.only(left: 4), child: Styles().images?.getImage('info', excludeFromSemantics: true) ?? Container()))
+              Text(type, style: Styles().textStyles.getTextStyle('widget.title.medium.extra_fat')),
+              GestureDetector(onTap: () => _onTapInfo(type), child: Padding(padding: EdgeInsets.only(left: 4), child: Styles().images.getImage('info', excludeFromSemantics: true) ?? Container()))
             ])
             ),
               Row(children: [
-                Padding(padding: EdgeInsets.only(right: 6), child: Styles().images?.getImage('person', excludeFromSemantics: true) ?? Container()),
-                Text(name, style: Styles().textStyles?.getTextStyle('widget.description.regular')),
+                Padding(padding: EdgeInsets.only(right: 6), child: Styles().images.getImage('person', excludeFromSemantics: true) ?? Container()),
+                Text(name, style: Styles().textStyles.getTextStyle('widget.description.regular')),
               ]),
               Row(children: [
-                Padding(padding: EdgeInsets.only(right: 6), child: Styles().images?.getImage('external-link', excludeFromSemantics: true) ?? Container()),
-                Text(contactInfo, style: Styles().textStyles?.getTextStyle('widget.description.regular'))]),
+                Padding(padding: EdgeInsets.only(right: 6), child: Styles().images.getImage('external-link', excludeFromSemantics: true) ?? Container()),
+                Text(contactInfo, style: Styles().textStyles.getTextStyle('widget.description.regular'))]),
             ]),
             Spacer(),
             Padding(padding: EdgeInsets.only(left: 8), child: ClipRRect(
               borderRadius: BorderRadius.circular(8),
               child: image != ""
                 ? Image.memory(base64Decode(image), width: 80, height: 80)
-                : Styles().images?.getImage('default-profile-photo', width: 80, height: 80, excludeFromSemantics: true)))
+                : Styles().images.getImage('default-profile-photo', width: 80, height: 80, excludeFromSemantics: true)))
           ]),
         ])
         )));
@@ -171,7 +171,7 @@ class _WellnessSuccessTeamContentWidgetState extends State<WellnessSuccessTeamCo
     bool pcpLoading = true;
     bool advisorsLoading = true;
     Wellness().getPrimaryCareProviders().then((item) {
-      this.primaryCareProviders = item != null ? item : [];
+      this.primaryCareProviders = item;
       pcpLoading = false;
       _setLoading(advisorsLoading);
     });
@@ -185,17 +185,17 @@ class _WellnessSuccessTeamContentWidgetState extends State<WellnessSuccessTeamCo
   void _onTapInfo(String type) {
     Widget textWidget = Text(
       type == "Primary Care Provider"
-        ? Localization().getStringEx('panel.wellness.successteam.pcp.info.description', "A primary care provider (PCP) provides essential healthcare services, promotes your overall wellbeing, and can refer you to services such as internal medicine, family practice, psychiatry, and gynecology.")!
-        : Localization().getStringEx('panel.wellness.successteam.advisors.info.description', "An academic advisor is your go-to person for course registration, degree planning, and academic support services.")!,
-      style: Styles().textStyles?.getTextStyle('panel.skills_self_evaluation.auth_dialog.text'),
+        ? Localization().getStringEx('panel.wellness.successteam.pcp.info.description', "A primary care provider (PCP) provides essential healthcare services, promotes your overall wellbeing, and can refer you to services such as internal medicine, family practice, psychiatry, and gynecology.")
+        : Localization().getStringEx('panel.wellness.successteam.advisors.info.description', "An academic advisor is your go-to person for course registration, degree planning, and academic support services."),
+      style: Styles().textStyles.getTextStyle('panel.skills_self_evaluation.auth_dialog.text'),
       textAlign: TextAlign.center,
     );
     showDialog(context: context, builder: (_) => InfoPopup(
-      backColor: Styles().colors?.surface,
+      backColor: Styles().colors.surface,
       padding: EdgeInsets.only(left: 32, right: 32, top: 40, bottom: 32),
       alignment: Alignment.center,
       infoTextWidget: textWidget,
-      closeIcon: Styles().images?.getImage('close', excludeFromSemantics: true),
+      closeIcon: Styles().images.getImage('close', excludeFromSemantics: true),
     ),);
   }
 
