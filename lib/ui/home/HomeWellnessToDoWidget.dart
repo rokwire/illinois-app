36679981f6da9814772ec422ca4a964279e0bc60
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2024-02-19 16:25:31 +0200

    Fix the images for completed To-Do items [#3984]

--- lib/ui/home/HomeWellnessToDoWidget.dart
+++ lib/ui/home/HomeWellnessToDoWidget.dart
@@ -141,7 +141,7 @@ class _HomeWellnessToDoWidgetState extends State<HomeWellnessToDoWidget> impleme
   }
 
   Widget _buildToDoItemWidget(WellnessToDoItem item) {
-    Widget? completedWidget = Styles().images.getImage(item.isCompleted ? 'check-circle-outline-gray-white' : 'circle-outline-white', color: Styles().colors.fillColorSecondary , excludeFromSemantics: true);
+    Widget? completedWidget = Styles().images.getImage(item.isCompleted ? 'check-circle-outline-gray-white-2' : 'circle-outline-white', color: Styles().colors.fillColorSecondary , excludeFromSemantics: true);
     return GestureDetector(onTap: () => _onTapToDoItem(item), child: Padding(padding: EdgeInsets.only(top: 10), child: Container(color: Colors.transparent, child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
       Padding(padding: EdgeInsets.only(right: 10), child: completedWidget),
       Expanded(child: Text(StringUtils.ensureNotEmpty(item.name), maxLines: 1, overflow: TextOverflow.ellipsis, textAlign: TextAlign.start, style: Styles().textStyles.getTextStyle("widget.info.small")))
