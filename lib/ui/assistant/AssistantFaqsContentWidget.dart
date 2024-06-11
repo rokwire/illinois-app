4ebfee6cf203a4bd386d6910bb6984f95a240611
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2024-06-11 13:31:19 +0300

    Implement FAQs in Assistant and load them from Content BB [#4089]

--- lib/ui/assistant/AssistantFaqsContentWidget.dart
+++ lib/ui/assistant/AssistantFaqsContentWidget.dart
@@ -13,6 +13,14 @@
 // limitations under the License.
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:illinois/service/Assistant.dart';
+import 'package:illinois/utils/AppUtils.dart';
+import 'package:rokwire_plugin/service/localization.dart';
+import 'package:rokwire_plugin/service/notification_service.dart';
+import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AssistantFaqsContentWidget extends StatefulWidget {
   AssistantFaqsContentWidget();
@@ -21,9 +29,70 @@ class AssistantFaqsContentWidget extends StatefulWidget {
   State<AssistantFaqsContentWidget> createState() => _AssistantFaqsContentWidgetState();
 }
 
-class _AssistantFaqsContentWidgetState extends State<AssistantFaqsContentWidget> {
+class _AssistantFaqsContentWidgetState extends State<AssistantFaqsContentWidget> implements NotificationsListener {
+
+  @override
+  void initState() {
+    super.initState();
+    NotificationService().subscribe(this, [
+      Assistant.notifyFaqsContentChanged,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    NotificationService().unsubscribe(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Padding(padding: EdgeInsets.symmetric(vertical: 30), child: Text('Not Implemented, yet')));
+    return Positioned.fill(
+        child: Padding(
+            padding: EdgeInsets.all(20),
+            child: SingleChildScrollView(
+                child: HtmlWidget(_faqsContent,
+                    onTapUrl: (url) {
+                      _launchUrl(url);
+                      return true;
+                    },
+                    textStyle: Styles().textStyles.getTextStyle('widget.detail.regular'),
+                    customStylesBuilder: (element) {
+                      if (element.localName == "h3") {
+                        String fontFamilyName = Styles().textStyles.getTextStyle('widget.detail.regular.fat')?.fontFamily.toString() ?? '';
+                        String fontSize = Styles().textStyles.getTextStyle('widget.detail.regular.fat')?.fontSize.toString() ?? '0';
+                        return {'font-size': '${fontSize}px', 'font-family': fontFamilyName};
+                      } else {
+                        return null;
+                      }
+                    }))));
+  }
+
+  void _launchUrl(String? url) async {
+    if (StringUtils.isNotEmpty(url)) {
+      if (StringUtils.isNotEmpty(url)) {
+        Uri? uri = Uri.tryParse(url!);
+        if ((uri != null) && (await canLaunchUrl(uri))) {
+          launchUrl(uri, mode: LaunchMode.externalApplication);
+        }
+      }
+    }
+  }
+
+  String get _faqsContent {
+    String? faqs = Assistant().faqs;
+    if (StringUtils.isEmpty(faqs)) {
+      faqs = Localization().getStringEx('panel.assistant.faqs.missing.msg', 'No FAQs available.');
+    }
+    return faqs!;
+  }
+
+  // NotificationsListener
+
+  @override
+  void onNotification(String name, param) {
+    if (name == Assistant.notifyFaqsContentChanged) {
+      setStateIfMounted((){});
+    }
   }
 }
