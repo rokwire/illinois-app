f882369fc18c19407a4ec329e50675c815eb9bd0
Mihail Varbanov <misho@inabyte.com>
2024-06-25 16:50:31 +0300

    Updated empty favorites message [#4121].

--- lib/ui/home/HomeEmptyFavoritesWidget.dart
+++ lib/ui/home/HomeEmptyFavoritesWidget.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:illinois/service/Analytics.dart';
 import 'package:illinois/ui/BrowsePanel.dart';
+import 'package:illinois/ui/home/HomeFavoritesPanel.dart';
 import 'package:illinois/ui/home/HomeWidgets.dart';
 import 'package:illinois/ui/profile/ProfileHomePanel.dart';
 import 'package:illinois/ui/settings/SettingsHomeContentPanel.dart';
@@ -34,6 +35,9 @@ class HomeFavoritesInstructionsMessageCard extends StatelessWidget {
   static const String _browseLocalUrlMacro          = '{{browse_local_url}}';
   static const String _browseLocalUrl               = 'browse';
 
+  static const String _favoritesLocalUrlMacro       = '{{favorites_local_url}}';
+  static const String _favoritesLocalUrl            = 'favorites';
+
   static const String _signInLocalUrlMacro          = '{{sign_in_local_url}}';
   static const String _signInLocalUrl               = 'sign_in';
 
@@ -43,8 +47,9 @@ class HomeFavoritesInstructionsMessageCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
     HomeMessageHtmlCard(
-      message: Localization().getStringEx("widget.home.favorites.instructions.message.text", "Tap the \u2606s under <a href='$_browseLocalUrlMacro'>Browse</a> to add shortcuts to Favorites. Note that some features require specific <a href='$_privacySettingsLocalUrlMacro'>privacy settings</a> and <a href='$_signInLocalUrlMacro'>signing in</a> with your NetID, phone number, or email address.")
+      message: Localization().getStringEx("widget.home.favorites.instructions.message.text", "Tap the \u2606s in <a href='$_browseLocalUrlMacro'>Sections</a> or <a href='$_favoritesLocalUrlMacro'>Favorites</a> to add shortcuts to Favorites. Note that some features require specific <a href='$_privacySettingsLocalUrlMacro'>privacy settings</a> and <a href='$_signInLocalUrlMacro'>signing in</a> with your NetID, phone number, or email address.")
         .replaceAll(_browseLocalUrlMacro, '$_localScheme://$_browseLocalUrl')
+        .replaceAll(_favoritesLocalUrlMacro, '$_localScheme://$_favoritesLocalUrl')
         .replaceAll(_signInLocalUrlMacro, '$_localScheme://$_signInLocalUrl')
         .replaceAll(_privacySettingsLocalUrlMacro, '$_localScheme://$_privacySettingsLocalUrl'),
       margin: EdgeInsets.only(bottom: 16),
@@ -55,8 +60,12 @@ class HomeFavoritesInstructionsMessageCard extends StatelessWidget {
     Uri? uri = (url != null) ? Uri.tryParse(url) : null;
     if (uri?.scheme == _localScheme) {
       if (uri?.host == _browseLocalUrl) {
-        Analytics().logSelect(target: 'Browse', source: runtimeType.toString());
+        Analytics().logSelect(target: 'Sections', source: runtimeType.toString());
         NotificationService().notify(BrowsePanel.notifySelect);
+      }
+      if (uri?.host == _favoritesLocalUrl) {
+        Analytics().logSelect(target: 'Favorites', source: runtimeType.toString());
+        NotificationService().notify(HomeFavoritesPanel.notifySelect);
       }
       else if (uri?.host == _signInLocalUrl) {
         Analytics().logSelect(target: 'Sign In', source: runtimeType.toString());
