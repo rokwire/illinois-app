6181736adc5ac340792ab3b72f4636f67c9b0b72
Mihail Varbanov <misho@inabyte.com>
2024-06-28 11:48:12 +0300

    Read wallet content types from flexUI [#4183].

--- lib/ui/wallet/WalletHomePanel.dart
+++ lib/ui/wallet/WalletHomePanel.dart
@@ -13,9 +13,11 @@
 // limitations under the License.
 
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:illinois/service/Analytics.dart';
 import 'package:illinois/service/Auth2.dart';
+import 'package:illinois/service/FlexUI.dart';
 import 'package:illinois/service/Storage.dart';
 import 'package:illinois/ui/wallet/WalletAddIlliniCashPanel.dart';
 import 'package:illinois/ui/wallet/WalletICardContentWidget.dart';
@@ -28,6 +30,7 @@ import 'package:rokwire_plugin/service/connectivity.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
 
 enum WalletContentType { illiniId, illiniIdFaqs, busPass, mealPlan, addIlliniCash }
 
@@ -67,7 +70,8 @@ class WalletHomePanel extends StatefulWidget {
 }
 
 class _WalletHomePanelState extends State<WalletHomePanel> implements NotificationsListener {
-  late WalletContentType _selectedContent;
+  late List<WalletContentType> _contentTypes;
+  WalletContentType? _selectedContentType;
   bool _contentValuesVisible = false;
   Map<WalletContentType, GlobalKey> _pageKeys = <WalletContentType, GlobalKey>{};
 
@@ -76,13 +80,23 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
     super.initState();
 
     NotificationService().subscribe(this, [
+      FlexUI.notifyChanged
     ]);
 
-    if (widget.contentType != null) {
-      Storage()._contentType = _selectedContent = widget.contentType!;
+    _contentTypes = _buildContentTypes();
+
+    WalletContentType? selectedContentType = widget.contentType;
+    if ((selectedContentType != null) && _contentTypes.contains(selectedContentType)) {
+      Storage()._contentType = _selectedContentType = selectedContentType;
     }
     else {
-      _selectedContent = Storage()._contentType ?? WalletContentType.values.first;
+      selectedContentType = Storage()._contentType;
+      if ((selectedContentType == null) || !_contentTypes.contains(selectedContentType)) {
+        selectedContentType = _contentTypes.isNotEmpty ? _contentTypes.first : null;
+      }
+      if (selectedContentType != null) {
+        _selectedContentType = selectedContentType;
+      }
     }
   }
 
@@ -96,6 +110,9 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
 
   @override
   void onNotification(String name, param) {
+    if (name == FlexUI.notifyChanged) {
+      _updateContentTypes();
+    }
   }
 
   @override
@@ -133,24 +150,15 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
 
   Widget get _panelContent {
 
-    GlobalKey pageKey = _pageKeys[_selectedContent] ??= GlobalKey();
-    Widget pagetWidget;
-    switch(_selectedContent) {
-      case WalletContentType.illiniId:      pagetWidget = WalletICardContentWidget(key: pageKey); break;
-      case WalletContentType.illiniIdFaqs:  pagetWidget = WalletICardFaqsContentWidget(key: pageKey); break;
-      case WalletContentType.busPass:       pagetWidget = WalletMTDBusPassContentWidget(key: pageKey, expandHeight: false,); break;
-      case WalletContentType.mealPlan:      pagetWidget = WalletMealPlanContentWidget(key: pageKey, headerHeight: 82,); break;
-      case WalletContentType.addIlliniCash: pagetWidget = WalletAddIlliniCashContentWidget(key: pageKey, topOffset: 82, hasCancel: false,); break;
-    }
-
-    Color backColor = (pagetWidget is WalletHomeContentWidget) ? (pagetWidget as WalletHomeContentWidget).backgroundColor  : Styles().colors.white;
+    Widget? pageWidget = _contentPage;
+    Color backColor = (pageWidget is WalletHomeContentWidget) ? (pageWidget as WalletHomeContentWidget).backgroundColor  : Styles().colors.white;
 
     return Column(children: <Widget>[
       Expanded(child:
         Container(color: backColor, child:
           SingleChildScrollView(physics: _contentValuesVisible ? NeverScrollableScrollPhysics() : null, child:
             Stack(children: [
-              SafeArea(child: pagetWidget),
+              SafeArea(child: pageWidget ?? Container()),
               Column(mainAxisSize: MainAxisSize.min, children: [
                 Padding(padding: EdgeInsets.only(left: 16, top: 16, right: 16), child:
                   RibbonButton(
@@ -159,7 +167,7 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
                     borderRadius: BorderRadius.all(Radius.circular(5)),
                     border: Border.all(color: Styles().colors.surfaceAccent, width: 1),
                     rightIconKey: (_contentValuesVisible ? 'chevron-up' : 'chevron-down'),
-                    label: _walletContentTypeToDisplayString(_selectedContent) ?? '',
+                    label: _walletContentTypeToDisplayString(_selectedContentType) ?? '',
                     onTap: _onTapContentSwitch
                   )
                 ),
@@ -171,6 +179,19 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
       )
     ]);
   }
+
+  Widget? get _contentPage {
+    switch(_selectedContentType!) {
+      case WalletContentType.illiniId:      return WalletICardContentWidget(key: _contentPageKey);
+      case WalletContentType.illiniIdFaqs:  return WalletICardFaqsContentWidget(key: _contentPageKey);
+      case WalletContentType.busPass:       return WalletMTDBusPassContentWidget(key: _contentPageKey, expandHeight: false,);
+      case WalletContentType.mealPlan:      return WalletMealPlanContentWidget(key: _contentPageKey, headerHeight: 82,);
+      case WalletContentType.addIlliniCash: return WalletAddIlliniCashContentWidget(key: _contentPageKey, topOffset: 82, hasCancel: false,);
+    }
+  }
+
+  GlobalKey? get _contentPageKey => (_selectedContentType != null) ?
+    (_pageKeys[_selectedContentType!] ??= GlobalKey()) : null;
 
   Widget get _dropdownContainer => Visibility(visible: _contentValuesVisible, child:
     Container(child:
@@ -193,7 +214,7 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
     List<Widget> contentList = <Widget>[];
     contentList.add(Container(color: Styles().colors.fillColorSecondary, height: 2));
     for (WalletContentType contentType in WalletContentType.values) {
-      if (_selectedContent != contentType) {
+      if (_selectedContentType != contentType) {
         contentList.add(RibbonButton(
           backgroundColor: Styles().colors.white,
           border: Border.all(color: Styles().colors.surfaceAccent, width: 1),
@@ -211,10 +232,37 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
     );
   }
 
+  List<WalletContentType> _buildContentTypes() {
+    List<WalletContentType> contentTypes = <WalletContentType>[];
+    List<String>? contentCodes = JsonUtils.listStringsValue(FlexUI()['wallet']);
+    if (contentCodes != null) {
+      for (String code in contentCodes) {
+        WalletContentType? value = _walletContentTypeFromString(code);
+        if (value != null) {
+          contentTypes.add(value);
+        }
+      }
+    }
+    return contentTypes;
+  }
+
+  void _updateContentTypes() {
+    List<WalletContentType> contentTypes = _buildContentTypes();
+    if (!DeepCollectionEquality().equals(_contentTypes, contentTypes) && mounted) {
+      setState(() {
+        _contentTypes = contentTypes;
+        _contentValuesVisible = false;
+        if (!_contentTypes.contains(_selectedContentType)) {
+          _selectedContentType = _contentTypes.isNotEmpty ? _contentTypes.first : null;
+        }
+      });
+    }
+  }
+
   void _onTapDropdownItem(WalletContentType contentType) {
     Analytics().logSelect(target: _walletContentTypeToDisplayString(contentType), source: widget.runtimeType.toString());
     setState(() {
-      Storage()._contentType = _selectedContent = contentType;
+      Storage()._contentType = _selectedContentType = contentType;
       _contentValuesVisible = !_contentValuesVisible;
     });
   }
@@ -242,22 +290,22 @@ class _WalletHomePanelState extends State<WalletHomePanel> implements Notificati
 
 WalletContentType? _walletContentTypeFromString(String? value) {
   switch(value) {
-    case 'illini-id': return WalletContentType.illiniId;
-    case 'illini-id-faq': return WalletContentType.illiniIdFaqs;
-    case 'bus-pass': return WalletContentType.busPass;
-    case 'meal-plan': return WalletContentType.mealPlan;
-    case 'add-illini-cash': return WalletContentType.addIlliniCash;
+    case 'illini_id': return WalletContentType.illiniId;
+    case 'illini_id_faq': return WalletContentType.illiniIdFaqs;
+    case 'bus_pass': return WalletContentType.busPass;
+    case 'meal_plan': return WalletContentType.mealPlan;
+    case 'add_illini_cash': return WalletContentType.addIlliniCash;
     default: return null;
   }
 }
 
 String? _walletContentTypeToString(WalletContentType? value) {
   switch(value) {
-    case WalletContentType.illiniId: return 'illini-id';
-    case WalletContentType.illiniIdFaqs: return 'illini-id-faq';
-    case WalletContentType.busPass: return 'bus-pass';
-    case WalletContentType.mealPlan: return 'meal-plan';
-    case WalletContentType.addIlliniCash: return 'add-illini-cash';
+    case WalletContentType.illiniId: return 'illini_id';
+    case WalletContentType.illiniIdFaqs: return 'illini_id_faq';
+    case WalletContentType.busPass: return 'bus_pass';
+    case WalletContentType.mealPlan: return 'meal_plan';
+    case WalletContentType.addIlliniCash: return 'add_illini_cash';
     default: return null;
   }
 }
