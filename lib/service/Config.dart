70d6e026ad2676a1114d41f50bca4b4d1b392692
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2024-07-01 13:57:59 +0300

    Show more canvas courses and sort them by createdAt date desc [#4136]

--- lib/service/Config.dart
+++ lib/service/Config.dart
@@ -216,6 +216,7 @@ class Config extends rokwire.Config {
   bool get allowGroupsAuthmanSync    => JsonUtils.boolValue(settings['allowGroupsAuthmanSync']) ?? false;
   bool get showGroupPostReactions    => JsonUtils.boolValue(settings['showGroupPostReactions']) ?? false;
   int  get notificationBodyMaxLength => JsonUtils.intValue(settings['notificationBodyMaxLength']) ?? 256;
+  int  get canvasCoursesCountPerPage => JsonUtils.intValue(settings['canvasCoursesCountPerPage']) ?? 100;
 
   String? get healthScreenerSurveyID => JsonUtils.stringValue(settings['health_screener_survey_id']);
   String? get bessiSurveyID          => JsonUtils.stringValue(settings['bessi_survey_id']);
