1fa3c2a4b2aa051498ac0637ff18b889b9a69d79
Mihail Varbanov <misho@inabyte.com>
2024-07-29 16:47:14 +0300

    Created ResourceAudioSound helper class, added m4a resource file in media app resources [#2258].

--- lib/utils/AudioUtils.dart
+++ lib/utils/AudioUtils.dart
@@ -1,6 +1,8 @@
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
 
 class Uint8ListAudioSource extends StreamAudioSource {
   final Uint8List _data;
@@ -17,5 +19,39 @@ class Uint8ListAudioSource extends StreamAudioSource {
       stream: Stream.fromIterable([_data.sublist(start ?? 0, end)]),
       contentType: 'audio/mp4',
     );
+  }
+}
+
+class ResourceAudioSound {
+  final String resourcePath;
+  AudioPlayer? _audioPlayer;
+
+  ResourceAudioSound(this.resourcePath);
+
+  Future<bool> play() async {
+    if (_audioPlayer == null) {
+      ByteData? resourceData = await AppBundle.loadBytes(resourcePath);
+      if (resourceData != null) {
+        _audioPlayer = AudioPlayer();
+        _audioPlayer?.playerStateStream.listen((PlayerState playerState) {
+          debugPrint("AudioPlayer state changed: ${playerState.processingState}");
+          if (playerState.processingState == ProcessingState.completed) {
+            _audioPlayer = null;
+          }
+        },
+        onError: (error) {
+          debugPrint("AudioPlayer error: $error");
+          _audioPlayer = null;
+        },
+        onDone: () {
+          debugPrint("AudioPlayer done.");
+          _audioPlayer = null;
+        });
+        await _audioPlayer?.setAudioSource(Uint8ListAudioSource(Uint8List.sublistView(resourceData)));
+        await _audioPlayer?.play();
+        return true;
+      }
+    }
+    return false;
   }
 }
