25c3ada09fce4f8e982f8a5e40a78ea3524c5cec
Mihail Varbanov <misho@inabyte.com>
2024-08-29 12:41:23 +0300

    Make sure to acknowledge a successful MTD stops request [#4307].

--- lib/service/MTD.dart
+++ lib/service/MTD.dart
@@ -146,7 +146,7 @@ class MTD with Service implements NotificationsListener {
   Future<void> _updateStops() async {
     String? stopsJsonString = await _loadStopsStringFromNet(changesetId: _stops?.changesetId);
     MTDStops? stops = MTDStops.fromJson(await JsonUtils.decodeMapAsync(stopsJsonString));
-    if ((stops != null) && (stops.changesetId != _stops?.changesetId)) {
+    if ((stops != null) && ((_stops == null) || ((stops.changesetId != null) && (stops.changesetId != _stops?.changesetId)))) {
       _stops = stops;
       _saveStopsStringToCache(stopsJsonString);
       NotificationService().notify(notifyStopsChanged);
