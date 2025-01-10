21212dc23e6730064da0cb128c1e8d91aeb95b07
Mihail Varbanov <mihail.varbanov@inabyte.com>
2025-01-10 15:27:08 +0200

    Updated Onboarding Profile Info flow [#4596].

--- lib/ui/onboarding2/Onboarding2ProfileInfoPanel.dart
+++ lib/ui/onboarding2/Onboarding2ProfileInfoPanel.dart
@@ -10,6 +10,7 @@ import 'package:rokwire_plugin/service/auth2.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/ui/widgets/rounded_button.dart';
 
 class Onboarding2ProfileInfoPanel extends StatefulWidget {
   final Map<String, dynamic>? onboardingContext;
@@ -22,7 +23,9 @@ class Onboarding2ProfileInfoPanel extends StatefulWidget {
 
 class _Onboarding2ProfileInfoPanelState extends State<Onboarding2ProfileInfoPanel> implements NotificationsListener, Onboarding2ProgressableState {
 
+  final GlobalKey<ProfileInfoPageState> _profileInfoKey = GlobalKey<ProfileInfoPageState>();
   bool _onboarding2Progress = false;
+  bool _saving = false;
 
   @override
   void initState() {
@@ -66,7 +69,12 @@ class _Onboarding2ProfileInfoPanelState extends State<Onboarding2ProfileInfoPane
 
   Widget get _headerWidget => Stack(children: [
     Styles().images.getImage('header-login', fit: BoxFit.fitWidth, width: MediaQuery.of(context).size.width, excludeFromSemantics: true,) ?? Container(),
-    OnboardingBackButton(padding: const EdgeInsets.only(left: 10, top: 30, right: 20, bottom: 20), onTap: _onTapBack),
+    Positioned(top: 0, left: 0, child:
+      _backImageButton,
+    ),
+    Positioned(top: 0, right: 0, child:
+      _skipLinkSection,
+    )
   ],);
 
   Widget get _titleWidget =>
@@ -81,30 +89,71 @@ class _Onboarding2ProfileInfoPanelState extends State<Onboarding2ProfileInfoPane
 
   Widget get _profileWidget =>
     Padding(padding: EdgeInsets.only(left: 16, right: 16, top: 16), child:
-      ProfileInfoPage(contentType: ProfileInfo.directoryInfo, onStateChanged: _onProfileStateChanged,),
+      ProfileInfoPage(key: _profileInfoKey,
+        contentType: ProfileInfo.directoryInfo,
+        onStateChanged: _onProfileStateChanged,
+        showProfileCommands: false,
+      ),
     );
 
-  Widget get _footerWidget =>  _onboarding2Progress ?
+  Widget get _footerWidget => _isLoaded ? _continueCommandSection : Container();
+
+  Widget get _continueCommandSection => Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 32), child:
+    _continueCommandButton,
+  );
+
+  bool get _canContinue => (_onboarding2Progress != true) && (_saving != true);
+
+  Widget get _continueCommandButton => RoundedButton(
+      label: Localization().getStringEx('panel.onboarding.profile_info.continue.title', 'Continue'),
+      hint: Localization().getStringEx('panel.onboarding.profile_info.continue.hint', ''),
+      textStyle: _canContinue ? Styles().textStyles.getTextStyle("widget.button.title.medium.fat") : Styles().textStyles.getTextStyle("widget.button.disabled.title.medium.fat.variant"),
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+      borderColor: _canContinue ? Styles().colors.fillColorSecondary : Styles().colors.fillColorPrimaryTransparent03,
+      progress: _onboarding2Progress || _saving,
+      enabled: _canContinue,
+      onTap: _onTapContinue
+  );
+
+  bool get _canSkip => (_onboarding2Progress != true);
+
+  Widget get _skipLinkSection => _onboarding2Progress ?
     Stack(children: [
-      _continueButton,
+      _skipLinkButton,
       Positioned.fill(child:
         Center(child:
-          _progressWidget
+          _skipProgressWidget
         )
       ),
-    ],) : _continueButton;
+    ],) : _skipLinkButton;
 
-  Widget get _continueButton =>
-    Onboarding2UnderlinedButton(
-      title: Localization().getStringEx('panel.onboarding.profile_info.continue.title', 'Continue'),
-      hint: Localization().getStringEx('panel.onboarding.profile_info.continue.hint', ''),
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16, right: 16),
-      onTap: _onTapContinue,
-    );
-
-  Widget get _progressWidget => SizedBox(width: 24, height: 24, child:
-    CircularProgressIndicator(strokeWidth: 2, color: Styles().colors.fillColorSecondary,)
+  Widget get _skipLinkButton => Onboarding2UnderlinedButton(
+    title: Localization().getStringEx('panel.onboarding.profile_info.skip.title', 'Skip'),
+    hint: Localization().getStringEx('panel.onboarding.profile_info.skip.hint', ''),
+    padding: EdgeInsets.only(top: 30, bottom: 30, left: 20, right: 20),
+    onTap: _onTapSkip,
   );
+
+  Widget get _skipProgressWidget => SizedBox(width: 16, height: 16, child:
+    CircularProgressIndicator(strokeWidth: 2, color: Styles().colors.fillColorPrimary,)
+  );
+
+  void _onTapSkip() {
+    Analytics().logSelect(target: "Skip");
+    if (_canSkip) {
+      _finishProfile();
+    }
+  }
+
+  Widget get _backImageButton => OnboardingBackButton(
+    padding: const EdgeInsets.only(top: 30, bottom: 30, left: 10, right: 20),
+    onTap: _onTapBack
+  );
+
+  void _onTapBack() {
+    Analytics().logSelect(target: "Back");
+    Navigator.pop(context);
+  }
 
   void _onProfileStateChanged() {
     if (mounted) {
@@ -114,8 +163,40 @@ class _Onboarding2ProfileInfoPanelState extends State<Onboarding2ProfileInfoPane
     }
   }
 
+  bool get _isLoaded => (_profileInfoKey.currentState?.isLoading == false);
+  bool get _isEditing => (_profileInfoKey.currentState?.isEditing == true);
+  bool get _isProfilePublic => (_profileInfoKey.currentState?.directoryVisibility == true);
+
   void _onTapContinue() {
     Analytics().logSelect(target: "Continue");
+    if (_canContinue) {
+      if (_isProfilePublic) {
+
+        if (_isEditing) {
+          setState(() {
+            _saving = true;
+          });
+          _profileInfoKey.currentState?.saveEdit().then((bool result){
+            if (mounted) {
+              setState(() {
+                _saving = false;
+              });
+              if (result) {
+                _finishProfile();              }
+            }
+          });
+        }
+        else {
+          _profileInfoKey.currentState?.setEditing(true);
+        }
+      }
+      else {
+        _finishProfile();
+      }
+    }
+  }
+
+  void _finishProfile() {
     Map<String, dynamic>? onboardingContext = widget.onboardingContext;
     Function? onContinue = onboardingContext?['onContinueAction'];
     Function? onContinueEx = onboardingContext?['onContinueActionEx'];
@@ -127,8 +208,4 @@ class _Onboarding2ProfileInfoPanelState extends State<Onboarding2ProfileInfoPane
     }
   }
 
-  void _onTapBack() {
-    Analytics().logSelect(target: "Back");
-    Navigator.pop(context);
-  }
 }
