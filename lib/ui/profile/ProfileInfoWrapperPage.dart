b29004337ad9e6ac395d18fd1978cb845c314c0d
Mihail Varbanov <mihail.varbanov@inabyte.com>
2025-03-07 15:12:43 +0200

    Added privacy level link to signed out description [#4847].

--- lib/ui/profile/ProfileInfoWrapperPage.dart
+++ lib/ui/profile/ProfileInfoWrapperPage.dart
@@ -1,13 +1,15 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 import 'package:illinois/service/Analytics.dart';
 import 'package:illinois/ui/profile/ProfileInfoPage.dart';
+import 'package:illinois/ui/settings/SettingsPrivacyPanel.dart';
 import 'package:illinois/utils/AppUtils.dart';
 import 'package:rokwire_plugin/service/auth2.dart';
 import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/styles.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
 
 class ProfileInfoWrapperPage extends StatefulWidget {
   static const String notifySignIn = "edu.illinois.rokwire.profile.sign_in";
@@ -23,6 +25,7 @@ class ProfileInfoWrapperPage extends StatefulWidget {
 class ProfileInfoWrapperPageState extends State<ProfileInfoWrapperPage> implements NotificationsListener {
 
   GestureRecognizer? _signInRecognizer;
+  GestureRecognizer? _privacyRecognizer;
   final GlobalKey<ProfileInfoPageState> _profileInfoKey = GlobalKey();
 
   Future<bool?> saveModified() async => _profileInfoKey.currentState?.saveModified();
@@ -33,6 +36,7 @@ class ProfileInfoWrapperPageState extends State<ProfileInfoWrapperPage> implemen
       Auth2.notifyLoginChanged,
     ]);
     _signInRecognizer = TapGestureRecognizer()..onTap = _onTapSignIn;
+    _privacyRecognizer = TapGestureRecognizer()..onTap = _onTapProfile;
 
     super.initState();
   }
@@ -41,6 +45,7 @@ class ProfileInfoWrapperPageState extends State<ProfileInfoWrapperPage> implemen
   void dispose() {
     NotificationService().unsubscribe(this);
     _signInRecognizer?.dispose();
+    _privacyRecognizer?.dispose();
     super.dispose();
   }
 
@@ -81,16 +86,27 @@ class ProfileInfoWrapperPageState extends State<ProfileInfoWrapperPage> implemen
 
   Widget get _loggedOutContent {
     final String linkLoginMacro = "{{link.login}}";
-    String messageTemplate = Localization().getStringEx('panel.profile.info_and_directory.message.signed_out', 'To view "My Info & User Directory", $linkLoginMacro with your NetID and set your privacy level to 4 or 5 under Settings.');
-    List<String> messages = messageTemplate.split(linkLoginMacro);
-    List<InlineSpan> spanList = <InlineSpan>[];
-    if (0 < messages.length)
-      spanList.add(TextSpan(text: messages.first));
-    for (int index = 1; index < messages.length; index++) {
-      spanList.add(TextSpan(text: Localization().getStringEx('panel.profile.info_and_directory.message.signed_out.link.login', "sign in"), style : Styles().textStyles.getTextStyle("widget.link.button.title.regular"),
-        recognizer: _signInRecognizer, ));
-      spanList.add(TextSpan(text: messages[index]));
-    }
+    final String linkPrivacyMacro = "{{link.privacy}}";
+    String messageTemplate = Localization().getStringEx('panel.profile.info_and_directory.message.signed_out', 'To view "My Info & User Directory", $linkLoginMacro with your NetID and set your privacy level to 4 or 5.');
+    List<InlineSpan> spanList = StringUtils.split<InlineSpan>(messageTemplate, macros: [linkLoginMacro, linkPrivacyMacro], builder: (String entry) {
+      if (entry == linkLoginMacro) {
+        return TextSpan(
+          text: Localization().getStringEx('panel.profile.info_and_directory.message.signed_out.link.login', "sign in"),
+          style : Styles().textStyles.getTextStyle("widget.link.button.title.regular"),
+          recognizer: _signInRecognizer,
+        );
+      }
+      else if (entry == linkPrivacyMacro) {
+        return TextSpan(
+          text: Localization().getStringEx('panel.profile.info_and_directory.message.signed_out.link.privacy', "set your privacy level"),
+          style : Styles().textStyles.getTextStyle("widget.link.button.title.regular"),
+          recognizer: _privacyRecognizer,
+        );
+      }
+      else {
+        return TextSpan(text: entry);
+      }
+    });
 
     return Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16), child:
       RichText(textAlign: TextAlign.left, text:
@@ -102,6 +118,11 @@ class ProfileInfoWrapperPageState extends State<ProfileInfoWrapperPage> implemen
   void _onTapSignIn() {
     Analytics().logSelect(target: 'sign in');
     NotificationService().notify(ProfileInfoWrapperPage.notifySignIn);
+  }
+
+  void _onTapProfile() {
+    Analytics().logSelect(target: 'Privacy Level');
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => SettingsPrivacyPanel(mode: SettingsPrivacyPanelMode.regular,)));
   }
 }
 
