8eb5e93b9ff9f9204042286fe3507f74bb9d0099
Mihail Varbanov <mihail.varbanov@inabyte.com>
2025-05-14 15:28:58 +0300

    Updated MTD Stops Home dropdown menu [#5117].

--- lib/ui/explore/ExploreMapPanel.dart
+++ lib/ui/explore/ExploreMapPanel.dart
@@ -1094,7 +1094,7 @@ class _ExploreMapPanelState extends State<ExploreMapPanel>
         descriptionList.add(TextSpan(text: '  ', style: regularStyle,),);
       }
       descriptionList.add(TextSpan(text: Localization().getStringEx('panel.explore.label.scope.label.title', 'Filter: ') , style: boldStyle, recognizer: _clearMTDStopsScopeRecognizer, ));
-      descriptionList.add(TextSpan(text: _mtdStopScope?.hint ?? '' , style: regularStyle, recognizer: _clearMTDStopsScopeRecognizer));
+      descriptionList.add(TextSpan(text: _mtdStopScope?.displayHint ?? '' , style: regularStyle, recognizer: _clearMTDStopsScopeRecognizer));
       descriptionList.add(_buildCloseSpan(onTap: _onMTDStopsClearScope));
     }
 
