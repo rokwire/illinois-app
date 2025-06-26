e44595071a6c4e4beccba2df8ab1e46b7133296a
Mihail Varbanov <mihail.varbanov@inabyte.com>
2025-06-26 15:56:59 +0300

    Handled config alerts [#5251].

--- lib/service/Config.dart
+++ lib/service/Config.dart
@@ -17,6 +17,7 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:illinois/model/Config.dart';
 import 'package:illinois/service/Storage.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/config.dart' as rokwire;
@@ -77,6 +78,8 @@ class Config extends rokwire.Config {
   Map<String, dynamic> get canvasDeepLink => JsonUtils.mapValue(canvas['deep_link']) ?? {};
 
   Map<String, dynamic> get wellness => JsonUtils.mapValue(content['wellness']) ?? {};
+
+  ConfigAlert? get alert => ConfigAlert.fromJson(JsonUtils.mapValue(content['alert']));
 
   // Getters: Secret Keys
 
@@ -244,6 +247,10 @@ class Config extends rokwire.Config {
 
   @override
   int get refreshTimeout=> kReleaseMode ? super.refreshTimeout : 0;
+
+  // Refresh
+
+  Future<void> refresh() => super.updateFromNet();
 
   // Upgrade
 
