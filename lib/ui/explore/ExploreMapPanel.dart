63262ba5bd88c65190a091c57b469e77c625cdaf
Todor Bachvarov <tobachvarov@gmail.com>
2025-10-16 19:06:00 +0300

    remove commented from merge

--- lib/ui/explore/ExploreMapPanel.dart
+++ lib/ui/explore/ExploreMapPanel.dart
@@ -2509,59 +2509,6 @@ class _ExploreMapPanelState extends State<ExploreMapPanel>
   }
 }
 
-// <<<<<<< HEAD
-// /////////////////////////////////
-// // ExploreMessagePopup
-//
-// class ExploreMessagePopup extends StatelessWidget {
-//   final String message;
-//   final bool Function(String url)? onTapUrl;
-//   ExploreMessagePopup({super.key, required this.message, this.onTapUrl});
-//
-//   static Future<void> show(BuildContext context, String message, { bool Function(String url)? onTapUrl}) =>
-//     showDialog(context: context, builder: (context) => ExploreMessagePopup(message: message, onTapUrl: onTapUrl));
-//
-//   @override
-//   Widget build(BuildContext context) =>
-//     AlertDialog(contentPadding: EdgeInsets.zero, content:
-//       Container(decoration: BoxDecoration(color: Styles().colors.white, borderRadius: BorderRadius.circular(10.0)), child:
-//         Stack(alignment: Alignment.center, fit: StackFit.loose, children: [
-//           Padding(padding: EdgeInsets.all(30), child:
-//             Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisSize: MainAxisSize.min, children: [
-//               Styles().images.getImage('university-logo') ?? Container(),
-//               Padding(padding: EdgeInsets.only(top: 20), child:
-//                 // Text(message, textAlign: TextAlign.center, style: Styles().textStyles.getTextStyle("widget.detail.small")'
-//                 Semantics(focused: true, container: true, child:
-//                   HtmlWidget(message,
-//                     onTapUrl: (url) => (onTapUrl != null) ? onTapUrl!(url) : false,
-//                     textStyle: Styles().textStyles.getTextStyle("widget.detail.small"),
-//                     customStylesBuilder: (element) => (element.localName == "a") ? {"color": ColorUtils.toHex(Styles().colors.fillColorSecondary)} : null
-//                   )
-//                 )
-//               )
-//             ])
-//           ),
-//           Positioned.fill(child:
-//             Align(alignment: Alignment.topRight, child:
-//               Semantics(container: true, child:
-//                 InkWell(onTap: () => _onClose(context, message), child:
-//                   Padding(padding: EdgeInsets.all(16), child:
-//                     Styles().images.getImage("close-circle")
-//                   )
-//                 )
-//               )
-//             )
-//           )
-//         ])
-//       )
-//     );
-//
-//   void _onClose(BuildContext context, String message) {
-//     Analytics().logAlert(text: message, selection: 'Close');
-//     Navigator.of(context).pop();
-//   }
-// }
-
 ///////////////////////////////////
 // ExploreMapSearchEventsParam
 
