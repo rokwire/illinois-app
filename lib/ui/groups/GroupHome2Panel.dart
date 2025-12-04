9cd4e7eff87e13e68e7f29a49a73e3ccd5b21f3f
Mihail Varbanov <mihail.varbanov@inabyte.com>
2025-12-04 12:53:01 +0200

    GroupHome2Panel in progress [#5353].

--- lib/ui/groups/GroupHome2Panel.dart
+++ lib/ui/groups/GroupHome2Panel.dart
@@ -1,9 +1,13 @@
 
+import 'dart:collection';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:illinois/model/Analytics.dart';
 import 'package:illinois/service/Analytics.dart';
 import 'package:illinois/service/Auth2.dart';
+import 'package:illinois/ui/events2/Event2Widgets.dart';
+import 'package:illinois/ui/groups/GroupCreatePanel.dart';
 import 'package:illinois/ui/groups/GroupWidgets.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:illinois/ui/widgets/TabBar.dart' as uiuc;
@@ -34,6 +38,7 @@ enum _ContentActivity { load, refresh, extend }
 class _GroupHome2PanelState extends State<GroupHome2Panel> with NotificationsListener {
 
   ScrollController _scrollController = ScrollController();
+  GlobalKey _filtersButtonKey = GlobalKey();
 
   List<Group>? _contentList;
   Map<String, GlobalKey> _cardKeys = <String, GlobalKey>{};
@@ -92,13 +97,116 @@ class _GroupHome2PanelState extends State<GroupHome2Panel> with NotificationsLis
     Expanded(child:
       RefreshIndicator(onRefresh: _onRefresh, child:
         SingleChildScrollView(controller: _scrollController, physics: AlwaysScrollableScrollPhysics(), child:
-        _bodyContent,
+          _bodyContent,
         )
       )
     )
   ],);
 
-  Widget get _commandBar => Container();
+  // Command Bar
+
+  Widget get _commandBar =>
+    Container(decoration: _commandBarDecoration, child:
+      Padding(padding: EdgeInsets.only(top: 8), child:
+        Column(children: [
+          _commandButtonsBar,
+          _contentDescriptionBar,
+        ],)
+      ),
+    );
+
+  Decoration get _commandBarDecoration => BoxDecoration(
+    color: Styles().colors.white,
+    border: Border.all(color: Styles().colors.disabledTextColor, width: 1)
+  );
+
+  Widget get _commandButtonsBar => Row(children: [
+    Padding(padding: EdgeInsets.only(left: 16)),
+    Expanded(flex: 6, child: Wrap(spacing: 8, runSpacing: 8, children: [ //Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+      MergeSemantics(key: _filtersButtonKey, child:
+        Semantics(/* TBD: value: _currentFilterParam.descriptionText, hint: _filtersButtonHint,*/ child:
+          Event2FilterCommandButton(
+            title: Localization().getStringEx('panel.group.home2.bar.button.filter.title', 'Filter'),
+            leftIconKey: 'filters',
+            rightIconKey: 'chevron-right',
+            onTap: _onFilter,
+          )
+        )
+      ),
+    ])),
+    Expanded(flex: 4, child: Wrap(alignment: WrapAlignment.end, crossAxisAlignment: WrapCrossAlignment.center, verticalDirection: VerticalDirection.up, children: [
+      Visibility(visible: Auth2().isOidcLoggedIn, child:
+        Event2ImageCommandButton(Styles().images.getImage('plus-circle'),
+          label: Localization().getStringEx('panel.group.home2.bar.button.create.title', 'Create'),
+          hint: Localization().getStringEx('panel.group.home2.bar.button.create.hint', 'Tap to create group'),
+          contentPadding: EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12),
+          onTap: _onCreate
+        ),
+      ),
+      Event2ImageCommandButton(Styles().images.getImage('search'),
+        label: Localization().getStringEx('panel.group.home2.bar.button.search.title', 'Search'),
+        hint: Localization().getStringEx('panel.group.home2.bar.button.search.hint', 'Tap to search groups'),
+        contentPadding: EdgeInsets.only(left: 8, right: 16, top: 12, bottom: 12),
+        onTap: _onSearch
+      ),
+    ])),
+  ],);
+
+  Widget get _contentDescriptionBar {
+    LinkedHashMap<String, List<String>>? descriptionMap = LinkedHashMap<String, List<String>>();
+    if ((descriptionMap != null) && descriptionMap.isNotEmpty)  {
+      List<InlineSpan> descriptionList = <InlineSpan>[];
+      TextStyle? boldStyle = Styles().textStyles.getTextStyle('widget.card.title.tiny.fat');
+      TextStyle? regularStyle = Styles().textStyles.getTextStyle('widget.card.detail.small.regular');
+      descriptionMap.forEach((String descriptionCategory, List<String> descriptionItems){
+        if (descriptionList.isNotEmpty) {
+          descriptionList.add(TextSpan(text: '; ', style: regularStyle,),);
+        }
+        if (descriptionItems.isEmpty) {
+          descriptionList.add(TextSpan(text: descriptionCategory, style: boldStyle,));
+        } else {
+          descriptionList.add(TextSpan(text: "$descriptionCategory: " , style: boldStyle,));
+          descriptionList.add(TextSpan(text: descriptionItems.join(', '), style: regularStyle,),);
+        }
+      });
+
+      return Padding(padding: EdgeInsets.only(top: 12), child:
+        Container(decoration: _contentDescriptionDecoration, child:
+          Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Expanded(child:
+              Padding(padding: EdgeInsets.only(left: 12, top: 16, bottom: 16), child:
+                RichText(text: TextSpan(style: regularStyle, children: descriptionList)),
+              ),
+            ),
+            Visibility(visible: _canShareFilters, child:
+              Event2ImageCommandButton(Styles().images.getImage('share-nodes'),
+                label: Localization().getStringEx('panel.group.home2.bar.button.share.title', 'Share'),
+                hint: Localization().getStringEx('panel.group.home2.bar.button.share.hint', 'Tap to share current groups'),
+                contentPadding: EdgeInsets.only(left: 16, right: _canClearFilter ? (8 + 2) : 16, top: 12, bottom: 12),
+                onTap: _onShareFilters
+              ),
+            ),
+            Visibility(visible: _canClearFilter, child:
+              Event2ImageCommandButton(Styles().images.getImage('close'), // size: 14
+                label: Localization().getStringEx('panel.group.home2.bar.button.clear.title', 'Clear Filters'),
+                hint: Localization().getStringEx('panel.group.home2.bar.button.clear.hint', 'Tap to clear current filters'),
+                contentPadding: EdgeInsets.only(left: 8 + 2, right: 16 + 2, top: 12, bottom: 12),
+                onTap: _onClearFilter
+              ),
+            ),
+          ],)
+      ));
+
+    } else {
+      return Container(height: 12);
+    }
+
+  }
+
+  Decoration get _contentDescriptionDecoration => BoxDecoration(
+    color: Styles().colors.white,
+    border: Border(top: BorderSide(color: Styles().colors.disabledTextColor, width: 1))
+  );
 
   Widget get _bodyContent {
     if (_contentActivity == _ContentActivity.load) {
@@ -141,7 +249,7 @@ class _GroupHome2PanelState extends State<GroupHome2Panel> with NotificationsLis
     );
   }
 
-  Widget _buildMessageContent(String message, { String? title }) =>
+  Widget _buildMessageContent(String message, { String? title }) => Center(child:
     Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: _screenHeight / 6), child:
       Column(children: [
         (title != null) ? Padding(padding: EdgeInsets.only(bottom: 12), child:
@@ -149,7 +257,8 @@ class _GroupHome2PanelState extends State<GroupHome2Panel> with NotificationsLis
         ) : Container(),
         Text(message, textAlign: TextAlign.center, style: Styles().textStyles.getTextStyle((title != null) ? 'widget.item.regular.thin' : 'widget.item.medium.fat'),),
       ],),
-    );
+    )
+  );
 
   Widget get _loadingContent => Column(children: [
     Padding(padding: EdgeInsets.symmetric(vertical: _screenHeight / 4), child:
@@ -185,4 +294,46 @@ class _GroupHome2PanelState extends State<GroupHome2Panel> with NotificationsLis
   Future<void> _loadContent({ int limit = _contentPageLength }) async {}
   Future<void> _refreshContent() async {}
   Future<void> _extendContent() async {}
+
+  // Command Handlers
+
+  void _onFilter() {
+    Analytics().logSelect(target: 'Filter');
+
+  }
+
+  void _onSearch() {
+    Analytics().logSelect(target: 'Search');
+    //Navigator.push(context, CupertinoPageRoute(builder: (context) => GroupsSearchPanel()));
+  }
+
+  void _onCreate() {
+    Analytics().logSelect(target: 'Create');
+    Navigator.push(context, MaterialPageRoute(builder: (context) => GroupCreatePanel()));
+  }
+
+  bool get _canShareFilters => true;
+
+  void _onShareFilters() {
+    Analytics().logSelect(target: 'Share Filters');
+    // Navigator.push(context, CupertinoPageRoute(builder: (context) => QrCodePanel.fromEventFilterParam(_currentFilterParam)));
+  }
+
+  bool get _canClearFilter => true;
+    /*(_timeFilter != Event2TimeFilter.upcoming) ||
+    (_sortType != Event2SortType.dateTime) ||
+    _types.isNotEmpty ||
+    _attributes.isNotEmpty;*/
+
+  void _onClearFilter() {
+    Analytics().logSelect(target: 'Clear Filter');
+    /*setState(() {
+      _timeFilter = Event2TimeFilter.upcoming;
+      _customStartTime = null;
+      _customEndTime = null;
+      _types = LinkedHashSet<Event2TypeFilter>();
+      _attributes = <String, dynamic>{};
+      _sortType = Event2SortType.dateTime;
+    });*/
+  }
 }
