6bebbfe8b3e136b4c1fa9cea3fab4cd8ad41cc08
William Widjaja <willgr81@gmail.com>
2026-06-04 22:51:55 -0400

    made University Reporting Form button wider to prevent linebreak

--- lib/ui/gbv/GBVDetailContentWidget.dart
+++ lib/ui/gbv/GBVDetailContentWidget.dart
@@ -80,12 +80,13 @@ class GBVDetailContentWidget extends StatelessWidget {
       case GBVResourceDetailType.button:
         return [
           Expanded(child:
-            Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12), child:
+            Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12), child:
               RoundedButton(
                   label: detail.title ?? detail.content ?? '',
                   textStyle: Styles().textStyles.getTextStyle('widget.detail.regular.fat'),
+                  textAlign: TextAlign.start,
                   rightIcon: Styles().images.getImage('external-link', excludeFromSemantics: true) ?? Container(),
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
                   onTap: () => _onTapButton(context, detail)
               )
             )
