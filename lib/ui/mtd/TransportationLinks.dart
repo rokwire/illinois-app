b64cce37140897e2582cef0a7bf25b769d0ecb4d
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2026-08-21 11:52:40 +0300

    Remove warning.

--- lib/ui/mtd/TransportationLinks.dart
+++ lib/ui/mtd/TransportationLinks.dart
@@ -1,5 +1,4 @@
 
-import 'package:illinois/model/Analytics.dart';
 import 'package:illinois/ui/gbv/GBVResourceDirectoryContentPanel.dart';
 import 'package:illinois/ui/home/HomeSavedGBVResourcesWidget.dart';
 import 'package:rokwire_plugin/service/localization.dart';
