eef2bb1fd7fdfb2dbe77e8af8c5b21a0914ec5a3
Mihail Varbanov <mihail.varbanov@inabyte.com>
2026-08-28 10:41:58 +0300

    Updated HomeLoginWidget [#5885].

--- lib/ui/home/HomeLoginWidget.dart
+++ lib/ui/home/HomeLoginWidget.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:illinois/ext/Auth2.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:illinois/service/Analytics.dart';
 import 'package:illinois/service/Storage.dart';
 import 'package:illinois/ui/home/HomeWidgets.dart';
@@ -14,6 +14,7 @@ import 'package:rokwire_plugin/service/localization.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/styles.dart';
 import 'package:rokwire_plugin/ui/widgets/rounded_button.dart';
+import 'package:rokwire_plugin/utils/utils.dart';
 
 class HomeLoginWidget extends StatefulWidget {
 
@@ -63,9 +64,9 @@ class _HomeLoginWidgetState extends State<HomeLoginWidget> with NotificationsLis
   Widget _buildContentCard(List<Widget> contentWidgets) =>
     HomeCardWidget(
       title: Localization().getStringEx("panel.home.connect.not_logged_in.title", "Connect to Illinois").toUpperCase(),
-      padding: const EdgeInsets.only(left: 12, right: 12, top: 12),
+      padding: const EdgeInsets.only(left: 12, right: 12, top: 16, bottom: 12),
       onClose: _onClose,
-      child: Column(children:contentWidgets),
+      child: Column(children: contentWidgets),
     );
 
   List<Widget> get _contentWidgets {
@@ -84,11 +85,7 @@ class _HomeLoginWidgetState extends State<HomeLoginWidget> with NotificationsLis
 
   void _addContentWidget(List<Widget> contentList, Widget contentWidget) {
     if (contentList.isNotEmpty) {
-      contentList.add(
-        Padding(padding: EdgeInsets.only(bottom: 16), child:
-          Container(height: 1, color: Styles().colors.fillColorPrimaryTransparent015,),
-        )
-      );
+      contentList.add(SizedBox(height: 16,));
     }
     contentList.add(contentWidget);
   }
@@ -116,20 +113,13 @@ class _HomeLoginNetIdWidgetState extends State<_HomeLoginNetIdWidget> {
   @override
   Widget build(BuildContext context) =>
     Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-      //Container(margin: EdgeInsets.only(bottom: 12), height: 1, color: Styles().colors.fillColorPrimaryTransparent015,),
-      RichText(textScaler: MediaQuery.of(context).textScaler, text:
-        TextSpan(style: Styles().textStyles.getTextStyle("widget.item.regular.thin"), children: <TextSpan>[
-          TextSpan(text: Localization().getStringEx("panel.home.connect.not_logged_in.netid.description.part_1", "Are you a ")),
-          TextSpan(text: Localization().getStringEx("panel.home.connect.not_logged_in.netid.description.part_2", "university student"), style: Styles().textStyles.getTextStyle("widget.detail.regular.fat")),
-          TextSpan(text: Localization().getStringEx("panel.home.connect.not_logged_in.netid.description.part_3", " or ")),
-          TextSpan(text: Localization().getStringEx("panel.home.connect.not_logged_in.netid.description.part_4", "employee"), style: Styles().textStyles.getTextStyle("widget.detail.regular.fat")),
-          TextSpan(text: Localization().getStringEx("panel.home.connect.not_logged_in.netid.description.part_5", "? Sign in with your NetID to access features connected to your university account.")),
-        ],),
+      Text(Localization().getStringEx('panel.home.connect.not_logged_in.netid.description', 'Sign in with your Illinois NetID to access your Illini ID, course schedule, and other personalized features.'),
+        style: Styles().textStyles.getTextStyle("widget.item.regular.thin"),
       ),
-      Padding(padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), child:
+      Padding(padding: EdgeInsets.only(left: 16, right: 16, top: 16), child:
         Semantics(explicitChildNodes: true, child:
           RoundedButton(
-            label: Localization().getStringEx("panel.home.connect.not_logged_in.netid.title", "Sign In with your NetID"),
+            label: Localization().getStringEx("panel.home.connect.not_logged_in.netid.button.title", "Sign In with your NetID"),
             textStyle: Styles().textStyles.getTextStyle("widget.button.title.medium.fat"),
             borderColor: Styles().colors.fillColorSecondary,
             backgroundColor: Styles().colors.surface,
@@ -140,7 +130,6 @@ class _HomeLoginNetIdWidgetState extends State<_HomeLoginNetIdWidget> {
         ),
       ),
     ]);
-
 
   void _onTapConnectNetIdClicked(BuildContext context) {
     Analytics().logSelect(target: "Connect netId", source: widget.runtimeType.toString());
@@ -165,45 +154,43 @@ class _HomeLoginNetIdWidgetState extends State<_HomeLoginNetIdWidget> {
 }
 
 class _HomeLoginPhoneOrEmailWidget extends StatelessWidget{
+
   @override
-  Widget build(BuildContext context) =>
-    Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-      Padding(padding: EdgeInsets.zero, child:
-        RichText(textScaler: MediaQuery.of(context).textScaler, text:
-        TextSpan(style: Styles().textStyles.getTextStyle("widget.item.regular.thin"), children: <TextSpan>[
-          TextSpan(text: Localization().getStringEx("panel.home.connect.not_logged_in.phone_or_email.description.part_1", "Don't have a NetID? "), style: Styles().textStyles.getTextStyle("widget.detail.regular.fat")),
-          TextSpan( text: (Auth2().prefs?.isProspective != true) ?
-            Localization().getStringEx("panel.home.connect.not_logged_in.phone_or_email.description.part_2a", "Verify your phone number or sign up/in by email.") :
-            Localization().getStringEx("panel.home.connect.not_logged_in.phone_or_email.description.part_2b", "Sign in with an email address to access more features.")
-          ),
-        ],),
-      )),
+  Widget build(BuildContext context) => HtmlWidget(_descriptionHtml,
+      onTapUrl : (url) { _onTapLink(context, url); return true; },
+      textStyle:  Styles().textStyles.getTextStyle("widget.description.small.semi_fat"),
+      customStylesBuilder: (element) => (element.localName == "a") ? _htmlLinkStyle : null,
+  );
 
-      Padding(padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16), child:
-        Semantics(explicitChildNodes: true, child:
-          RoundedButton(
-            label: Localization().getStringEx("panel.home.connect.not_logged_in.phone_or_email.title", "Continue"),
-            hint: '',
-            textStyle: Styles().textStyles.getTextStyle("widget.button.title.medium.fat"),
-            borderColor: Styles().colors.fillColorSecondary,
-            backgroundColor: Styles().colors.surface,
-            onTap: ()=> _onTapPhoneOrEmailClicked(context),
-          ),
-        ),
-      ),
+  static const String localScheme = 'local';
+  static const String signInHost = 'signin';
+  static const String signInUrlMacro = '{{signin_url}}';
+  static const String signInUrl = '$localScheme://$signInHost';
 
-    ]);
+  String get _descriptionHtml => Localization().getStringEx("panel.home.connect.not_logged_in.phone_or_email.description", "<b>Don’t have a NetID?</b><a href='$signInUrlMacro'>Use your mobile phone number or personal (non-Illinois) email address to sign in.</a>").
+    replaceAll(signInUrlMacro, signInUrl);
 
-  void _onTapPhoneOrEmailClicked(BuildContext context) {
-    Analytics().logSelect(target: "Phone or Email Login", source: runtimeType.toString());
-    if (Connectivity().isOffline) {
-      AppAlert.showOfflineMessage(context, Localization().getStringEx('panel.label.offline.phone_or_email', 'Feature not available when offline.'));
-    }
-    else if (!FlexUI().isAuthenticationAvailable) {
-      AppAlert.showAuthenticationNAMessage(context);
-    }
-    else {
-      Navigator.push(context, CupertinoPageRoute(settings: RouteSettings(), builder: (context) => ProfileLoginPhoneOrEmailPanel(onFinish: () => _didLogin(context)),),);
+  Map<String, String> get _htmlLinkStyle => <String, String>{
+    //'color': _htmlLinkColor,
+    'text-decoration-color': _htmlLinkColor,
+  };
+
+  String get _htmlLinkColor =>
+    ColorUtils.toHex(Styles().colors.fillColorSecondary);
+
+  void _onTapLink(BuildContext context, String? url) {
+    Uri? uri = (url != null) ? Uri.tryParse(url) : null;
+    if ((uri?.scheme == localScheme) && (uri?.host == signInHost)) {
+      Analytics().logSelect(target: "Phone or Email Login", source: runtimeType.toString());
+      if (Connectivity().isOffline) {
+        AppAlert.showOfflineMessage(context, Localization().getStringEx('panel.label.offline.phone_or_email', 'Feature not available when offline.'));
+      }
+      else if (!FlexUI().isAuthenticationAvailable) {
+        AppAlert.showAuthenticationNAMessage(context);
+      }
+      else {
+        Navigator.push(context, CupertinoPageRoute(settings: RouteSettings(), builder: (context) => ProfileLoginPhoneOrEmailPanel(onFinish: () => _didLogin(context)),),);
+      }
     }
   }
 
