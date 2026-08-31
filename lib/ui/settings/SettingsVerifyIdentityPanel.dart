2b4a0d922b17cd1265b4817d50effdda24b13844
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2026-08-31 12:45:23 +0300

    Remove warning [web]

--- lib/ui/settings/SettingsVerifyIdentityPanel.dart
+++ lib/ui/settings/SettingsVerifyIdentityPanel.dart
@@ -28,7 +28,6 @@ import 'package:rokwire_plugin/service/styles.dart';
 import 'package:illinois/ui/widgets/HeaderBar.dart';
 import 'package:illinois/ui/widgets/TabBar.dart' as uiuc;
 import 'package:illinois/utils/AppUtils.dart';
-import 'package:rokwire_plugin/ui/widgets/web_semantics.dart';
 import 'package:rokwire_plugin/ui/widgets/rounded_button.dart';
 import 'package:rokwire_plugin/utils/utils.dart';
 import 'package:web/web.dart' as web;
